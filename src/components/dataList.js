81acd3ccfac1bfaa9fb89621cbe79bafccbd563e
kupara <ewkups@gmail.com>
2019-10-31 15:59:32 +0100

    fix: set min height on search wrapper

--- src/components/dataList.js
+++ src/components/dataList.js
@@ -290,7 +290,6 @@
         alignItems: 'center',
         justifyContent: 'space-between',
         flexDirection: 'row-reverse',
-        minHeight: '4rem',
         width: '100%',
       },
       searchWrapper: {
@@ -298,6 +297,7 @@
         alignItems: 'center',
         padding: [0, '0.5rem'],
         borderBottom: [1, 'solid', '#000'],
+        minHeight: '4rem',
       },
       searchIcon: {
         fontSize: '1.25rem',
