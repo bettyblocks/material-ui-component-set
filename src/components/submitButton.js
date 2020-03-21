9c53a3167fc95af178bea75f52bc73251696c394
Chris Obdam <>
2020-03-21 15:32:58 +0100

    Add color

--- src/components/submitButton.js
+++ src/components/submitButton.js
@@ -43,6 +43,7 @@
       },
       root: {
         '&.MuiButton-root': {
+          color: style.getColor('White'),
           marginTop: ({ options: { outerSpacing } }) =>
             getSpacing(outerSpacing[0]),
           marginRight: ({ options: { outerSpacing } }) =>
