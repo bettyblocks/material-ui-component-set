753c63100aff878e5ded9dbe681ab6fe437706fb
Chris Obdam <>
2020-03-22 17:32:23 +0100

    Aded IconButton

---
+++ src/components/iconButton.js
@@ -0,0 +1,119 @@
+(() => ({
+  name: 'IconButton',
+  type: 'CONTENT_COMPONENT',
+  allowedTypes: [],
+  orientation: 'VERTICAL',
+  jsx: (() => {
+    const { IconButton } = window.MaterialUI.Core;
+    const { Icons } = window.MaterialUI;
+    const { size, icon, linkType, linkTo, linkToExternal } = options;
+    const isDev = B.env === 'dev';
+
+    const IconButtonComponent = (
+      <IconButton
+        classes={{
+          root: classes.root,
+        }}
+        // if prefab is button set href when link is external
+        href={linkType === 'External' ? linkToExternal : undefined}
+        // if prefab is button and internal link
+        component={linkType === 'Internal' ? B.Link : undefined}
+        endpoint={linkType === 'Internal' ? linkTo : undefined}
+      >
+        {React.createElement(Icons[icon], { fontSize: size })}
+      </IconButton>
+    );
+    return isDev ? (
+      <div className={classes.wrapper}>{IconButtonComponent}</div>
+    ) : (
+      IconButtonComponent
+    );
+  })(),
+  styles: B => t => {
+    const style = new B.Styling(t);
+    const getSpacing = (idx, device = 'Mobile') =>
+      idx === '0' ? '0rem' : style.getSpacing(idx, device);
+    return {
+      wrapper: {
+        display: ({ options: { fullWidth } }) =>
+          fullWidth ? 'block' : 'inline-block',
+        width: ({ options: { fullWidth } }) => fullWidth && '100%',
+        minHeight: '1rem',
+        '& > *': {
+          pointerEvents: 'none',
+        },
+      },
+      root: {
+        color: ({ options: { color } }) => [
+          style.getColor(color),
+          '!important',
+        ],
+        '&.MuiButton-root': {
+          width: ({ options: { fullWidth, outerSpacing } }) => {
+            if (!fullWidth) return 'auto';
+            const marginRight = getSpacing(outerSpacing[1]);
+            const marginLeft = getSpacing(outerSpacing[3]);
+            return `calc(100% - ${marginRight} - ${marginLeft})`;
+          },
+          marginTop: ({ options: { outerSpacing } }) =>
+            getSpacing(outerSpacing[0]),
+          marginRight: ({ options: { outerSpacing } }) =>
+            getSpacing(outerSpacing[1]),
+          marginBottom: ({ options: { outerSpacing } }) =>
+            getSpacing(outerSpacing[2]),
+          marginLeft: ({ options: { outerSpacing } }) =>
+            getSpacing(outerSpacing[3]),
+
+          [`@media ${B.mediaMinWidth(768)}`]: {
+            width: ({ options: { fullWidth, outerSpacing } }) => {
+              if (!fullWidth) return 'auto';
+              const marginRight = getSpacing(outerSpacing[1], 'Portrait');
+              const marginLeft = getSpacing(outerSpacing[3], 'Portrait');
+              return `calc(100% - ${marginRight} - ${marginLeft})`;
+            },
+            marginTop: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[0], 'Portrait'),
+            marginRight: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[1], 'Portrait'),
+            marginBottom: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[2], 'Portrait'),
+            marginLeft: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[3], 'Portrait'),
+          },
+          [`@media ${B.mediaMinWidth(1024)}`]: {
+            width: ({ options: { fullWidth, outerSpacing } }) => {
+              if (!fullWidth) return 'auto';
+              const marginRight = getSpacing(outerSpacing[1], 'Landscape');
+              const marginLeft = getSpacing(outerSpacing[3], 'Landscape');
+              return `calc(100% - ${marginRight} - ${marginLeft})`;
+            },
+            marginTop: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[0], 'Landscape'),
+            marginRight: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[1], 'Landscape'),
+            marginBottom: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[2], 'Landscape'),
+            marginLeft: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[3], 'Landscape'),
+          },
+          [`@media ${B.mediaMinWidth(1200)}`]: {
+            width: ({ options: { fullWidth, outerSpacing } }) => {
+              if (!fullWidth) return 'auto';
+              const marginRight = getSpacing(outerSpacing[1], 'Desktop');
+              const marginLeft = getSpacing(outerSpacing[3], 'Desktop');
+              return `calc(100% - ${marginRight} - ${marginLeft})`;
+            },
+            marginTop: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[0], 'Desktop'),
+            marginRight: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[1], 'Desktop'),
+            marginBottom: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[2], 'Desktop'),
+            marginLeft: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[3], 'Desktop'),
+          },
+        },
+      },
+    };
+  },
+}))();
