385faeaa3cd641dfef0a08d7f108cca36541354b
Chris Obdam <>
2020-03-26 11:04:05 +0100

    Margin default to normal

--- src/prefabs/radio.js
+++ src/prefabs/radio.js
@@ -175,7 +175,7 @@
         {
           label: 'Margin',
           key: 'margin',
-          value: 'none',
+          value: 'normal',
           type: 'CUSTOM',
           configuration: {
             as: 'BUTTONGROUP',
