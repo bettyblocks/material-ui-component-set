6ada57072244576017139f26046002cc6d49d0bb
Jeroen Reijs <jeroen.reijs@bettyblocks.com>
2020-03-26 11:14:51 +0100

    fix: margin default to normal

--- src/prefabs/select.js
+++ src/prefabs/select.js
@@ -174,7 +174,7 @@
         {
           label: 'Margin',
           key: 'margin',
-          value: 'none',
+          value: 'normal',
           type: 'CUSTOM',
           configuration: {
             as: 'BUTTONGROUP',
