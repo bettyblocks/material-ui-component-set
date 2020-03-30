38d16f52464aec7879aba0f2517353f7a06451b6
Chris Obdam <>
2020-03-30 11:07:03 +0200

    Card Component

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,47 @@
 (() => ({
   name: 'Card',
   type: 'CONTAINER_COMPONENT',
-  allowedTypes: ['BODY_COMPONENT', 'CONTAINER_COMPONENT', 'CONTENT_COMPONENT'],
+  allowedTypes: [
+    'BODY_COMPONENT',
+    'CONTAINER_COMPONENT',
+    'CONTENT_COMPONENT',
+    'CARD_MEDIA',
+    'CARD_ACTIONS',
+    'CARD_CONTENT',
+  ],
   orientation: 'HORIZONTAL',
   jsx: (() => {
-    const { Card, CardHeader, CardContent } = window.MaterialUI.Core;
-    const { raised, variant, title, subTitle } = options;
+    const { Card } = window.MaterialUI.Core;
+    const { raised, variant } = options;
     const isDev = B.env === 'dev';
-    const card = (
+    const isPristine = children.length === 0 && isDev;
+    const PlaceHolder = <div className={classes.empty}>Card</div>;
+
+    const CardComponent = (
       <Card variant={variant} raised={raised}>
-        {title.length || subTitle.length ? (
-          <CardHeader title={title} subheader={subTitle} />
-        ) : (
-          ''
-        )}
-        <CardContent>
-          {!children.length & isDev ? 'Card' : children}
-        </CardContent>
+        {isPristine ? PlaceHolder : children}
       </Card>
     );
-    return isDev ? <div>{card}</div> : card;
+
+    return isDev ? <div>{CardComponent}</div> : CardComponent;
   })(),
-  styles: () => () => ({}),
+  styles: () => () => ({
+    empty: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '4rem',
+      height: '100%',
+      width: '100%',
+      fontSize: '0.75rem',
+      color: '#262A3A',
+      textTransform: 'uppercase',
+      boxSizing: 'border-box',
+      borderWidth: '0.0625rem',
+      borderColor: '#AFB5C8',
+      borderStyle: 'dashed',
+      backgroundColor: '#F0F1F5',
+    },
+  }),
 }))();
--- src/prefabs/card.js
+++ src/prefabs/card.js
@@ -1,7 +1,7 @@
 (() => ({
   name: 'Card',
   icon: 'ContainerIcon',
-  category: 'LAYOUT',
+  category: 'CARDS',
   structure: [
     {
       name: 'Card',
@@ -39,7 +39,302 @@
           },
         },
       ],
-      descendants: [],
+      descendants: [
+        {
+          name: 'CardMedia',
+          options: [
+            {
+              value:
+                'https://material-ui.com/static/images/cards/contemplative-reptile.jpg',
+              label: 'Image',
+              key: 'image',
+              type: 'TEXT',
+            },
+          ],
+          descendants: [],
+        },
+        {
+          name: 'CardContent',
+          options: [],
+          descendants: [
+            {
+              name: 'Text',
+              options: [
+                {
+                  type: 'VARIABLE',
+                  label: 'Content',
+                  key: 'content',
+                  value: ['Lizard'],
+                  configuration: {
+                    as: 'MULTILINE',
+                  },
+                },
+                {
+                  value: 'Title5',
+                  label: 'Type',
+                  key: 'type',
+                  type: 'FONT',
+                },
+                {
+                  type: 'CUSTOM',
+                  label: 'Text Alignment',
+                  key: 'textAlignment',
+                  value: 'left',
+                  configuration: {
+                    as: 'BUTTONGROUP',
+                    dataType: 'string',
+                    allowedInput: [
+                      { name: 'Left', value: 'left' },
+                      { name: 'Center', value: 'center' },
+                      { name: 'Right', value: 'right' },
+                    ],
+                  },
+                },
+                {
+                  value: ['0rem', '0rem', '0rem', '0rem'],
+                  label: 'Outer space',
+                  key: 'outerSpacing',
+                  type: 'SIZES',
+                },
+              ],
+              descendants: [],
+            },
+            {
+              name: 'Text',
+              options: [
+                {
+                  type: 'VARIABLE',
+                  label: 'Content',
+                  key: 'content',
+                  value: [
+                    'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+                  ],
+                  configuration: {
+                    as: 'MULTILINE',
+                  },
+                },
+                {
+                  value: 'Body2',
+                  label: 'Type',
+                  key: 'type',
+                  type: 'FONT',
+                },
+                {
+                  type: 'CUSTOM',
+                  label: 'Text Alignment',
+                  key: 'textAlignment',
+                  value: 'left',
+                  configuration: {
+                    as: 'BUTTONGROUP',
+                    dataType: 'string',
+                    allowedInput: [
+                      { name: 'Left', value: 'left' },
+                      { name: 'Center', value: 'center' },
+                      { name: 'Right', value: 'right' },
+                    ],
+                  },
+                },
+                {
+                  value: ['0rem', '0rem', '0rem', '0rem'],
+                  label: 'Outer space',
+                  key: 'outerSpacing',
+                  type: 'SIZES',
+                },
+              ],
+              descendants: [],
+            },
+          ],
+        },
+        {
+          name: 'CardActions',
+          options: [
+            {
+              value:
+                'https://material-ui.com/static/images/cards/contemplative-reptile.jpg',
+              label: 'Image',
+              key: 'image',
+              type: 'TEXT',
+            },
+          ],
+          descendants: [
+            {
+              name: 'Button',
+              options: [
+                {
+                  label: 'Visible',
+                  key: 'visible',
+                  value: true,
+                  type: 'TOGGLE',
+                },
+                {
+                  type: 'VARIABLE',
+                  label: 'Button text',
+                  key: 'buttonText',
+                  value: ['Button'],
+                },
+                {
+                  type: 'CUSTOM',
+                  label: 'Link to',
+                  key: 'linkType',
+                  value: 'Internal',
+                  configuration: {
+                    as: 'BUTTONGROUP',
+                    dataType: 'string',
+                    allowedInput: [
+                      { name: 'Internal page', value: 'Internal' },
+                      { name: 'External page', value: 'External' },
+                      { name: 'Action', value: 'Action' },
+                    ],
+                  },
+                },
+                {
+                  value: '',
+                  label: 'Page',
+                  key: 'linkTo',
+                  type: 'ENDPOINT',
+                  configuration: {
+                    condition: {
+                      type: 'SHOW',
+                      option: 'linkType',
+                      comparator: 'EQ',
+                      value: 'Internal',
+                    },
+                  },
+                },
+                {
+                  value: '',
+                  label: 'URL',
+                  key: 'linkToExternal',
+                  type: 'TEXT',
+                  configuration: {
+                    placeholder: 'Starts with https:// or http://',
+                    condition: {
+                      type: 'SHOW',
+                      option: 'linkType',
+                      comparator: 'EQ',
+                      value: 'External',
+                    },
+                  },
+                },
+                {
+                  value: '',
+                  label: 'Action',
+                  key: 'ActionId',
+                  type: 'ACTION',
+                  configuration: {
+                    condition: {
+                      type: 'SHOW',
+                      option: 'linkType',
+                      comparator: 'EQ',
+                      value: 'Action',
+                    },
+                  },
+                },
+                {
+                  type: 'CUSTOM',
+                  label: 'variant',
+                  key: 'variant',
+                  value: 'text',
+                  configuration: {
+                    as: 'BUTTONGROUP',
+                    dataType: 'string',
+                    allowedInput: [
+                      { name: 'Text', value: 'text' },
+                      { name: 'Outlined', value: 'outlined' },
+                      { name: 'Contained', value: 'contained' },
+                    ],
+                  },
+                },
+                {
+                  value: false,
+                  label: 'Full width',
+                  key: 'fullWidth',
+                  type: 'TOGGLE',
+                },
+                {
+                  value: 'medium',
+                  label: 'Size',
+                  key: 'size',
+                  type: 'CUSTOM',
+                  configuration: {
+                    as: 'BUTTONGROUP',
+                    dataType: 'string',
+                    allowedInput: [
+                      { name: 'Medium', value: 'medium' },
+                      { name: 'Small', value: 'small' },
+                    ],
+                  },
+                },
+                {
+                  label: 'StartIcon',
+                  key: 'startIcon',
+                  value: 'None',
+                  type: 'CUSTOM',
+                  configuration: {
+                    as: 'DROPDOWN',
+                    dataType: 'string',
+                    allowedInput: [
+                      {
+                        name: '',
+                        value: 'None',
+                      },
+                      {
+                        name: 'ExpandMore',
+                        value: 'ExpandMore',
+                      },
+                      {
+                        name: 'FilterList',
+                        value: 'FilterList',
+                      },
+                      {
+                        name: 'Search',
+                        value: 'Search',
+                      },
+                      {
+                        name: 'FileCopy',
+                        value: 'FileCopy',
+                      },
+                      {
+                        name: 'GetApp',
+                        value: 'GetApp',
+                      },
+                      {
+                        name: 'Email',
+                        value: 'Email',
+                      },
+                    ],
+                  },
+                },
+                {
+                  type: 'COLOR',
+                  label: 'Text color',
+                  key: 'textColor',
+                  value: 'Black',
+                },
+                {
+                  type: 'COLOR',
+                  label: 'Background color',
+                  key: 'background',
+                  value: 'Success',
+                },
+                {
+                  value: ['0rem', 'M', '0rem', '0rem'],
+                  label: 'Outer space',
+                  key: 'outerSpacing',
+                  type: 'SIZES',
+                },
+                {
+                  label: 'Disabled',
+                  key: 'disabled',
+                  value: false,
+                  type: 'TOGGLE',
+                },
+              ],
+              descendants: [],
+            },
+          ],
+        },
+      ],
     },
   ],
 }))();
