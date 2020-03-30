29204ea5c04e7c8854b0e34ed90ccf637802ff6b
Chris Obdam <>
2020-03-30 22:18:50 +0200

    Added new breadcrumbs (plural!)

---
+++ src/prefabs/breadcrumbs.js
@@ -0,0 +1,19 @@
+(() => ({
+  name: 'Breadcrumbs',
+  icon: 'BreadcrumbIcon',
+  category: 'NAVIGATION',
+  structure: [
+    {
+      name: 'Breadcrumbs',
+      options: [
+        {
+          type: 'TEXT',
+          label: 'Separator',
+          key: 'separator',
+          value: '/',
+        },
+      ],
+      descendants: [],
+    },
+  ],
+}))();
