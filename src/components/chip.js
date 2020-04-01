b931453ba0b81f74ed8ae2efa2828ab78a4848be
Chris Obdam <>
2020-04-01 16:35:14 +0200

    Add Chip component

---
+++ src/components/chip.js
@@ -0,0 +1,38 @@
+(() => ({
+  name: 'Chip',
+  type: 'CONTENT_COMPONENT',
+  allowedTypes: [],
+  orientation: 'HORIZONTAL',
+  jsx: (() => {
+    const { Chip } = window.MaterialUI.Core;
+    const { label, disabled, variant } = options;
+    const isDev = B.env === 'dev';
+    const chip = (
+      <Chip
+        className={classes.chip}
+        label={label}
+        disabled={disabled}
+        variant={variant}
+      />
+    );
+    return isDev ? <span>{chip}</span> : chip;
+  })(),
+  styles: B => theme => {
+    const style = new B.Styling(theme);
+    const convertSizes = sizes =>
+      sizes.map(size => style.getSpacing(size)).join(' ');
+    return {
+      chip: {
+        margin: ({ options: { margin } }) => convertSizes(margin),
+      },
+      root: {
+        '.&MuiChip-root': {
+          backgroundColor: ({ options: { color } }) => [
+            style.getColor(color),
+            '!important',
+          ],
+        },
+      },
+    };
+  },
+}))();
---
+++ src/prefabs/chip.js
@@ -0,0 +1,51 @@
+(() => ({
+  name: 'Chip',
+  icon: 'ContainerIcon',
+  category: 'CONTENT',
+  structure: [
+    {
+      name: 'Chip',
+      options: [
+        {
+          type: 'VARIABLE',
+          label: 'Label',
+          key: 'label',
+          value: ['Label'],
+        },
+        {
+          type: 'TOGGLE',
+          label: 'Disabled',
+          key: 'disabled',
+          value: false,
+        },
+        {
+          label: 'Variant',
+          key: 'variant',
+          value: 'default',
+          type: 'CUSTOM',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              { name: 'Default', value: 'default' },
+              { name: 'Outlined', value: 'outlined' },
+            ],
+          },
+        },
+        {
+          type: 'COLOR',
+          label: 'Color',
+          key: 'color',
+          value: 'primary',
+        },
+        {
+          type: 'SIZES',
+          label: 'Outer Space',
+          key: 'margin',
+          value: ['M', 'M', 'M', 'M'],
+        },
+      ],
+      descendants: [],
+    },
+  ],
+}))();
