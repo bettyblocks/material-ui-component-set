274d360a571cd8d104e205ab172c2da24a9f5b6c
Anton van den Berg <anton@Antons-MacBook-Pro.local>
2020-04-03 18:21:05 +0200

    Add default list of icons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import * as Core from '@material-ui/core';
-import * as Icons from '@material-ui/icons';
 import * as Lab from '@material-ui/lab';
 import * as Pickers from '@material-ui/pickers';
 import * as Styles from '@material-ui/styles';
 import DateFnsUtils from '@date-io/date-fns';
+import Icons from '../src/icons';
 
 export default { Core, Icons, Lab, Pickers, Styles, DateFnsUtils };
