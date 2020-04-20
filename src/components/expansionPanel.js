2b4771468ee55ecdd25558820ec212aab81d9784
Jeroen Reijs <jeroen.reijs@bettyblocks.com>
2020-04-20 15:52:22 +0200

    feat: add expansion panel properties and remove old panel

--- src/components/expansionPanel.js
+++ src/components/expansionPanel.js
@@ -14,14 +14,39 @@
     const { ExpandMore } = window.MaterialUI.Icons;
     const { useText, env } = B;
     const isDev = env === 'dev';
-    const { title, disabled } = options;
+    const isEmpty = children.length === 0;
+    const isPristine = isEmpty && isDev;
+    const PlaceHolder = (
+      <div
+        className={[
+          isEmpty ? classes.empty : '',
+          isPristine ? classes.pristine : '',
+        ].join(' ')}
+      />
+    );
+    const {
+      title,
+      disabled,
+      defaultExpanded,
+      square,
+      variant,
+      elevation,
+    } = options;
     const titleText = title.map(t => (t.name ? t.name : t)).join(' ');
     const ExpansionPanelComponent = (
-      <ExpansionPanel disabled={disabled}>
+      <ExpansionPanel
+        disabled={disabled}
+        defaultExpanded={defaultExpanded}
+        square={square}
+        variant={variant}
+        elevation={elevation}
+      >
         <ExpansionPanelSummary expandIcon={<ExpandMore />}>
           <Typography>{isDev ? titleText : useText(title)}</Typography>
         </ExpansionPanelSummary>
-        <ExpansionPanelDetails>{children}</ExpansionPanelDetails>
+        <ExpansionPanelDetails>
+          {isEmpty ? PlaceHolder : children}
+        </ExpansionPanelDetails>
       </ExpansionPanel>
     );
 
@@ -31,5 +56,27 @@
       ExpansionPanelComponent
     );
   })(),
-  styles: () => () => ({}),
+  styles: () => () => ({
+    empty: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '4rem',
+      height: '100%',
+      width: '100%',
+      fontSize: '0.75rem',
+      color: '#262A3A',
+      textTransform: 'uppercase',
+      boxSizing: 'border-box',
+    },
+    pristine: {
+      borderWidth: '0.0625rem',
+      borderColor: '#AFB5C8',
+      borderStyle: 'dashed',
+      backgroundColor: '#F0F1F5',
+      '&::after': {
+        content: '"Expansion panel"',
+      },
+    },
+  }),
 }))();
