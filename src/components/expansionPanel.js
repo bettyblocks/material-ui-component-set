24aa8fd200a79b0e18fc6780eda1fbf36a4c99fb
Jeroen Reijs <jeroen.reijs@bettyblocks.com>
2020-04-20 16:19:37 +0200

    refactor: remove unneeded category in component

--- src/components/expansionPanel.js
+++ src/components/expansionPanel.js
@@ -1,6 +1,5 @@
 (() => ({
   name: 'ExpansionPanel',
-  category: 'LAYOUT',
   type: 'CONTAINER_COMPONENT',
   allowedTypes: ['BODY_COMPONENT', 'CONTAINER_COMPONENT', 'CONTENT_COMPONENT'],
   orientation: 'HORIZONTAL',
