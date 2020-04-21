034b362a89629cd52c886440f74325d49142dc47
Chiel Wester <chiel.wester@bettyblocks.com>
2020-04-21 11:10:55 +0200

    Add options to change the width and height of an image

--- src/components/image.js
+++ src/components/image.js
@@ -52,8 +52,8 @@
         paddingBottom: ({ options: { imgUrl } }) => !imgUrl && '62.5%',
       },
       image: {
-        width: '100%',
-        height: 'auto',
+        width: ({options: { width } }) => width ? width : 'auto',
+        height: ({options: { height } }) => height ? height : 'auto',
       },
       placeholderWrapper: {
         position: 'absolute',
