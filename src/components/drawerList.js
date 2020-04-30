d2b9f1afe349cdfeccf70099628e0b086b9f3f2b
Bryan Oemar <bryan.oemar@gmail.com>
2020-05-01 01:01:25 +0200

    [List component] working list component very much hard coded yet

--- src/components/drawerList.js
+++ src/components/drawerList.js
@@ -1,39 +1,65 @@
 (() => ({
   name: 'DrawerList',
   icon: 'PanelIcon',
-  type: 'DRAWER_LIST_COMPONENT',
+  type: 'CONTENT_COMPONENT',
   allowedTypes: ['BODY_COMPONENT', 'CONTENT_COMPONENT', "DRAWER_LIST_ITEM_COMPONENT"],
   orientation: 'HORIZONTAL',
   jsx: (<div>
     {(() => {
-      return <div><p style={{marginLeft: '10px'}}> Drawer list component</p>{children}</div>
+      const { env } = B;
+      const isDev = env === "dev";
+
+      const { makeStyles,
+              List,
+              ListItem,
+              Divider,
+              ListItemText,
+              ListItemIcon } = window.MaterialUI.Core;
+
+      const { Inbox: InboxIcon, 
+              Drafts: DraftsIcon } = window.MaterialUI.Icons;
+
+      const ListItemLink =  props => <ListItem button component="a" {...props} />;
+
+      const list =  (
+        <div className={classes.root}>
+          <List component="nav" aria-label="main mailbox folders">
+            <ListItem button>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItem>
+          </List>
+          <Divider />
+          <List component="nav" aria-label="secondary mailbox folders">
+            <ListItem button>
+              <ListItemText primary="Trash" />
+            </ListItem>
+            <ListItemLink href="#simple-list">
+              <ListItemText primary="Spam" />
+            </ListItemLink>
+          </List>
+        </div>
+      );
+
+      return isDev ? 'List' : list
     })()} 
   </div>
   ),
   styles: B => theme => {
     return {
-      list: {
-        width: 250,
+      root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: "white"
       },
-      fullList: {
-        width: 'auto',
-      },
-      empty: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: () => 'center',
-        minHeight: '4rem',
-        height: '100%',
-        margin: '1rem',
-        fontSize: '0.75rem',
-      },
-      pristine: {
-        color: '#262A3A',
-        textTransform: 'uppercase',
-        boxSizing: 'border-box',
-        border: '0.0625rem dashed #AFB5C8',
-        backgroundColor: '#F0F1F5',
-      },
-    };
+    }
   },
 }))();
