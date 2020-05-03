3b1faba7c569351216dcfef193f80eefca3f50be
Bryan Oemar <bryan.oemar@gmail.com>
2020-05-03 22:37:07 +0200

    cleanup

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -61,53 +61,10 @@
           setState({ ...state, [anchor]: open });
         };
 
-        const list = (anchor) => (
-          <div
-            className={clsx(classes.list, {
-              [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-            })}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-          >
-            <List>
-              {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>{index % 2 === 0 ? <Inbox /> : <Mail />}</ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-              ))}
-            </List>
-            <Divider />
-            <List>
-              {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>{index % 2 === 0 ? <Inbox /> : <Mail />}</ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-              ))}
-            </List>
-          </div>
-        );
-
-        const drawerComponent = (
-          <div>
-            {
-              <React.Fragment key={anchor}>
-                <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-                <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
-                  {list(anchor)}
-                </Drawer>
-              </React.Fragment>
-            }
-          </div>
-        );
-
         const drawerComponentWithList = (
           <div>
             {
               <React.Fragment key={anchor}>
-                <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
                 <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
                   <div
                     className={clsx(classes.list, {
