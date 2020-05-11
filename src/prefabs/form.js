bbfa8a4f3f0a4f7d75139692c3c1b92d6d6b8153
Jeroen Reijs <jeroen.reijs@bettyblocks.com>
2020-05-11 16:28:32 +0200

    Revert "Feat/add custom model generation to form ta 1413"

--- src/prefabs/form.js
+++ src/prefabs/form.js
@@ -20,19 +20,6 @@
           label: 'Model',
           key: 'model',
           type: 'MODEL',
-          configuration: {
-            modal: {
-              type: 'MODEL_AND_PROPERTIES',
-              generateCustomModel: true,
-              modelRequired: false,
-            },
-          },
-        },
-        {
-          value: '',
-          label: 'Custom Model',
-          key: 'customModel',
-          type: 'CUSTOM_MODEL',
         },
         {
           value: {},
