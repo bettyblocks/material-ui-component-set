e9b39e1b187dbb304412862f291e596bab246341
Anton van den Berg <anton@Antons-MacBook-Pro.local>
2020-05-14 20:03:34 +0200

    add size, color and spacing

--- src/components/icon.js
+++ src/components/icon.js
@@ -20,6 +20,8 @@
   })(),
   styles: B => t => {
     const style = new B.Styling(t);
+    const getSpacing = (idx, device = 'Mobile') =>
+      idx === '0' ? '0rem' : style.getSpacing(idx, device);
 
     return {
       wrapper: {
@@ -27,17 +29,45 @@
       },
       root: {
         '&.MuiSvgIcon-root': {
+          fontSize: ({ options: { size } }) => style.getIconSize(size),
+          color: ({ options: { color } }) => style.getColor(color),
+          marginTop: ({ options: { outerSpacing } }) =>
+            getSpacing(outerSpacing[0]),
+          marginRight: ({ options: { outerSpacing } }) =>
+            getSpacing(outerSpacing[1]),
+          marginBottom: ({ options: { outerSpacing } }) =>
+            getSpacing(outerSpacing[2]),
+          marginLeft: ({ options: { outerSpacing } }) =>
+            getSpacing(outerSpacing[3]),
           [`@media ${B.mediaMinWidth(768)}`]: {
-            fontSize: ({ options: { type } }) =>
-              style.getFontSize(type, 'Portrait'),
+            marginTop: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[0], 'Portrait'),
+            marginRight: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[1], 'Portrait'),
+            marginBottom: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[2], 'Portrait'),
+            marginLeft: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[3], 'Portrait'),
           },
           [`@media ${B.mediaMinWidth(1024)}`]: {
-            fontSize: ({ options: { type } }) =>
-              style.getFontSize(type, 'Landscape'),
+            marginTop: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[0], 'Landscape'),
+            marginRight: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[1], 'Landscape'),
+            marginBottom: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[2], 'Landscape'),
+            marginLeft: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[3], 'Landscape'),
           },
           [`@media ${B.mediaMinWidth(1200)}`]: {
-            fontSize: ({ options: { type } }) =>
-              style.getFontSize(type, 'Desktop'),
+            marginTop: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[0], 'Desktop'),
+            marginRight: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[1], 'Desktop'),
+            marginBottom: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[2], 'Desktop'),
+            marginLeft: ({ options: { outerSpacing } }) =>
+              getSpacing(outerSpacing[3], 'Desktop'),
           },
         },
       },
