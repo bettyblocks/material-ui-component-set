749ab1b0e669502d6f2ffff47e8106c4850b3fc4
Anton van den Berg <anton@Antons-MacBook-Pro.local>
2020-06-09 11:13:05 +0200

    refactor: remove unused option

--- src/prefabs/drawerContainer.js
+++ src/prefabs/drawerContainer.js
@@ -12,12 +12,6 @@
           key: 'themeBgColor',
           value: 'Transparent',
         },
-        {
-          type: 'TEXT',
-          label: 'Background color overwrite',
-          key: 'bgColor',
-          value: '',
-        },
       ],
       descendants: [],
     },
