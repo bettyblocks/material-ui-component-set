3db217288d8fcc1a9fcdcb99f05ec8c748633ad4
Mahad <mahad.walusimbi@bettyblocks.com>
2020-06-12 15:07:02 +0200

    feat: add sample prefabs to the dialog

--- src/components/dialog.js
+++ src/components/dialog.js
@@ -4,7 +4,7 @@
   allowedTypes: ['BODY_COMPONENT', 'CONTAINER_COMPONENT', 'CONTENT_COMPONENT'],
   orientation: 'VERTICAL',
   jsx: (() => {
-    const { isVisible, isFullscreen } = options;
+    const { isVisible, isFullscreen, width } = options;
     const { Dialog } = window.MaterialUI.Core;
     const isDev = B.env === 'dev';
     const [isOpen, setIsOpen] = useState(isDev);
@@ -36,6 +36,8 @@
         open={isOpen}
         onClose={closeDialog}
         fullScreen={isFullscreen}
+        fullWidth
+        maxWidth={width}
         aria-labelledby="modal-dialog"
       >
         {children}
@@ -50,52 +52,69 @@
       DialogCmp
     );
   })(),
-  styles: () => () => ({
-    root: {
-      display: 'block',
-    },
-    overlay: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      position: 'absolute',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      top: '0.25rem',
-      left: '0.25rem',
-      bottom: '0.25rem',
-      right: '0.25rem',
-      zIndex: 2,
-      boxSizing: 'border-box',
-    },
-    empty: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: '1rem',
-      fontSize: '0.75rem',
-      color: '#262A3A',
-      textTransform: 'uppercase',
-      borderWidth: '0.0625rem',
-      borderColor: '#AFB5C8',
-      borderStyle: 'dashed',
-      backgroundColor: '#F0F1F5',
-    },
-    dirty: {
-      backgroundColor: '#fff',
-    },
-    dialog: {
-      display: 'flex',
-      flexDirection: 'column',
-      overflowY: 'auto',
-    },
-    fullScreen: {
-      width: '100%',
-      height: '100%',
-    },
-    windowed: {
-      borderRadius: '0.25rem',
-      maxWidth: '37.5rem',
-      margin: '2rem',
-      maxHeight: 'calc(100% - 4rem);',
-    },
-  }),
+  styles: () => () => {
+    const { useTheme } = window.MaterialUI.Core;
+    const theme = useTheme();
+    return {
+      root: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: '0.0625rem',
+        borderColor: '#AFB5C8',
+        borderStyle: 'dashed',
+        backgroundColor: '#F0F1F5',
+        minHeight: '2rem',
+        width: '100%',
+        fontSize: '0.75rem',
+        color: '#262A3A',
+        textTransform: 'uppercase',
+        boxSizing: 'border-box',
+      },
+      overlay: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        top: '0.25rem',
+        left: '0.25rem',
+        bottom: '0.25rem',
+        right: '0.25rem',
+        zIndex: 2,
+        boxSizing: 'border-box',
+      },
+      empty: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '1rem',
+        fontSize: '0.75rem',
+        color: '#262A3A',
+        textTransform: 'uppercase',
+        borderWidth: '0.0625rem',
+        borderColor: '#AFB5C8',
+        borderStyle: 'dashed',
+        backgroundColor: '#F0F1F5',
+      },
+      dirty: {
+        backgroundColor: '#fff',
+      },
+      dialog: {
+        display: 'flex',
+        flexDirection: 'column',
+        overflowY: 'auto',
+      },
+      fullScreen: {
+        width: '100%',
+        height: '100%',
+      },
+      windowed: {
+        borderRadius: '0.25rem',
+        width: ({ options: { width } }) =>
+          Math.max(theme.breakpoints.values[width], 444),
+        margin: '2rem',
+        maxHeight: 'calc(100% - 4rem);',
+      },
+    };
+  },
 }))();
