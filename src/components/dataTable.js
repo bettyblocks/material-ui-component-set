a75608d76ba9d4074255cd57741889a666532ebe
Thomas van Ophem <thomas.vanophem@gmail.com>
2020-07-14 20:31:06 +0200

    fix: replace for..in loop with Object.keys and normal forloop

--- src/components/dataTable.js
+++ src/components/dataTable.js
@@ -309,13 +309,15 @@
       const source = sources.shift();
 
       if (isObject(target) && isObject(source)) {
-        // eslint-disable-next-line no-restricted-syntax
-        for (const key in source) {
-          if (isObject(source[key])) {
+        const keys = Object.keys(source);
+        for (let index = 0; index < keys.length; index += 1) {
+          const key = keys[index];
+          const value = source[key];
+          if (isObject(value)) {
             if (!target[key]) Object.assign(target, { [key]: {} });
-            deepMerge(target[key], source[key]);
+            deepMerge(target[key], value);
           } else {
-            Object.assign(target, { [key]: source[key] });
+            Object.assign(target, { [key]: value });
           }
         }
       }
@@ -337,6 +339,7 @@
       searchProperty && search !== ''
         ? deepMerge({}, filter, searchFilter)
         : filter;
+    debugger;
     const where = useFilter(newFilter);
 
     return (
