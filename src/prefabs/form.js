e4d5765930c2c77cb875d50a438be115e0c7ee66
robin <robin.mulder@bettyblocks.com>
2020-08-17 15:01:30 +0200

    fix: remove the model modal config in the form prefab

--- src/prefabs/form.js
+++ src/prefabs/form.js
@@ -34,13 +34,6 @@
           label: 'Model',
           key: 'model',
           type: 'MODEL',
-          configuration: {
-            modal: {
-              type: 'MODEL_AND_PROPERTIES',
-              generateCustomModel: true,
-              modelRequired: false,
-            },
-          },
         },
         {
           value: '',
