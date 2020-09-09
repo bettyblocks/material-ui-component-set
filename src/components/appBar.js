f9b2e5b5e9c92087638ac1c3a23fe72168689767
Angelo de Voer <angelo_de_voer@hotmail.com>
2020-09-09 11:44:09 +0200

    Changed to title to const title

--- src/components/appBar.js
+++ src/components/appBar.js
@@ -26,7 +26,7 @@
     const isDev = env === 'dev';
     const [anchorEl, setAnchorEl] = useState(null);
     const open = !!anchorEl;
-    title = useText(title);
+    const titleText = useText(title);
 
     const handleMenu = event => {
       setAnchorEl(event.currentTarget);
@@ -62,7 +62,7 @@
             component={endpoint.id && Link}
             endpoint={endpoint.id && endpoint}
           >
-            {title}
+            {titleText}
           </Typography>
           <div className={classes.spacer} />
           {!isDev && !!children.length ? (
