3335bb15f8b9d7243730a31556c9d4ab10f8bd3b
Menouer Nobach <menouer.nobach@bettyblocks.com>
2020-09-15 10:15:54 +0200

    feat/meContainer

---
+++ src/components/meContainer.js
@@ -0,0 +1,122 @@
+(() => ({
+  name: 'MeContainer',
+  type: 'CONTAINER_COMPONENT',
+  allowedTypes: ['BODY_COMPONENT', 'CONTAINER_COMPONENT', 'CONTENT_COMPONENT'],
+  orientation: 'HORIZONTAL',
+  jsx: (
+    <div>
+      {(() => {
+        const isEmpty = children.length === 0;
+        const isDev = B.env === 'dev';
+        const isPristine = isEmpty && isDev;
+        const { authenticationProfile } = options;
+        const builderLayout = () => (
+          <>
+            <div
+              className={[
+                isEmpty ? classes.empty : '',
+                isPristine ? classes.pristine : '',
+              ].join(' ')}
+            >
+              {isPristine ? 'Me Container' : children}
+            </div>
+          </>
+        );
+        const canvasLayout = () => {
+          if (!authenticationProfile) {
+            return builderLayout();
+          }
+          return (
+            <B.GetMe authenticationProfileId={authenticationProfile}>
+              {({ loading, error }) => {
+                if (loading) {
+                  return <span>Loading...</span>;
+                }
+                if (error) {
+                  return <span>{error.message}</span>;
+                }
+                return children;
+              }}
+            </B.GetMe>
+          );
+        };
+        return isDev ? builderLayout() : canvasLayout();
+      })()}
+    </div>
+  ),
+  styles: () => () => ({
+    header: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      flexDirection: 'row-reverse',
+      minHeight: '4rem',
+      width: '100%',
+    },
+    searchWrapper: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: [0, '0.5rem'],
+      borderBottom: [1, 'solid', '#000'],
+    },
+    searchIcon: {
+      fontSize: '1.25rem',
+      marginRight: '1rem',
+    },
+    search: {
+      padding: ['0.25rem', 0],
+      fontSize: '1rem',
+      border: 'none',
+      outline: 'none',
+    },
+    button: {
+      background: 'transparent',
+      border: 'none',
+      display: 'inline-block',
+      padding: 0,
+      margin: 0,
+      cursor: 'pointer',
+      '&:active': {
+        outline: 'none',
+      },
+    },
+    footer: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: ['0.75rem', 0],
+    },
+    placeholder: {
+      opacity: '0.4',
+    },
+    pagination: {
+      marginLeft: '1rem',
+    },
+    arrow: {
+      padding: '1rem',
+      fontSize: '1.625rem',
+      color: '#000',
+      textDecoration: 'none',
+    },
+    arrowDisabled: { color: '#ccc' },
+    empty: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '4rem',
+      height: '100%',
+      width: '100%',
+      fontSize: '0.75rem',
+      color: '#262A3A',
+      textTransform: 'uppercase',
+      boxSizing: 'border-box',
+    },
+    pristine: {
+      borderWidth: '0.0625rem',
+      borderColor: '#AFB5C8',
+      borderStyle: 'dashed',
+      backgroundColor: '#F0F1F5',
+    },
+  }),
+}))();
---
+++ src/prefabs/meContainer.js
@@ -0,0 +1,19 @@
+(() => ({
+  name: 'MeContainer',
+  icon: 'DataContainer',
+  category: 'DATA',
+  structure: [
+    {
+      name: 'MeContainer',
+      options: [
+        {
+          value: '',
+          label: 'Authentication profile',
+          key: 'authenticationProfile',
+          type: 'AUTHENTICATION_PROFILE',
+        },
+      ],
+      descendants: [],
+    },
+  ],
+}))();
