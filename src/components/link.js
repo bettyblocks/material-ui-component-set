93cf681f6662bb099d2ee1b6b3367f7ba1f61ba6
ReinierVanAltena <63792435+ReinierVanAltena@users.noreply.github.com>
2020-10-05 16:14:27 +0200

    linter

--- src/components/link.js
+++ src/components/link.js
@@ -26,52 +26,45 @@
         href={
           linkType === 'external' && hasExternalLink
             ? linkToExternal
-            : undefined
+            : 'https://google.com'
         }
         component={linkType === 'internal' && hasLink ? B.Link : undefined}
         endpoint={linkType === 'internal' && hasLink ? linkTo : undefined}
         color={color}
         underline={underline}
       >
-        {isPristine ? (
+        {isPristine && (
           <div className={[classes.pristine, classes.empty].join(' ')}>
             Link
           </div>
-        ) : isEmpty ? (
-          linkName
-        ) : (
-          children
-        )}
+        )}{' '}
+        {isEmpty ? linkName : children}
       </Link>
     );
 
     return isDev ? <div>{LinkComponent}</div> : LinkComponent;
   })(),
-  styles: B => t => {
-    const style = new B.Styling(t);
-
-    return {
-      root: {
-        display: 'inline-flex',
-        justifyContent: 'center',
-        verticalAlign: 'middle',
-      },
-      pristine: {
-        borderWidth: '0.0625rem',
-        borderColor: '#AFB5C8',
-        borderStyle: 'dashed',
-        backgroundColor: '#F0F1F5',
-      },
-      empty: {
-        display: ['flex', '!important'],
-        justifyContent: ['center', '!important'],
-        alignItems: 'center',
-        height: ['2.5rem', '!important'],
-        fontSize: '0.75rem',
-        color: '#262A3A',
-        textTransform: 'uppercase',
-        width: '100%',
-      },
-    };
-  },
+  styles: () => ({
+    root: {
+      display: 'inline-flex',
+      justifyContent: 'center',
+      verticalAlign: 'middle',
+    },
+    pristine: {
+      borderWidth: '0.0625rem',
+      borderColor: '#AFB5C8',
+      borderStyle: 'dashed',
+      backgroundColor: '#F0F1F5',
+    },
+    empty: {
+      display: ['flex', '!important'],
+      justifyContent: ['center', '!important'],
+      alignItems: 'center',
+      height: ['2.5rem', '!important'],
+      fontSize: '0.75rem',
+      color: '#262A3A',
+      textTransform: 'uppercase',
+      width: '100%',
+    },
+  }),
 }))();
