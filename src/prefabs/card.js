3f49363734cd9487be1c932e4ff63232ecbc083a
Dylan <dylc2770@gmail.com>
2020-10-13 10:18:09 +0200

    fix: change text to variables in card, cardActions and dialog

--- src/prefabs/card.js
+++ src/prefabs/card.js
@@ -373,10 +373,10 @@
                   },
                 },
                 {
-                  value: '',
+                  value: [''],
                   label: 'URL',
                   key: 'linkToExternal',
-                  type: 'TEXT',
+                  type: 'VARIABLE',
                   configuration: {
                     placeholder: 'Starts with https:// or http://',
                     condition: {
