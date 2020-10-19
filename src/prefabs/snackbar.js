edd8f18e514b71c593986c8fd947565a9b893a78
Jeroen Reijs <jeroen.reijs@bettyblocks.com>
2020-10-19 10:15:03 +0200

    feat: add snackbar component

---
+++ src/prefabs/snackbar.js
@@ -0,0 +1,84 @@
+(() => ({
+  name: 'Snackbar',
+  icon: 'AlertIcon',
+  category: 'CONTENT',
+  structure: [
+    {
+      name: 'Snackbar',
+      options: [
+        {
+          type: 'TOGGLE',
+          label: 'Visible',
+          key: 'visible',
+          value: true,
+        },
+        {
+          label: 'Vertical position',
+          key: 'anchorOriginVertical',
+          value: 'bottom',
+          type: 'CUSTOM',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: 'Top',
+                value: 'top',
+              },
+              {
+                name: 'Bottom',
+                value: 'bottom',
+              },
+            ],
+          },
+        },
+        {
+          label: 'Horizontal position',
+          key: 'anchorOriginHorizontal',
+          value: 'center',
+          type: 'CUSTOM',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: 'Left',
+                value: 'left',
+              },
+              {
+                name: 'Center',
+                value: 'center',
+              },
+              {
+                name: 'Right',
+                value: 'right',
+              },
+            ],
+          },
+        },
+        {
+          type: 'TOGGLE',
+          label: 'Auto hide',
+          key: 'autoHide',
+          value: true,
+        },
+
+        {
+          type: 'NUMBER',
+          label: 'Auto Hide Duration',
+          key: 'autoHideDuration',
+          value: 6000,
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'autoHide',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+      ],
+      descendants: [],
+    },
+  ],
+}))();
