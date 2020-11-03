f9add32b182455b36dbd5373c51b7563a94f4cbf
Dylan Carver <dylc2770@gmail.com>
2020-11-03 11:22:40 +0100

    feat: add consistent container spacing options
    * feat: add dynamic inner spacing to panel
    
    * feat: add dynamic spacing to drawer sidebar
    
    * feat: add media widths to panel padding
    
    * feat: add media widths to drawerbar spacing
    
    * feat: add dynamic inner space to drawerContainer
    
    * feat: add dynamic spacing options to expantionPanel
    
    * feat: add outerSpacing to drawerContainer
    
    * fix: replace fragment with wrapper div

--- src/prefabs/expansionPanel.js
+++ src/prefabs/expansionPanel.js
@@ -81,6 +81,18 @@
             },
           },
         },
+        {
+          value: ['0rem', '0rem', '0rem', '0rem'],
+          label: 'Outer space',
+          key: 'outerSpacing',
+          type: 'SIZES',
+        },
+        {
+          value: ['M', 'M', 'M', 'M'],
+          label: 'Inner space',
+          key: 'innerSpacing',
+          type: 'SIZES',
+        },
       ],
       descendants: [],
     },
