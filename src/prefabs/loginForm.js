387284b2356164453b6d54792fb09eb1e7ee8365
Stefan van Delft <stefan.van.delft@bettyblocks.com>
2020-12-30 12:51:23 +0100

    chore(ART-2902): Add title to login prefab.
    The title component is pre-configured and concats the other descendants
    so it is on top when renderd.

--- src/prefabs/loginForm.js
+++ src/prefabs/loginForm.js
@@ -3851,9 +3851,104 @@
                 }
               });
 
-              newPrefab.structure[0].descendants = descendantsArray.concat(
-                newPrefab.structure[0].descendants,
-              );
+
+              const titleDescendant = () => ([
+                     {
+                    name: 'Text',
+                    options: [
+                      {
+                        type: 'VARIABLE',
+                        label: 'Content',
+                        key: 'content',
+                        value: ["Login"],
+                        configuration: {
+                          as: 'MULTILINE',
+                        },
+                      },
+                      {
+                        value: 'Title2',
+                        label: 'Type',
+                        key: 'type',
+                        type: 'FONT',
+                      },
+                      {
+                        type: 'CUSTOM',
+                        label: 'Text Alignment',
+                        key: 'textAlignment',
+                        value: 'left',
+                        configuration: {
+                          as: 'BUTTONGROUP',
+                          dataType: 'string',
+                          allowedInput: [
+                            { name: 'Left', value: 'left' },
+                            { name: 'Center', value: 'center' },
+                            { name: 'Right', value: 'right' },
+                          ],
+                        },
+                      },
+                      {
+                        value: ['0rem', '0rem', '0rem', '0rem'],
+                        label: 'Outer space',
+                        key: 'outerSpacing',
+                        type: 'SIZES',
+                      },
+                      {
+                        value: false,
+                        label: 'Styles',
+                        key: 'styles',
+                        type: 'TOGGLE',
+                      },
+                      {
+                        type: 'COLOR',
+                        label: 'Text color',
+                        key: 'textColor',
+                        value: 'Black',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        type: 'CUSTOM',
+                        label: 'Font weight',
+                        key: 'fontWeight',
+                        value: '400',
+                        configuration: {
+                          as: 'DROPDOWN',
+                          dataType: 'string',
+                          allowedInput: [
+                            { name: '100', value: '100' },
+                            { name: '200', value: '200' },
+                            { name: '300', value: '300' },
+                            { name: '400', value: '400' },
+                            { name: '500', value: '500' },
+                            { name: '600', value: '600' },
+                            { name: '700', value: '700' },
+                            { name: '800', value: '800' },
+                            { name: '900', value: '900' },
+                          ],
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                    ],
+                    descendants: [],
+                  }]);
+
+              newPrefab.structure[0].descendants =
+                titleDescendant().concat(
+                    descendantsArray.concat(
+                      newPrefab.structure[0].descendants,
+                  ),
+                );
 
               save(newPrefab);
             }
