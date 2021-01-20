c80938f47b3b7615ee387196e9f3d59e3bc46ee7
kagia <benjamin.kagia@bettyblocks.com>
2021-01-20 19:06:22 +0300

    feat: add redirect to logout

--- src/interactions/logout.ts
+++ src/interactions/logout.ts
@@ -1,4 +1,17 @@
-function Logout(): void {
+interface Page {
+  name: string;
+  url: string;
+}
+
+function Logout({
+  event,
+  redirectTo,
+}: {
+  event: Event;
+  redirectTo: Page;
+}): void {
+  const { name, url } = redirectTo;
   localStorage.removeItem('TOKEN');
   localStorage.removeItem('REFRESH_TOKEN');
+  window.location.href = url;
 }
