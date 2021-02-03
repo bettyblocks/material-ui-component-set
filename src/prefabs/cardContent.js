48a24d4bf1ffa8116e9f7dcf1789de24d4957f4e
Flaviu <flaviu.mariean@bettyblocks.com>
2021-02-03 12:48:42 +0200

    chore(ta-2230): added link option to title and text components

--- src/prefabs/cardContent.js
+++ src/prefabs/cardContent.js
@@ -46,6 +46,49 @@
               key: 'outerSpacing',
               type: 'SIZES',
             },
+            {
+              type: 'CUSTOM',
+              label: 'Link to',
+              key: 'linkType',
+              value: 'internal',
+              configuration: {
+                as: 'BUTTONGROUP',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Internal page', value: 'internal' },
+                  { name: 'External page', value: 'external' },
+                ],
+              },
+            },
+            {
+              value: '',
+              label: 'Page',
+              key: 'linkTo',
+              type: 'ENDPOINT',
+              configuration: {
+                condition: {
+                  type: 'SHOW',
+                  option: 'linkType',
+                  comparator: 'EQ',
+                  value: 'internal',
+                },
+              },
+            },
+            {
+              value: [''],
+              label: 'URL',
+              key: 'linkToExternal',
+              type: 'VARIABLE',
+              configuration: {
+                placeholder: 'Starts with https:// or http://',
+                condition: {
+                  type: 'SHOW',
+                  option: 'linkType',
+                  comparator: 'EQ',
+                  value: 'external',
+                },
+              },
+            },
           ],
           descendants: [],
         },
@@ -87,6 +130,49 @@
               label: 'Outer space',
               key: 'outerSpacing',
               type: 'SIZES',
+            },
+            {
+              type: 'CUSTOM',
+              label: 'Link to',
+              key: 'linkType',
+              value: 'internal',
+              configuration: {
+                as: 'BUTTONGROUP',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Internal page', value: 'internal' },
+                  { name: 'External page', value: 'external' },
+                ],
+              },
+            },
+            {
+              value: '',
+              label: 'Page',
+              key: 'linkTo',
+              type: 'ENDPOINT',
+              configuration: {
+                condition: {
+                  type: 'SHOW',
+                  option: 'linkType',
+                  comparator: 'EQ',
+                  value: 'internal',
+                },
+              },
+            },
+            {
+              value: [''],
+              label: 'URL',
+              key: 'linkToExternal',
+              type: 'VARIABLE',
+              configuration: {
+                placeholder: 'Starts with https:// or http://',
+                condition: {
+                  type: 'SHOW',
+                  option: 'linkType',
+                  comparator: 'EQ',
+                  value: 'external',
+                },
+              },
             },
           ],
           descendants: [],
