d8c7a0c267af72a62c3276d1d5dbf374a1ffc814
garethloot <32203526+garethloot@users.noreply.github.com>
2021-02-05 10:31:51 +0100

    feat: make sure submit interaction also validates inputs

--- src/components/form.js
+++ src/components/form.js
@@ -47,10 +47,7 @@
         const mounted = useRef(false);
 
         B.defineFunction('Submit', () => {
-          if (formRef.current)
-            formRef.current.dispatchEvent(
-              new Event('submit', { cancelable: true }),
-            );
+          if (formRef.current) formRef.current.requestSubmit();
         });
 
         useEffect(() => {
