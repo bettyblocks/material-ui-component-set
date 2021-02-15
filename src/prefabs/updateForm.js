0a31861bc1ffe7a708684e61998db71571378af6
Emiel Wit <emiel.wit@bettyblocks.com>
2021-02-15 11:34:44 +0100

    fix: replace model param with query

--- src/prefabs/updateForm.js
+++ src/prefabs/updateForm.js
@@ -15,11 +15,16 @@
       PropertiesSelector,
       Text,
     },
+    helpers: { useModelQuery },
   }) => {
     const [modelId, setModelId] = React.useState('');
-    const [model, setModel] = React.useState(null);
     const [properties, setProperties] = React.useState([]);
     const [showValidation, setShowValidation] = React.useState(false);
+
+    const { data } = useModelQuery({
+      variables: { id: modelId },
+      skip: !modelId,
+    });
 
     React.useEffect(() => {
       setProperties([]);
@@ -36,10 +41,9 @@
             }
           >
             <ModelSelector
-              onChange={(id, modelObject) => {
+              onChange={id => {
                 setShowValidation(false);
                 setModelId(id);
-                setModel(modelObject);
               }}
               value={modelId}
             />
@@ -100,8 +104,8 @@
             }
 
             const newPrefab = { ...prefab };
-            if (model) {
-              newPrefab.variables[1].name = camelToSnakeCase(model.label);
+            if (data && data.model) {
+              newPrefab.variables[1].name = camelToSnakeCase(data.model.label);
             }
             newPrefab.structure[0].options[0].value.modelId = modelId;
             newPrefab.structure[0].options[1].value = modelId;
