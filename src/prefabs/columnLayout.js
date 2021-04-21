7202f3463eb765f7e777eebcf7f38a56839f9bdb
sventruschel <sven.truschel@bettyblocks.com>
2021-04-21 11:29:57 +0200

    chore: added responsiveness and fixed types

--- src/prefabs/columnLayout.js
+++ src/prefabs/columnLayout.js
@@ -1,8 +1,6 @@
 (() => ({
   name: 'Column Layout',
   icon: 'GridIcon',
-  description:
-    'With this component you can select the amount of rows and the number of columns for each row',
   category: 'LAYOUT',
   beforeCreate: ({
     close,
@@ -21,7 +19,7 @@
     prefab,
     save,
   }) => {
-    const [rows, setRows] = React.useState([{ index: 1, columns: '2' }]);
+    const [rows, setRows] = React.useState([{ index: 1, columns: 2 }]);
 
     const createElements = n => {
       const elements = [];
@@ -55,19 +53,21 @@
               <Field
                 info={
                   <Text size="small" color="grey700">
-                    {`Click the Add rows button to add a new row to the page.
-                    You can specify the amount of columns per row`}
+                    Click the <b>+ Add row</b> button to add a new row to the
+                    page.
+                    <br />
+                    You can specify the amount of columns per row
                   </Text>
                 }
               >
                 <Button
-                  label="Add row"
+                  label="+ Add row"
                   disabled={!maxRows}
                   onClick={() => {
                     if (maxRows) {
                       setRows([
                         ...rows,
-                        { index: rows.length + 1, columns: '1' },
+                        { index: rows.length + 1, columns: 1 },
                       ]);
                     }
                   }}
@@ -91,7 +91,7 @@
                             currentRow => currentRow.index === row.index,
                           );
                           const updatedRows = rows;
-                          updatedRows[index].columns = value;
+                          updatedRows[index].columns = parseInt(value, 10);
                           setRows([...updatedRows]);
                         }}
                         value={row.columns.toString()}
@@ -177,6 +177,14 @@
               ))}
             </Box>
           </Box>
+          <Box pad={{ top: '2rem' }}>
+            <Text color="#666d85" size="small">
+              Note: On smaller screens the preview may differ from what you see
+              on the canvas.
+              <br />
+              It should be the same when you build the page
+            </Text>
+          </Box>
         </Content>
         <Footer
           onClose={close}
@@ -229,6 +237,32 @@
               };
 
               for (let index = 0; index < row.columns; index += 1) {
+                let widthArray = [];
+                switch (row.columns) {
+                  case 2:
+                    widthArray = ['6', '6', '12', '12'];
+                    break;
+                  case 3:
+                    widthArray = ['4', '4', '12', '12'];
+                    break;
+                  case 4:
+                    widthArray = ['3', '6', '6', '12'];
+                    break;
+                  case 5:
+                    widthArray = ['flexible', 'flexible', '6', '12'];
+                    break;
+                  case 6:
+                    widthArray = ['2', '2', '6', '6'];
+                    break;
+                  default:
+                    widthArray = [
+                      'flexible',
+                      'flexible',
+                      'flexible',
+                      'flexible',
+                    ];
+                    break;
+                }
                 newRow.descendants.push({
                   name: 'Column',
                   options: [
@@ -242,7 +276,7 @@
                       },
                     },
                     {
-                      value: 'flexible',
+                      value: widthArray[0],
                       label: 'Column width',
                       key: 'columnWidth',
                       type: 'CUSTOM',
@@ -269,7 +303,7 @@
                       },
                     },
                     {
-                      value: 'flexible',
+                      value: widthArray[1],
                       label: 'Column width (tablet landscape)',
                       key: 'columnWidthTabletLandscape',
                       type: 'CUSTOM',
@@ -296,7 +330,7 @@
                       },
                     },
                     {
-                      value: 'flexible',
+                      value: widthArray[2],
                       label: 'Column width (tablet portrait)',
                       key: 'columnWidthTabletPortrait',
                       type: 'CUSTOM',
@@ -323,7 +357,7 @@
                       },
                     },
                     {
-                      value: 'flexible',
+                      value: widthArray[3],
                       label: 'Column width (mobile)',
                       key: 'columnWidthMobile',
                       type: 'CUSTOM',
@@ -412,7 +446,7 @@
                   descendants: [],
                 });
               }
-              newPrefab.structure[0].descendants.push(newRow);
+              newPrefab.structure[0].descendants[0].descendants.push(newRow);
             });
             save(newPrefab);
           }}
@@ -420,6 +454,7 @@
       </>
     );
   },
+  interactions: [],
   structure: [
     {
       name: 'Row',
@@ -463,7 +498,190 @@
           type: 'SIZES',
         },
       ],
-      descendants: [],
+      descendants: [
+        {
+          name: 'Column',
+          options: [
+            {
+              label: 'Toggle visibility',
+              key: 'visible',
+              value: true,
+              type: 'TOGGLE',
+              configuration: {
+                as: 'VISIBILITY',
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width',
+              key: 'columnWidth',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width (tablet landscape)',
+              key: 'columnWidthTabletLandscape',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width (tablet portrait)',
+              key: 'columnWidthTabletPortrait',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width (mobile)',
+              key: 'columnWidthMobile',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: '',
+              label: 'Height',
+              key: 'columnHeight',
+              type: 'TEXT',
+              configuration: {
+                as: 'UNIT',
+              },
+            },
+            {
+              value: 'transparent',
+              label: 'Background color',
+              key: 'backgroundColor',
+              type: 'COLOR',
+            },
+            {
+              type: 'CUSTOM',
+              label: 'Horizontal Alignment',
+              key: 'horizontalAlignment',
+              value: 'inherit',
+              configuration: {
+                as: 'BUTTONGROUP',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'None', value: 'inherit' },
+                  { name: 'Left', value: 'flex-start' },
+                  { name: 'Center', value: 'center' },
+                  { name: 'Right', value: 'flex-end' },
+                ],
+              },
+            },
+            {
+              type: 'CUSTOM',
+              label: 'Vertical Alignment',
+              key: 'verticalAlignment',
+              value: 'inherit',
+              configuration: {
+                as: 'BUTTONGROUP',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'None', value: 'inherit' },
+                  { name: 'Top', value: 'flex-start' },
+                  { name: 'Center', value: 'center' },
+                  { name: 'Bottom', value: 'flex-end' },
+                ],
+              },
+            },
+            {
+              value: ['0rem', '0rem', '0rem', '0rem'],
+              label: 'Outer space',
+              key: 'outerSpacing',
+              type: 'SIZES',
+            },
+            {
+              value: ['M', 'M', 'M', 'M'],
+              label: 'Inner space',
+              key: 'innerSpacing',
+              type: 'SIZES',
+            },
+          ],
+          descendants: [],
+        },
+      ],
     },
   ],
 }))();
