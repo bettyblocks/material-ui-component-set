8d0ab26c89ef446f6bc3fd3459d3e55d4ac506ac
Rob Dekker <rob-ajax@live.nl>
2021-04-22 12:22:27 +0200

    fix: add outerspace option to paper

--- src/prefabs/pageWithListView.js
+++ src/prefabs/pageWithListView.js
@@ -3371,6 +3371,12 @@
                                       },
                                     },
                                   },
+                                  {
+                                    value: ['0rem', '0rem', '0rem', '0rem'],
+                                    label: 'Outer space',
+                                    key: 'outerSpacing',
+                                    type: 'SIZES',
+                                  },
                                 ],
                                 descendants: [
                                   {
