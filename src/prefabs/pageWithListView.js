50e781b8be0e067a789752feb2b35e4900727741
progbert <ro-ru@live.nl>
2021-05-03 11:44:19 +0200

    chore: added custom search field with interaction

--- src/prefabs/pageWithListView.js
+++ src/prefabs/pageWithListView.js
@@ -2810,7 +2810,394 @@
                     descendants: [],
                   },
                   {
+                    name: 'TextField',
+                    ref: {
+                      id: '#searchField',
+                    },
+                    options: [
+                      {
+                        value: { label: ['Search'], value: [] },
+                        label: 'Label',
+                        key: 'customModelAttribute',
+                        type: 'CUSTOM_MODEL_ATTRIBUTE',
+                        configuration: {
+                          allowedTypes: ['string'],
+                        },
+                      },
+                      {
+                        value: false,
+                        label: 'Validation options',
+                        key: 'validationOptions',
+                        type: 'TOGGLE',
+                      },
+                      {
+                        label: 'Validation pattern',
+                        key: 'pattern',
+                        value: '',
+                        type: 'TEXT',
+                        configuration: {
+                          placeholder: '(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}',
+                          condition: {
+                            type: 'SHOW',
+                            option: 'validationOptions',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        label: 'Min length',
+                        key: 'minlength',
+                        value: '',
+                        type: 'NUMBER',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'validationOptions',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        label: 'Max length',
+                        key: 'maxlength',
+                        value: '',
+                        type: 'NUMBER',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'validationOptions',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        value: ['This field is required'],
+                        label: 'Value required message',
+                        key: 'validationValueMissing',
+                        type: 'VARIABLE',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'validationOptions',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        value: ['Invalid value'],
+                        label: 'Pattern mismatch message',
+                        key: 'validationPatternMismatch',
+                        type: 'VARIABLE',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'validationOptions',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        value: ['This value is too short'],
+                        label: 'Value too short message',
+                        key: 'validationTooShort',
+                        type: 'VARIABLE',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'validationOptions',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        value: ['This value is too long'],
+                        label: 'Value too long message',
+                        key: 'validationTooLong',
+                        type: 'VARIABLE',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'validationOptions',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        type: 'TOGGLE',
+                        label: 'Disabled',
+                        key: 'disabled',
+                        value: false,
+                      },
+                      {
+                        value: [],
+                        label: 'Placeholder',
+                        key: 'placeholder',
+                        type: 'VARIABLE',
+                      },
+                      {
+                        value: [],
+                        label: 'Helper text',
+                        key: 'helperText',
+                        type: 'VARIABLE',
+                      },
+                      {
+                        label: 'Variant',
+                        key: 'variant',
+                        value: 'outlined',
+                        type: 'CUSTOM',
+                        configuration: {
+                          as: 'BUTTONGROUP',
+                          dataType: 'string',
+                          allowedInput: [
+                            { name: 'Standard', value: 'standard' },
+                            { name: 'Outlined', value: 'outlined' },
+                            { name: 'Filled', value: 'filled' },
+                          ],
+                        },
+                      },
+                      {
+                        type: 'TOGGLE',
+                        label: 'Full width',
+                        key: 'fullWidth',
+                        value: true,
+                      },
+                      {
+                        label: 'Size',
+                        key: 'size',
+                        value: 'medium',
+                        type: 'CUSTOM',
+                        configuration: {
+                          as: 'BUTTONGROUP',
+                          dataType: 'string',
+                          allowedInput: [
+                            { name: 'Medium', value: 'medium' },
+                            { name: 'Small', value: 'small' },
+                          ],
+                        },
+                      },
+                      {
+                        label: 'Margin',
+                        key: 'margin',
+                        value: 'normal',
+                        type: 'CUSTOM',
+                        configuration: {
+                          as: 'BUTTONGROUP',
+                          dataType: 'string',
+                          allowedInput: [
+                            { name: 'None', value: 'none' },
+                            { name: 'Dense', value: 'dense' },
+                            { name: 'Normal', value: 'normal' },
+                          ],
+                        },
+                      },
+                      {
+                        label: 'Adornment',
+                        key: 'adornmentIcon',
+                        value: 'Search',
+                        type: 'CUSTOM',
+                        configuration: iconConfiguration,
+                      },
+                      {
+                        type: 'CUSTOM',
+                        label: 'Position',
+                        key: 'adornmentPosition',
+                        value: 'start',
+                        configuration: {
+                          condition: {
+                            type: 'HIDE',
+                            option: 'adornmentIcon',
+                            comparator: 'EQ',
+                            value: '',
+                          },
+                          as: 'BUTTONGROUP',
+                          dataType: 'string',
+                          allowedInput: [
+                            { name: 'Start', value: 'start' },
+                            { name: 'End', value: 'end' },
+                          ],
+                        },
+                      },
+                      {
+                        value: false,
+                        label: 'Styles',
+                        key: 'styles',
+                        type: 'TOGGLE',
+                      },
+                      {
+                        type: 'COLOR',
+                        label: 'Background color',
+                        key: 'backgroundColor',
+                        value: 'White',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        type: 'COLOR',
+                        label: 'Border color',
+                        key: 'borderColor',
+                        value: 'Accent1',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        type: 'COLOR',
+                        label: 'Border color (hover)',
+                        key: 'borderHoverColor',
+                        value: 'Black',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        type: 'COLOR',
+                        label: 'Border color (focus)',
+                        key: 'borderFocusColor',
+                        value: 'Primary',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        value: false,
+                        label: 'Hide label',
+                        key: 'hideLabel',
+                        type: 'TOGGLE',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        type: 'COLOR',
+                        label: 'Label color',
+                        key: 'labelColor',
+                        value: 'Accent3',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        type: 'COLOR',
+                        label: 'Text color',
+                        key: 'textColor',
+                        value: 'Black',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        type: 'COLOR',
+                        label: 'Placeholder color',
+                        key: 'placeholderColor',
+                        value: 'Light',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        type: 'COLOR',
+                        label: 'Helper color',
+                        key: 'helperColor',
+                        value: 'Accent2',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        type: 'COLOR',
+                        label: 'Error color',
+                        key: 'errorColor',
+                        value: 'Danger',
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'styles',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                      {
+                        value: false,
+                        label: 'Advanced settings',
+                        key: 'advancedSettings',
+                        type: 'TOGGLE',
+                      },
+                      {
+                        type: 'VARIABLE',
+                        label: 'name attribute',
+                        key: 'nameAttribute',
+                        value: [],
+                        configuration: {
+                          condition: {
+                            type: 'SHOW',
+                            option: 'advancedSettings',
+                            comparator: 'EQ',
+                            value: true,
+                          },
+                        },
+                      },
+                    ],
+                    descendants: [],
+                  },
+                  {
                     name: 'DataList',
+                    ref: {
+                      id: '#dataList',
+                    },
                     options: [
                       {
                         value: '',
@@ -2866,7 +3253,7 @@
                         },
                       },
                       {
-                        value: '',
+                        value: 'true',
                         label: 'Hide built-in search field',
                         key: 'hideSearch',
                         type: 'TOGGLE',
@@ -2953,7 +3340,7 @@
                         },
                       },
                       {
-                        value: ['0rem', '0rem', 'M', '0rem'],
+                        value: ['M', '0rem', 'M', '0rem'],
                         label: 'Outer space',
                         key: 'outerSpacing',
                         type: 'SIZES',
@@ -5275,7 +5662,7 @@
             const newPrefab = { ...prefab };
             if (modelId) {
               const dataList =
-                prefabStructure[1].descendants[0].descendants[0].descendants[2];
+                prefabStructure[1].descendants[0].descendants[0].descendants[3];
               dataList.options[0].value = modelId;
               if (imageProperty.id) {
                 dataList.descendants[0].descendants[0].descendants[0].descendants[0].descendants[0].descendants[0].options[1].value = [
@@ -5309,7 +5696,17 @@
   },
   variables: [],
   actions: [],
-  interactions: [],
+  interactions: [
+    {
+      name: 'SetSearchValue',
+      sourceEvent: 'Change',
+      ref: {
+        targetComponentId: '#dataList',
+        sourceComponentId: '#searchField',
+      },
+      type: 'Custom',
+    },
+  ],
   structure: [
     {
       name: 'Row',
