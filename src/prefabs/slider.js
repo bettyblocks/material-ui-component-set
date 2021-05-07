15fc69f3dfe30b866ebe37f0e2e68c81c022e8eb
z190mn <sander.v.riessen@hotmail.nl>
2021-05-07 13:23:57 +0200

    feat: slider input

---
+++ src/prefabs/slider.js
@@ -0,0 +1,154 @@
+(() => ({
+  name: 'Slider Input',
+  icon: 'ParagraphIcon',
+  category: 'CONTENT',
+  structure: [
+    {
+      name: 'sliderInput',
+      options: [
+        {
+          value: { label: ['slider'], value: [] },
+          label: 'Label',
+          key: 'customModelAttribute',
+          type: 'CUSTOM_MODEL_ATTRIBUTE',
+          configuration: {
+            allowedTypes: ['integer'],
+          },
+        },
+        {
+          type: 'NUMBER',
+          label: 'Start',
+          key: 'startNumber',
+          value: 0,
+        },
+        {
+          type: 'NUMBER',
+          label: 'End',
+          key: 'endNumber',
+          value: 100,
+        },
+        {
+          type: 'NUMBER',
+          label: 'Step',
+          key: 'stepNumber',
+          value: 1,
+        },
+        {
+          value: ['M', '0rem', '0rem', '0rem'],
+          label: 'Outer space',
+          key: 'outerSpacing',
+          type: 'SIZES',
+        },
+        {
+          type: 'CUSTOM',
+          label: 'Border',
+          key: 'border',
+          value: 'withBorder',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: 'With Border',
+                value: 'withBorder',
+              },
+              {
+                name: 'Without Border',
+                value: 'withoutBorder',
+              },
+            ],
+          },
+        },
+        {
+          value: false,
+          label: 'Marks',
+          key: 'marks',
+          type: 'TOGGLE',
+        },
+        {
+          type: 'TOGGLE',
+          label: 'Disable',
+          key: 'disable',
+          value: false,
+        },
+        {
+          value: false,
+          label: 'Styles',
+          key: 'styles',
+          type: 'TOGGLE',
+        },
+        {
+          type: 'COLOR',
+          label: 'Slider color',
+          key: 'sliderColor',
+          value: 'Primary',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'styles',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          type: 'COLOR',
+          label: 'Border color',
+          key: 'borderColor',
+          value: 'Accent1',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'styles',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          type: 'COLOR',
+          label: 'Border color (hover)',
+          key: 'borderHoverColor',
+          value: 'Black',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'styles',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          type: 'COLOR',
+          label: 'Border color (focus)',
+          key: 'borderFocusColor',
+          value: 'Primary',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'styles',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          type: 'COLOR',
+          label: 'Label color',
+          key: 'labelColor',
+          value: 'Accent3',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'styles',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+      ],
+      descendants: [],
+    },
+  ],
+}))();
