1f4c511560ce979b1be16daa14c7dc717d8cd228
robbertruiter <robbert.ruiter@bettyblocks.com>
2021-05-27 12:10:16 +0200

    chore: updated image and button options

--- src/prefabs/pageWithMasterDetailVertical.js
+++ src/prefabs/pageWithMasterDetailVertical.js
@@ -1921,34 +1921,10 @@
                         },
                       },
                       {
-                        type: 'CUSTOM',
-                        label: 'variant',
-                        key: 'variant',
-                        value: 'text',
-                        configuration: {
-                          as: 'BUTTONGROUP',
-                          dataType: 'string',
-                          allowedInput: [
-                            { name: 'Text', value: 'text' },
-                            { name: 'Outlined', value: 'outlined' },
-                            { name: 'Contain', value: 'contained' },
-                            { name: 'Icon', value: 'icon' },
-                          ],
-                        },
-                      },
-                      {
                         type: 'VARIABLE',
                         label: 'Button text',
                         key: 'buttonText',
                         value: ['Menu 1'],
-                        configuration: {
-                          condition: {
-                            type: 'HIDE',
-                            option: 'variant',
-                            comparator: 'EQ',
-                            value: 'icon',
-                          },
-                        },
                       },
                       {
                         type: 'CUSTOM',
@@ -1961,7 +1937,6 @@
                           allowedInput: [
                             { name: 'Internal page', value: 'internal' },
                             { name: 'External page', value: 'external' },
-                            { name: 'Action', value: 'action' },
                           ],
                         },
                       },
@@ -2015,36 +1990,20 @@
                         },
                       },
                       {
-                        value: '',
-                        label: 'Action',
-                        key: 'actionId',
-                        type: 'ACTION',
-                        configuration: {
-                          apiVersion: 'v1',
-                          condition: {
-                            type: 'SHOW',
-                            option: 'linkType',
-                            comparator: 'EQ',
-                            value: 'action',
-                          },
-                        },
-                      },
-                      {
                         value: false,
                         label: 'Full width',
                         key: 'fullWidth',
                         type: 'TOGGLE',
-                        configuration: {
-                          condition: {
-                            type: 'HIDE',
-                            option: 'variant',
-                            comparator: 'EQ',
-                            value: 'icon',
-                          },
-                        },
                       },
                       {
-                        value: 'medium',
+                        label: 'Icon',
+                        key: 'icon',
+                        value: 'None',
+                        type: 'CUSTOM',
+                        configuration: iconConfiguration,
+                      },
+                      {
+                        value: 'small',
                         label: 'Size',
                         key: 'size',
                         type: 'CUSTOM',
@@ -2056,14 +2015,13 @@
                             { name: 'Medium', value: 'medium' },
                             { name: 'Small', value: 'small' },
                           ],
+                          condition: {
+                            type: 'HIDE',
+                            option: 'icon',
+                            comparator: 'EQ',
+                            value: 'None',
+                          },
                         },
-                      },
-                      {
-                        label: 'Icon',
-                        key: 'icon',
-                        value: 'None',
-                        type: 'CUSTOM',
-                        configuration: iconConfiguration,
                       },
                       {
                         type: 'CUSTOM',
@@ -2075,9 +2033,9 @@
                           dataType: 'string',
                           condition: {
                             type: 'HIDE',
-                            option: 'variant',
+                            option: 'icon',
                             comparator: 'EQ',
-                            value: 'icon',
+                            value: 'None',
                           },
                           allowedInput: [
                             { name: 'Start', value: 'start' },
@@ -2086,27 +2044,7 @@
                         },
                       },
                       {
-                        type: 'COLOR',
-                        label: 'Text color',
-                        key: 'textColor',
-                        value: 'White',
-                        configuration: {
-                          condition: {
-                            type: 'HIDE',
-                            option: 'variant',
-                            comparator: 'EQ',
-                            value: 'icon',
-                          },
-                        },
-                      },
-                      {
-                        type: 'COLOR',
-                        label: 'Color',
-                        key: 'background',
-                        value: 'Primary',
-                      },
-                      {
-                        value: ['0rem', '0rem', '0rem', '0rem'],
+                        value: ['0rem', 'M', '0rem', '0rem'],
                         label: 'Outer space',
                         key: 'outerSpacing',
                         type: 'SIZES',
@@ -2246,34 +2184,10 @@
                         },
                       },
                       {
-                        type: 'CUSTOM',
-                        label: 'variant',
-                        key: 'variant',
-                        value: 'text',
-                        configuration: {
-                          as: 'BUTTONGROUP',
-                          dataType: 'string',
-                          allowedInput: [
-                            { name: 'Text', value: 'text' },
-                            { name: 'Outlined', value: 'outlined' },
-                            { name: 'Contain', value: 'contained' },
-                            { name: 'Icon', value: 'icon' },
-                          ],
-                        },
-                      },
-                      {
                         type: 'VARIABLE',
                         label: 'Button text',
                         key: 'buttonText',
                         value: ['Menu 2'],
-                        configuration: {
-                          condition: {
-                            type: 'HIDE',
-                            option: 'variant',
-                            comparator: 'EQ',
-                            value: 'icon',
-                          },
-                        },
                       },
                       {
                         type: 'CUSTOM',
@@ -2286,7 +2200,6 @@
                           allowedInput: [
                             { name: 'Internal page', value: 'internal' },
                             { name: 'External page', value: 'external' },
-                            { name: 'Action', value: 'action' },
                           ],
                         },
                       },
@@ -2340,36 +2253,20 @@
                         },
                       },
                       {
-                        value: '',
-                        label: 'Action',
-                        key: 'actionId',
-                        type: 'ACTION',
-                        configuration: {
-                          apiVersion: 'v1',
-                          condition: {
-                            type: 'SHOW',
-                            option: 'linkType',
-                            comparator: 'EQ',
-                            value: 'action',
-                          },
-                        },
-                      },
-                      {
                         value: false,
                         label: 'Full width',
                         key: 'fullWidth',
                         type: 'TOGGLE',
-                        configuration: {
-                          condition: {
-                            type: 'HIDE',
-                            option: 'variant',
-                            comparator: 'EQ',
-                            value: 'icon',
-                          },
-                        },
                       },
                       {
-                        value: 'medium',
+                        label: 'Icon',
+                        key: 'icon',
+                        value: 'None',
+                        type: 'CUSTOM',
+                        configuration: iconConfiguration,
+                      },
+                      {
+                        value: 'small',
                         label: 'Size',
                         key: 'size',
                         type: 'CUSTOM',
@@ -2381,14 +2278,13 @@
                             { name: 'Medium', value: 'medium' },
                             { name: 'Small', value: 'small' },
                           ],
+                          condition: {
+                            type: 'HIDE',
+                            option: 'icon',
+                            comparator: 'EQ',
+                            value: 'None',
+                          },
                         },
-                      },
-                      {
-                        label: 'Icon',
-                        key: 'icon',
-                        value: 'None',
-                        type: 'CUSTOM',
-                        configuration: iconConfiguration,
                       },
                       {
                         type: 'CUSTOM',
@@ -2398,37 +2294,17 @@
                         configuration: {
                           as: 'BUTTONGROUP',
                           dataType: 'string',
-                          condition: {
-                            type: 'HIDE',
-                            option: 'variant',
-                            comparator: 'EQ',
-                            value: 'icon',
-                          },
                           allowedInput: [
                             { name: 'Start', value: 'start' },
                             { name: 'End', value: 'end' },
                           ],
-                        },
-                      },
-                      {
-                        type: 'COLOR',
-                        label: 'Text color',
-                        key: 'textColor',
-                        value: 'White',
-                        configuration: {
                           condition: {
                             type: 'HIDE',
-                            option: 'variant',
+                            option: 'icon',
                             comparator: 'EQ',
-                            value: 'icon',
+                            value: 'None',
                           },
                         },
-                      },
-                      {
-                        type: 'COLOR',
-                        label: 'Color',
-                        key: 'background',
-                        value: 'Primary',
                       },
                       {
                         value: ['0rem', '0rem', '0rem', '0rem'],
@@ -3420,6 +3296,55 @@
                           },
                         },
                         {
+                          type: 'CUSTOM',
+                          label: 'Link to',
+                          key: 'linkType',
+                          value: 'internal',
+                          configuration: {
+                            as: 'BUTTONGROUP',
+                            dataType: 'string',
+                            allowedInput: [
+                              { name: 'Internal page', value: 'internal' },
+                              { name: 'External page', value: 'external' },
+                            ],
+                            condition: {
+                              type: 'SHOW',
+                              option: 'type',
+                              comparator: 'EQ',
+                              value: 'img',
+                            },
+                          },
+                        },
+                        {
+                          value: '',
+                          label: 'Page',
+                          key: 'linkTo',
+                          type: 'ENDPOINT',
+                          configuration: {
+                            condition: {
+                              type: 'SHOW',
+                              option: 'linkType',
+                              comparator: 'EQ',
+                              value: 'internal',
+                            },
+                          },
+                        },
+                        {
+                          value: [''],
+                          label: 'URL',
+                          key: 'linkToExternal',
+                          type: 'VARIABLE',
+                          configuration: {
+                            placeholder: 'Starts with https:// or http://',
+                            condition: {
+                              type: 'SHOW',
+                              option: 'linkType',
+                              comparator: 'EQ',
+                              value: 'external',
+                            },
+                          },
+                        },
+                        {
                           value: [],
                           label: 'Image Alternative Text',
                           key: 'imgAlt',
@@ -3682,6 +3607,55 @@
                                 option: 'type',
                                 comparator: 'EQ',
                                 value: 'iframe',
+                              },
+                            },
+                          },
+                          {
+                            type: 'CUSTOM',
+                            label: 'Link to',
+                            key: 'linkType',
+                            value: 'internal',
+                            configuration: {
+                              as: 'BUTTONGROUP',
+                              dataType: 'string',
+                              allowedInput: [
+                                { name: 'Internal page', value: 'internal' },
+                                { name: 'External page', value: 'external' },
+                              ],
+                              condition: {
+                                type: 'SHOW',
+                                option: 'type',
+                                comparator: 'EQ',
+                                value: 'img',
+                              },
+                            },
+                          },
+                          {
+                            value: '',
+                            label: 'Page',
+                            key: 'linkTo',
+                            type: 'ENDPOINT',
+                            configuration: {
+                              condition: {
+                                type: 'SHOW',
+                                option: 'linkType',
+                                comparator: 'EQ',
+                                value: 'internal',
+                              },
+                            },
+                          },
+                          {
+                            value: [''],
+                            label: 'URL',
+                            key: 'linkToExternal',
+                            type: 'VARIABLE',
+                            configuration: {
+                              placeholder: 'Starts with https:// or http://',
+                              condition: {
+                                type: 'SHOW',
+                                option: 'linkType',
+                                comparator: 'EQ',
+                                value: 'external',
                               },
                             },
                           },
