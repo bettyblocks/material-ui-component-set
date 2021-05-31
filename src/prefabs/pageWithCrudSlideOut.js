f93adebe8164a3f64918ca20fa5a3639ad2de3fa
Sven Truschel <sventruschel@Svens-MacBook-Pro.local>
2021-05-31 14:15:59 +0200

    chore: fixed background height

--- src/prefabs/pageWithCrudSlideOut.js
+++ src/prefabs/pageWithCrudSlideOut.js
@@ -8211,7 +8211,7 @@
               }}
             />
           </Field>
-          <Field label="Create Form">
+          <Field label="Create & Update Form">
             <CheckBox
               label="Use the same properties as the data table in the create and update form"
               checked={createFormUseDataProperties}
@@ -21352,8 +21352,7 @@
             newPrefab.variables[2].options.modelId = modelId;
             newPrefab.variables[3].options.modelId = modelId;
             newPrefab.variables[4].options.modelId = modelId;
-
-            newPrefab.structure[0].descendants = [
+            newPrefab.structure[0].descendants[0].descendants[0].descendants = [
               ...drawerSidebar,
               ...drawerContainer,
             ];
@@ -21755,106 +21754,334 @@
   ],
   structure: [
     {
-      name: 'Drawer',
+      name: 'Row',
       options: [
         {
-          type: 'SIZE',
-          label: 'Drawer Width',
-          key: 'drawerWidth',
-          value: '480px',
+          type: 'CUSTOM',
+          label: 'Width',
+          key: 'maxRowWidth',
+          value: 'Full',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              { name: 'S', value: 'S' },
+              { name: 'M', value: 'M' },
+              { name: 'L', value: 'L' },
+              { name: 'XL', value: 'XL' },
+              { name: 'Full', value: 'Full' },
+            ],
+          },
+        },
+        {
+          value: '100%',
+          label: 'Height',
+          key: 'rowHeight',
+          type: 'TEXT',
           configuration: {
             as: 'UNIT',
           },
         },
         {
-          value: 'temporary',
-          label: 'Drawer type',
-          key: 'drawerType',
-          type: 'CUSTOM',
-          configuration: {
-            as: 'BUTTONGROUP',
-            dataType: 'string',
-            allowedInput: [
-              { name: 'Persistent', value: 'persistent' },
-              { name: 'Temporary', value: 'temporary' },
-            ],
-          },
+          value: 'Accent1',
+          label: 'Background color',
+          key: 'backgroundColor',
+          type: 'COLOR',
         },
         {
-          value: 'sm',
-          label: 'Responsively temporary on',
-          key: 'breakpoint',
-          type: 'CUSTOM',
-          configuration: {
-            as: 'DROPDOWN',
-            dataType: 'string',
-            allowedInput: [
-              { name: 'Permanent', value: 'xs' },
-              { name: 'Mobile', value: 'sm' },
-              { name: 'Tablet portrait', value: 'md' },
-              { name: 'Tablet landscape', value: 'lg' },
-            ],
-            condition: {
-              type: 'SHOW',
-              option: 'drawerType',
-              comparator: 'EQ',
-              value: 'persistent',
-            },
-          },
-        },
-        {
-          value: 'right',
-          label: 'Alignment',
-          key: 'temporaryAnchor',
-          type: 'CUSTOM',
-          configuration: {
-            as: 'BUTTONGROUP',
-            dataType: 'string',
-            allowedInput: [
-              { name: 'Left', value: 'left' },
-              { name: 'Top', value: 'top' },
-              { name: 'Right', value: 'right' },
-              { name: 'Bottom', value: 'bottom' },
-            ],
-            condition: {
-              type: 'SHOW',
-              option: 'drawerType',
-              comparator: 'EQ',
-              value: 'temporary',
-            },
-          },
-        },
-        {
-          value: 'left',
-          label: 'Alignment',
-          key: 'persistentAnchor',
-          type: 'CUSTOM',
-          configuration: {
-            as: 'BUTTONGROUP',
-            dataType: 'string',
-            allowedInput: [
-              { name: 'Left', value: 'left' },
-              { name: 'Right', value: 'right' },
-            ],
-            condition: {
-              type: 'HIDE',
-              option: 'drawerType',
-              comparator: 'EQ',
-              value: 'temporary',
-            },
-          },
-        },
-        {
-          label: 'Toggle visibility',
-          key: 'visibility',
-          value: false,
-          type: 'TOGGLE',
-          configuration: {
-            as: 'VISIBILITY',
-          },
+          value: ['0rem', '0rem', '0rem', '0rem'],
+          label: 'Outer space',
+          key: 'outerSpacing',
+          type: 'SIZES',
         },
       ],
-      descendants: [],
+      descendants: [
+        {
+          name: 'Column',
+          options: [
+            {
+              label: 'Toggle visibility',
+              key: 'visible',
+              value: true,
+              type: 'TOGGLE',
+              configuration: {
+                as: 'VISIBILITY',
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width',
+              key: 'columnWidth',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width (tablet landscape)',
+              key: 'columnWidthTabletLandscape',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width (tablet portrait)',
+              key: 'columnWidthTabletPortrait',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width (mobile)',
+              key: 'columnWidthMobile',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: '',
+              label: 'Height',
+              key: 'columnHeight',
+              type: 'TEXT',
+              configuration: {
+                as: 'UNIT',
+              },
+            },
+            {
+              value: 'transparent',
+              label: 'Background color',
+              key: 'backgroundColor',
+              type: 'COLOR',
+            },
+            {
+              type: 'CUSTOM',
+              label: 'Horizontal Alignment',
+              key: 'horizontalAlignment',
+              value: 'inherit',
+              configuration: {
+                as: 'BUTTONGROUP',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'None', value: 'inherit' },
+                  { name: 'Left', value: 'flex-start' },
+                  { name: 'Center', value: 'center' },
+                  { name: 'Right', value: 'flex-end' },
+                ],
+              },
+            },
+            {
+              type: 'CUSTOM',
+              label: 'Vertical Alignment',
+              key: 'verticalAlignment',
+              value: 'inherit',
+              configuration: {
+                as: 'BUTTONGROUP',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'None', value: 'inherit' },
+                  { name: 'Top', value: 'flex-start' },
+                  { name: 'Center', value: 'center' },
+                  { name: 'Bottom', value: 'flex-end' },
+                ],
+              },
+            },
+            {
+              value: ['0rem', '0rem', '0rem', '0rem'],
+              label: 'Outer space',
+              key: 'outerSpacing',
+              type: 'SIZES',
+            },
+            {
+              value: ['0rem', '0rem', '0rem', '0rem'],
+              label: 'Inner space',
+              key: 'innerSpacing',
+              type: 'SIZES',
+            },
+          ],
+          descendants: [
+            {
+              name: 'Drawer',
+              options: [
+                {
+                  type: 'SIZE',
+                  label: 'Drawer Width',
+                  key: 'drawerWidth',
+                  value: '480px',
+                  configuration: {
+                    as: 'UNIT',
+                  },
+                },
+                {
+                  value: 'temporary',
+                  label: 'Drawer type',
+                  key: 'drawerType',
+                  type: 'CUSTOM',
+                  configuration: {
+                    as: 'BUTTONGROUP',
+                    dataType: 'string',
+                    allowedInput: [
+                      { name: 'Persistent', value: 'persistent' },
+                      { name: 'Temporary', value: 'temporary' },
+                    ],
+                  },
+                },
+                {
+                  value: 'sm',
+                  label: 'Responsively temporary on',
+                  key: 'breakpoint',
+                  type: 'CUSTOM',
+                  configuration: {
+                    as: 'DROPDOWN',
+                    dataType: 'string',
+                    allowedInput: [
+                      { name: 'Permanent', value: 'xs' },
+                      { name: 'Mobile', value: 'sm' },
+                      { name: 'Tablet portrait', value: 'md' },
+                      { name: 'Tablet landscape', value: 'lg' },
+                    ],
+                    condition: {
+                      type: 'SHOW',
+                      option: 'drawerType',
+                      comparator: 'EQ',
+                      value: 'persistent',
+                    },
+                  },
+                },
+                {
+                  value: 'right',
+                  label: 'Alignment',
+                  key: 'temporaryAnchor',
+                  type: 'CUSTOM',
+                  configuration: {
+                    as: 'BUTTONGROUP',
+                    dataType: 'string',
+                    allowedInput: [
+                      { name: 'Left', value: 'left' },
+                      { name: 'Top', value: 'top' },
+                      { name: 'Right', value: 'right' },
+                      { name: 'Bottom', value: 'bottom' },
+                    ],
+                    condition: {
+                      type: 'SHOW',
+                      option: 'drawerType',
+                      comparator: 'EQ',
+                      value: 'temporary',
+                    },
+                  },
+                },
+                {
+                  value: 'left',
+                  label: 'Alignment',
+                  key: 'persistentAnchor',
+                  type: 'CUSTOM',
+                  configuration: {
+                    as: 'BUTTONGROUP',
+                    dataType: 'string',
+                    allowedInput: [
+                      { name: 'Left', value: 'left' },
+                      { name: 'Right', value: 'right' },
+                    ],
+                    condition: {
+                      type: 'HIDE',
+                      option: 'drawerType',
+                      comparator: 'EQ',
+                      value: 'temporary',
+                    },
+                  },
+                },
+                {
+                  label: 'Toggle visibility',
+                  key: 'visibility',
+                  value: false,
+                  type: 'TOGGLE',
+                  configuration: {
+                    as: 'VISIBILITY',
+                  },
+                },
+              ],
+              descendants: [],
+            },
+          ],
+        },
+      ],
     },
   ],
 }))();
