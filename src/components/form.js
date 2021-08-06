56467a87f6f9537b2d5e3b995d3d2383963daca9
Jeroen Reijs <jeroen.reijs@bettyblocks.com>
2021-08-06 11:14:06 +0200

    fix: remove redundant classnames in runtime version as well

--- src/components/form.js
+++ src/components/form.js
@@ -194,10 +194,7 @@
                       handleSubmit(evt, callAction, item);
                     }}
                     ref={formRef}
-                    className={[
-                      isEmpty && classes.empty,
-                      isPristine && classes.pristine,
-                    ].join(' ')}
+                    className={classNames || undefined}
                   >
                     {isPristine && (
                       <span>
