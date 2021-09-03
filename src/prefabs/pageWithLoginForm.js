04e0c9f5c423a48e237dccee67576e1849acca54
Jeroen Reijs <jeroen.reijs@bettyblocks.com>
2021-09-03 16:47:18 +0200

    fix: remove double option after conflicts

--- src/prefabs/pageWithLoginForm.js
+++ src/prefabs/pageWithLoginForm.js
@@ -169,12 +169,6 @@
                         },
                         {
                           value: false,
-                          label: 'Autocomplete',
-                          key: 'autoComplete',
-                          type: 'TOGGLE',
-                        },
-                        {
-                          value: false,
                           label: 'Validation options',
                           key: 'validationOptions',
                           type: 'TOGGLE',
