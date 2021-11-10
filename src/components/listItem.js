fb1b55697017d4376b67e8dd0f6e85906bed0949
Jeroen Reijs <jeroen.reijs@bettyblocks.com>
2021-11-10 16:34:17 +0100

    fix: update list and list item in drawer component

--- src/components/listItem.js
+++ src/components/listItem.js
@@ -28,7 +28,7 @@
       linkTo,
       linkToExternal,
       dense,
-      dataComponentAttribute,
+      dataComponentAttribute = ['ListItem'],
     } = options;
     const { env, useText, Link } = B;
     const isDev = env === 'dev';
@@ -41,6 +41,7 @@
 
     const primary = useText(primaryText);
     const secondary = useText(secondaryText);
+    const dataComponentAttributeValue = useText(dataComponentAttribute);
 
     const IconComponent = icon !== 'None' && (
       <ListItemIcon>{React.createElement(Icons[icon])}</ListItemIcon>
@@ -63,8 +64,9 @@
     const itemText = isEmpty && isDev ? 'Empty content' : primary;
 
     let linkComponent = 'li';
-    if (linkType === 'internal' && hasLink) linkComponent = Link;
-    if (linkType === 'external' && hasExternalLink) linkComponent = 'a';
+    if (linkType === 'internal' && hasLink && !isDev) linkComponent = Link;
+    if (linkType === 'external' && hasExternalLink && !isDev)
+      linkComponent = 'a';
 
     return (
       <ListItem
@@ -72,7 +74,9 @@
         button={hasLink || linkToExternalVariable}
         href={hasExternalLink ? linkToExternalVariable : undefined}
         component={linkComponent}
-        endpoint={linkType === 'internal' && hasLink ? linkTo : undefined}
+        endpoint={
+          linkType === 'internal' && hasLink && !isDev ? linkTo : undefined
+        }
         alignItems={alignItems}
         disabled={disabled}
         disableGutters={disableGutters}
@@ -80,7 +84,7 @@
         selected={selected}
         className={classes.root}
         dense={dense}
-        data-component={useText(dataComponentAttribute) || 'ListItem'}
+        data-component={dataComponentAttributeValue}
       >
         {avatarOrIcon === 'avatar' || (avatarOrIcon === 'icon' && avatar)
           ? AvatarComponent
@@ -95,6 +99,8 @@
   })(),
   styles: B => t => {
     const { Styling } = B;
+    const { env } = B;
+    const isDev = env === 'dev';
     const style = new Styling(t);
     return {
       root: {
@@ -120,6 +126,9 @@
         },
         '& .MuiListItemIcon-root': {
           color: ({ options: { iconColor } }) => style.getColor(iconColor),
+        },
+        '& .MuiTouchRipple-root': {
+          display: isDev ? 'none' : undefined,
         },
       },
       placeholder: {
--- src/prefabs/drawer.js
+++ src/prefabs/drawer.js
@@ -172,21 +172,6 @@
               name: 'List',
               options: [
                 {
-                  value: '',
-                  label: 'Model',
-                  key: 'model',
-                  type: 'MODEL',
-                },
-                {
-                  value: {},
-                  label: 'Filter',
-                  key: 'filter',
-                  type: 'FILTER',
-                  configuration: {
-                    dependsOn: 'model',
-                  },
-                },
-                {
                   type: 'COLOR',
                   label: 'Background color',
                   key: 'backgroundColor',
@@ -254,28 +239,47 @@
                       value: 'Transparent',
                     },
                     {
-                      type: 'COLOR',
-                      label: 'Title color',
-                      key: 'titleColor',
-                      value: 'Black',
-                    },
-                    {
-                      type: 'COLOR',
-                      label: 'Icon color',
-                      key: 'iconColor',
-                      value: 'Black',
-                    },
-                    {
-                      type: 'COLOR',
-                      label: 'Subtitle color',
-                      key: 'subtitleColor',
-                      value: 'Secondary',
+                      type: 'CUSTOM',
+                      label: 'Link to',
+                      key: 'linkType',
+                      value: 'internal',
+                      configuration: {
+                        as: 'BUTTONGROUP',
+                        dataType: 'string',
+                        allowedInput: [
+                          { name: 'Internal page', value: 'internal' },
+                          { name: 'External page', value: 'external' },
+                        ],
+                      },
                     },
                     {
                       value: '',
                       label: 'Page',
                       key: 'linkTo',
                       type: 'ENDPOINT',
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'linkType',
+                          comparator: 'EQ',
+                          value: 'internal',
+                        },
+                      },
+                    },
+                    {
+                      value: [''],
+                      label: 'URL',
+                      key: 'linkToExternal',
+                      type: 'VARIABLE',
+                      configuration: {
+                        placeholder: 'Starts with https:// or http://',
+                        condition: {
+                          type: 'SHOW',
+                          option: 'linkType',
+                          comparator: 'EQ',
+                          value: 'external',
+                        },
+                      },
                     },
                     {
                       type: 'CUSTOM',
@@ -292,18 +296,35 @@
                       },
                     },
                     {
+                      type: 'CUSTOM',
+                      label: 'Visual',
+                      key: 'avatarOrIcon',
+                      value: 'none',
+                      configuration: {
+                        as: 'BUTTONGROUP',
+                        dataType: 'string',
+                        allowedInput: [
+                          { name: 'None', value: 'none' },
+                          { name: 'Icon', value: 'icon' },
+                          { name: 'Avatar', value: 'avatar' },
+                        ],
+                      },
+                    },
+                    {
                       label: 'Icon',
                       key: 'icon',
-                      value: 'None',
+                      value: 'Person',
                       type: 'CUSTOM',
                       configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'avatarOrIcon',
+                          comparator: 'EQ',
+                          value: 'icon',
+                        },
                         as: 'DROPDOWN',
                         dataType: 'string',
                         allowedInput: [
-                          {
-                            name: 'None',
-                            value: 'None',
-                          },
                           {
                             name: 'AcUnit',
                             value: 'AcUnit',
@@ -1560,16 +1581,44 @@
                       },
                     },
                     {
+                      type: 'COLOR',
+                      label: 'Icon color',
+                      key: 'iconColor',
+                      value: 'Black',
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'avatarOrIcon',
+                          comparator: 'EQ',
+                          value: 'icon',
+                        },
+                      },
+                    },
+                    {
                       value: false,
                       label: 'Show icon as avatar',
                       key: 'avatar',
                       type: 'TOGGLE',
                       configuration: {
                         condition: {
-                          type: 'HIDE',
-                          option: 'icon',
+                          type: 'SHOW',
+                          option: 'avatarOrIcon',
                           comparator: 'EQ',
-                          value: 'None',
+                          value: 'icon',
+                        },
+                      },
+                    },
+                    {
+                      type: 'VARIABLE',
+                      label: 'Avatar URL',
+                      key: 'avatarUrl',
+                      value: [''],
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'avatarOrIcon',
+                          comparator: 'EQ',
+                          value: 'avatar',
                         },
                       },
                     },
@@ -1602,6 +1651,124 @@
                       label: 'Selected',
                       key: 'selected',
                       value: false,
+                    },
+                    {
+                      value: false,
+                      label: 'Styles',
+                      key: 'styles',
+                      type: 'TOGGLE',
+                    },
+                    {
+                      value: '1rem',
+                      label: 'Title Font Size',
+                      key: 'titleSize',
+                      type: 'TEXT',
+                      configuration: {
+                        as: 'UNIT',
+                        condition: {
+                          type: 'SHOW',
+                          option: 'styles',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                    {
+                      type: 'COLOR',
+                      label: 'Title color',
+                      key: 'titleColor',
+                      value: 'Black',
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'styles',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                    {
+                      type: 'CUSTOM',
+                      label: 'Title Font weight',
+                      key: 'titleWeight',
+                      value: '400',
+                      configuration: {
+                        as: 'DROPDOWN',
+                        dataType: 'string',
+                        allowedInput: [
+                          { name: '100', value: '100' },
+                          { name: '200', value: '200' },
+                          { name: '300', value: '300' },
+                          { name: '400', value: '400' },
+                          { name: '500', value: '500' },
+                          { name: '600', value: '600' },
+                          { name: '700', value: '700' },
+                          { name: '800', value: '800' },
+                          { name: '900', value: '900' },
+                        ],
+                        condition: {
+                          type: 'SHOW',
+                          option: 'styles',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                    {
+                      value: '0.875rem',
+                      label: 'Subtitle Font Size',
+                      key: 'subtitleSize',
+                      type: 'TEXT',
+                      configuration: {
+                        as: 'UNIT',
+                        condition: {
+                          type: 'SHOW',
+                          option: 'styles',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                    {
+                      type: 'COLOR',
+                      label: 'Subtitle color',
+                      key: 'subtitleColor',
+                      value: 'Secondary',
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'styles',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                    {
+                      type: 'CUSTOM',
+                      label: 'Subtitle Font weight',
+                      key: 'subtitleWeight',
+                      value: '400',
+                      configuration: {
+                        as: 'DROPDOWN',
+                        dataType: 'string',
+                        allowedInput: [
+                          { name: '100', value: '100' },
+                          { name: '200', value: '200' },
+                          { name: '300', value: '300' },
+                          { name: '400', value: '400' },
+                          { name: '500', value: '500' },
+                          { name: '600', value: '600' },
+                          { name: '700', value: '700' },
+                          { name: '800', value: '800' },
+                          { name: '900', value: '900' },
+                        ],
+                        condition: {
+                          type: 'SHOW',
+                          option: 'styles',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
                     },
                     {
                       value: false,
