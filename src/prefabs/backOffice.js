642cedf704179e5950bba7dca546d480ffd18fe2
robbertruiter <robbert.ruiter@bettyblocks.com>
2021-11-16 13:25:56 +0100

    chore: updated fileinput and form object names

--- src/prefabs/backOffice.js
+++ src/prefabs/backOffice.js
@@ -6393,83 +6393,6 @@
                   value: false,
                 },
                 {
-                  label: 'Button size',
-                  key: 'size',
-                  value: 'medium',
-                  type: 'CUSTOM',
-                  configuration: {
-                    as: 'BUTTONGROUP',
-                    dataType: 'string',
-                    allowedInput: [
-                      { name: 'Large', value: 'large' },
-                      { name: 'Medium', value: 'medium' },
-                      { name: 'Small', value: 'small' },
-                    ],
-                  },
-                },
-                {
-                  type: 'CUSTOM',
-                  label: 'Button variant',
-                  key: 'variant',
-                  value: 'contained',
-                  configuration: {
-                    as: 'BUTTONGROUP',
-                    dataType: 'string',
-                    allowedInput: [
-                      { name: 'Text', value: 'text' },
-                      {
-                        name: 'Outlined',
-                        value: 'outlined',
-                      },
-                      {
-                        name: 'Contain',
-                        value: 'contained',
-                      },
-                      { name: 'Icon', value: 'icon' },
-                    ],
-                  },
-                },
-                {
-                  type: 'VARIABLE',
-                  label: 'Button text',
-                  key: 'buttonText',
-                  value: ['Upload'],
-                  configuration: {
-                    condition: {
-                      type: 'HIDE',
-                      option: 'variant',
-                      comparator: 'EQ',
-                      value: 'icon',
-                    },
-                  },
-                },
-                {
-                  label: 'Button icon',
-                  key: 'icon',
-                  value: 'None',
-                  type: 'ICON',
-                },
-                {
-                  type: 'CUSTOM',
-                  label: 'Button icon position',
-                  key: 'iconPosition',
-                  value: 'start',
-                  configuration: {
-                    as: 'BUTTONGROUP',
-                    dataType: 'string',
-                    condition: {
-                      type: 'HIDE',
-                      option: 'variant',
-                      comparator: 'EQ',
-                      value: 'icon',
-                    },
-                    allowedInput: [
-                      { name: 'Start', value: 'start' },
-                      { name: 'End', value: 'end' },
-                    ],
-                  },
-                },
-                {
                   label: 'Margin',
                   key: 'margin',
                   value: 'normal',
@@ -6508,48 +6431,6 @@
                   type: 'COLOR',
                   label: 'Label color',
                   key: 'labelColor',
-                  value: 'Black',
-                  configuration: {
-                    condition: {
-                      type: 'SHOW',
-                      option: 'styles',
-                      comparator: 'EQ',
-                      value: true,
-                    },
-                  },
-                },
-                {
-                  type: 'COLOR',
-                  label: 'Button text color',
-                  key: 'buttonTextColor',
-                  value: 'White',
-                  configuration: {
-                    condition: {
-                      type: 'SHOW',
-                      option: 'styles',
-                      comparator: 'EQ',
-                      value: true,
-                    },
-                  },
-                },
-                {
-                  type: 'COLOR',
-                  label: 'Button color',
-                  key: 'background',
-                  value: 'Primary',
-                  configuration: {
-                    condition: {
-                      type: 'SHOW',
-                      option: 'styles',
-                      comparator: 'EQ',
-                      value: true,
-                    },
-                  },
-                },
-                {
-                  type: 'COLOR',
-                  label: 'Text color',
-                  key: 'textColor',
                   value: 'Black',
                   configuration: {
                     condition: {
@@ -6623,7 +6504,247 @@
                   },
                 },
               ],
-              descendants: [],
+              descendants: [
+                {
+                  name: 'Button',
+                  style: {
+                    overwrite: {
+                      backgroundColor: {
+                        type: 'THEME_COLOR',
+                        value: 'primary',
+                      },
+                      boxShadow: 'none',
+                      color: {
+                        type: 'THEME_COLOR',
+                        value: 'white',
+                      },
+                      fontFamily: 'Roboto',
+                      fontSize: '0.875rem',
+                      fontStyle: 'none',
+                      fontWeight: '400',
+                      padding: ['0.6875rem', '1.375rem'],
+                      textDecoration: 'none',
+                      textTransform: 'none',
+                    },
+                  },
+                  options: [
+                    {
+                      label: 'Toggle visibility',
+                      key: 'visible',
+                      value: true,
+                      type: 'TOGGLE',
+                      configuration: {
+                        as: 'VISIBILITY',
+                      },
+                    },
+                    {
+                      type: 'VARIABLE',
+                      label: 'Button text',
+                      key: 'buttonText',
+                      value: ['Upload'],
+                    },
+                    {
+                      value: false,
+                      label: 'Full width',
+                      key: 'fullWidth',
+                      type: 'TOGGLE',
+                    },
+                    {
+                      label: 'Icon',
+                      key: 'icon',
+                      value: 'None',
+                      type: 'ICON',
+                    },
+                    {
+                      value: 'small',
+                      label: 'Icon size',
+                      key: 'size',
+                      type: 'CUSTOM',
+                      configuration: {
+                        as: 'BUTTONGROUP',
+                        dataType: 'string',
+                        allowedInput: [
+                          { name: 'Small', value: 'small' },
+                          { name: 'Medium', value: 'medium' },
+                          { name: 'Large', value: 'large' },
+                        ],
+                        condition: {
+                          type: 'HIDE',
+                          option: 'icon',
+                          comparator: 'EQ',
+                          value: 'None',
+                        },
+                      },
+                    },
+                    {
+                      type: 'CUSTOM',
+                      label: 'Icon position',
+                      key: 'iconPosition',
+                      value: 'start',
+                      configuration: {
+                        as: 'BUTTONGROUP',
+                        dataType: 'string',
+                        allowedInput: [
+                          { name: 'Start', value: 'start' },
+                          { name: 'End', value: 'end' },
+                        ],
+                        condition: {
+                          type: 'HIDE',
+                          option: 'icon',
+                          comparator: 'EQ',
+                          value: 'None',
+                        },
+                      },
+                    },
+                    {
+                      value: ['0rem', '0rem', '0rem', '0rem'],
+                      label: 'Outer space',
+                      key: 'outerSpacing',
+                      type: 'SIZES',
+                    },
+                    {
+                      label: 'Disabled',
+                      key: 'disabled',
+                      value: false,
+                      type: 'TOGGLE',
+                    },
+                    {
+                      label: 'Add Tooltip',
+                      key: 'addTooltip',
+                      value: false,
+                      type: 'TOGGLE',
+                    },
+                    {
+                      label: 'Toggle tooltip visibility',
+                      key: 'hasVisibleTooltip',
+                      value: true,
+                      type: 'TOGGLE',
+                      configuration: {
+                        as: 'VISIBILITY',
+                        condition: {
+                          type: 'SHOW',
+                          option: 'addTooltip',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                    {
+                      type: 'VARIABLE',
+                      label: 'Tooltip Content',
+                      key: 'tooltipContent',
+                      value: ['Tips'],
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'addTooltip',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                    {
+                      label: 'Tooltip Placement',
+                      key: 'tooltipPlacement',
+                      value: 'bottom',
+                      type: 'CUSTOM',
+                      configuration: {
+                        as: 'DROPDOWN',
+                        dataType: 'string',
+                        allowedInput: [
+                          {
+                            name: 'Top Start',
+                            value: 'top-start',
+                          },
+                          {
+                            name: 'Top',
+                            value: 'top',
+                          },
+                          {
+                            name: 'Top End',
+                            value: 'top-end',
+                          },
+                          {
+                            name: 'Right',
+                            value: 'right',
+                          },
+                          {
+                            name: 'Left',
+                            value: 'left',
+                          },
+                          {
+                            name: 'Botttom Start',
+                            value: 'bottom-start',
+                          },
+                          {
+                            name: 'Bottom',
+                            value: 'bottom',
+                          },
+                          {
+                            name: 'Bottom End',
+                            value: 'bottom-end',
+                          },
+                        ],
+                        condition: {
+                          type: 'SHOW',
+                          option: 'addTooltip',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                    {
+                      type: 'COLOR',
+                      label: 'Tooltip Background',
+                      key: 'tooltipBackground',
+                      value: 'Medium',
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'addTooltip',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                    {
+                      type: 'COLOR',
+                      label: 'Tooltip Text',
+                      key: 'tooltipText',
+                      value: 'Black',
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'addTooltip',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                    {
+                      value: false,
+                      label: 'Advanced settings',
+                      key: 'advancedSettings',
+                      type: 'TOGGLE',
+                    },
+                    {
+                      type: 'VARIABLE',
+                      label: 'Test attribute',
+                      key: 'dataComponentAttribute',
+                      value: ['Button'],
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'advancedSettings',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                  ],
+                  descendants: [],
+                },
+              ],
             };
           }
           case 'BOOLEAN': {
@@ -17641,6 +17762,70 @@
           property => property.name === 'id',
         );
 
+        const variables = [
+          {
+            kind: 'object',
+            name: 'delete_form_object',
+            ref: {
+              id: '#objectVariableId',
+              endpointId: '#endpointId',
+            },
+            options: {
+              modelId,
+            },
+          },
+          {
+            kind: 'construct',
+            name: 'form_data',
+            ref: {
+              id: '#customModelVariableId',
+              endpointId: '#createEndpointId',
+            },
+            options: {
+              modelId,
+              ref: {
+                customModelId: '#customModelVariableId',
+              },
+            },
+          },
+          {
+            kind: 'object',
+            name: `${data.model.label}_create_form_object`,
+            ref: {
+              id: '#createObjectVariableId',
+              endpointId: '#createEndpointId',
+            },
+            options: {
+              modelId,
+            },
+          },
+          {
+            kind: 'construct',
+            name: `${data.model.label}_edit_form_data`,
+            ref: {
+              id: '#editCustomModelVariableId',
+              endpointId: '#editEndpointId',
+            },
+            options: {
+              modelId,
+              ref: {
+                customModelId: '#editCustomModelId',
+              },
+            },
+          },
+          {
+            kind: 'object',
+            name: 'edit_form_object',
+            ref: {
+              id: '#editObjectVariableId',
+              endpointId: '#editEndpointId',
+            },
+            options: {
+              modelId,
+            },
+          },
+        ];
+
         const interactions = [
           {
             name: 'Hide',
@@ -21766,15 +21951,11 @@
         );
 
         if (data.model.label) {
-          newPrefab.variables[0].name = data.model.label;
+          variables[0].name = data.model.label;
         }
-        newPrefab.variables[0].options.modelId = modelId;
-        newPrefab.variables[1].options.modelId = modelId;
-        newPrefab.variables[2].options.modelId = modelId;
-        newPrefab.variables[3].options.modelId = modelId;
-        newPrefab.variables[4].options.modelId = modelId;
         newPrefab.structure[0].descendants = prefabStructure;
         newPrefab.interactions = interactions;
+        newPrefab.variables = variables;
         save(newPrefab);
       },
       buttons: () => (
@@ -21849,69 +22030,7 @@
     );
   },
   interactions: [],
-  variables: [
-    {
-      kind: 'object',
-      name: 'delete_form_object',
-      ref: {
-        id: '#objectVariableId',
-        endpointId: '#endpointId',
-      },
-      options: {
-        modelId: '',
-      },
-    },
-    {
-      kind: 'construct',
-      name: 'form_data',
-      ref: {
-        id: '#customModelVariableId',
-        endpointId: '#createEndpointId',
-      },
-      options: {
-        modelId: '',
-        ref: {
-          customModelId: '#customModelVariableId',
-        },
-      },
-    },
-    {
-      kind: 'object',
-      name: 'create_form_object',
-      ref: {
-        id: '#createObjectVariableId',
-        endpointId: '#createEndpointId',
-      },
-      options: {
-        modelId: '',
-      },
-    },
-    {
-      kind: 'construct',
-      name: 'edit_form_data',
-      ref: {
-        id: '#editCustomModelVariableId',
-        endpointId: '#editEndpointId',
-      },
-      options: {
-        modelId: '',
-        ref: {
-          customModelId: '#editCustomModelId',
-        },
-      },
-    },
-    {
-      kind: 'object',
-      name: 'edit_form_object',
-      ref: {
-        id: '#editObjectVariableId',
-        endpointId: '#editEndpointId',
-      },
-      options: {
-        modelId: '',
-      },
-    },
-  ],
+  variables: [],
   actions: [
     {
       name: 'Delete record action',
