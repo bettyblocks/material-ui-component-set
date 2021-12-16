50678cd7ffa321dda034cb3eea12c720b8bf1f03
Martijn Jansen <martijn.jansen@bettyblocks.com>
2021-12-16 11:07:54 +0100

    feat: adds submit button

---
+++ src/prefabs/submit.js
@@ -0,0 +1,19 @@
+(() => ({
+  category: 'FORM',
+  icon: 'ButtonIcon',
+  name: 'SubmitButton',
+  structure: [
+    {
+      name: 'SubmitButton',
+      options: [
+        {
+          key: 'label',
+          label: 'Label',
+          type: 'TEXT',
+          value: 'Submit',
+        },
+      ],
+      descendants: [],
+    },
+  ],
+}))();
