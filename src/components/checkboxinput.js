e013d2330c513298ea6b2c151bb23ca99c5315b0
kagia <benjamin.kagia@bettyblocks.com>
2022-01-05 17:17:37 +0300

    feat: restore field functionality

--- src/components/checkboxinput.js
+++ src/components/checkboxinput.js
@@ -4,52 +4,246 @@
   allowedTypes: [],
   orientation: 'HORIZONTAL',
   jsx: (() => {
-    const { actionVariableId: name, label, value } = options;
-    const isDev = B.env === 'dev';
-    const valueText = B.useText(value);
+    const {
+      label,
+      actionVariableId: name,
+      required,
+      disabled,
+      position,
+      size,
+      helperText = [''],
+      validationValueMissing = [''],
+      isSwitch,
+      dataComponentAttribute = ['Checkbox'],
+    } = options;
+    const { env, useText } = B;
+    const isDev = env === 'dev';
 
-    const Input = () => (
-      <div className={[classes.formField, isDev ? classes.dev : ''].join(' ')}>
-        <label htmlFor={name}>{label}</label>
-        <input
-          className={classes.input}
-          checked={valueText === 'true'}
-          id={name}
-          name={name}
-          type="checkbox"
-        />
-      </div>
+    const [errorState, setErrorState] = useState(false);
+    const [helper, setHelper] = useState(useText(helperText));
+
+    const mounted = useRef(false);
+    const labelText = label;
+    const componentChecked = 'false';
+    const [checked, setChecked] = useState(componentChecked === 'true');
+    const dataComponentAttributeValue = useText(dataComponentAttribute);
+    const helperTextResolved = useText(helperText);
+    const validationValueMissingText = useText(validationValueMissing);
+
+    const {
+      Checkbox: MUICheckbox,
+      Switch,
+      FormControlLabel,
+      FormControl,
+      FormHelperText,
+    } = window.MaterialUI.Core;
+
+    B.defineFunction('Uncheck', () => setChecked(false));
+    B.defineFunction('Check', () => setChecked(true));
+    B.defineFunction('Check/Uncheck', () => setChecked(prev => !prev));
+
+    const handleValidation = isValid => {
+      setErrorState(!isValid);
+      const message = !isValid
+        ? validationValueMissingText
+        : helperTextResolved;
+      setHelper(message);
+    };
+
+    const handleChange = evt => {
+      const isChecked = evt.target.checked;
+      const isValid = (isChecked && required) || !required;
+      handleValidation(isValid);
+      setChecked(evt.target.checked);
+    };
+
+    const invalidHandler = event => {
+      event.preventDefault();
+      const {
+        target: {
+          validity: { valid: isValid },
+        },
+      } = event;
+      handleValidation(isValid);
+    };
+
+    B.defineFunction('Reset', () => setChecked(componentChecked === 'true'));
+
+    useEffect(() => {
+      if (checked) {
+        B.triggerEvent('isTrue', true);
+      } else {
+        B.triggerEvent('isFalse', false);
+      }
+      if (mounted.current) {
+        B.triggerEvent('onChange', checked);
+      }
+    }, [checked]);
+
+    useEffect(() => {
+      mounted.current = true;
+      return () => {
+        mounted.current = false;
+      };
+    }, []);
+
+    useEffect(() => {
+      if (isDev) {
+        setChecked(componentChecked === 'true');
+        setHelper(helperTextResolved);
+      }
+    }, [isDev, helperTextResolved]);
+
+    const props = {
+      checked,
+      required,
+      onInvalid: invalidHandler,
+      onChange: handleChange,
+      name,
+      disabled,
+      size,
+      tabIndex: isDev ? -1 : undefined,
+      value: 'on',
+      'data-component': dataComponentAttributeValue,
+    };
+
+    const Checkbox = <MUICheckbox {...props} />;
+    const SwitchComponent = <Switch {...props} />;
+
+    const ControlLabel = (
+      <>
+        {labelText}
+        {required ? (
+          <span className={errorState ? classes.formControlRequired : null}>
+            {' '}
+            *
+          </span>
+        ) : (
+          ''
+        )}
+      </>
     );
 
-    if (B.env !== 'prod') {
-      return (
-        <div>
-          <Input />
-        </div>
-      );
-    }
-
-    return <Input />;
+    const Control = (
+      <FormControl
+        required={required}
+        error={errorState}
+        classes={{ root: classes.formControl }}
+      >
+        <FormControlLabel
+          control={isSwitch ? SwitchComponent : Checkbox}
+          label={ControlLabel}
+          labelPlacement={position}
+        />
+        {!!helper && <FormHelperText>{helper}</FormHelperText>}
+      </FormControl>
+    );
+    return isDev ? <div className={classes.root}>{Control}</div> : Control;
   })(),
-  styles: () => ({
-    dev: {
-      '& > *': {
-        pointerEvents: 'none',
+  styles: B => t => {
+    const { color: colorFunc, Styling } = B;
+    const style = new Styling(t);
+    const getOpacColor = (col, val) => colorFunc.alpha(col, val);
+    return {
+      root: {
+        '& > *': {
+          pointerEvents: 'none',
+        },
       },
-    },
-    formField: {
-      display: 'flex',
-      flexDirection: 'column',
-      marginTop: '0.75rem',
-    },
-    input: {
-      border: '1px solid #DDD',
-      borderRadius: '3px',
-      fontSize: '1rem',
-      height: '1.125rem',
-      lineHeight: '1.125rem',
-      margin: 0,
-      padding: '1.125rem 1rem',
-    },
-  }),
+      formControlRequired: {
+        color: ({ options: { errorColor } }) => [
+          style.getColor(errorColor),
+          '!important',
+        ],
+      },
+      formControl: {
+        '& > legend': {
+          '&.Mui-error': {
+            color: ({ options: { errorColor } }) => [
+              style.getColor(errorColor),
+              '!important',
+            ],
+          },
+          '&.Mui-disabled': {
+            pointerEvents: 'none',
+            opacity: '0.7',
+          },
+        },
+        '& > p': {
+          color: ({ options: { helperColor } }) => [
+            style.getColor(helperColor),
+            '!important',
+          ],
+          '&.Mui-error': {
+            color: ({ options: { errorColor } }) => [
+              style.getColor(errorColor),
+              '!important',
+            ],
+          },
+        },
+        '& .MuiFormControlLabel-root': {
+          '& .MuiCheckbox-root': {
+            color: ({ options: { checkboxColor } }) => [
+              style.getColor(checkboxColor),
+              '!important',
+            ],
+            '&:hover': {
+              backgroundColor: ({ options: { checkboxColor } }) => [
+                getOpacColor(style.getColor(checkboxColor), 0.04),
+                '!important',
+              ],
+            },
+            '&.Mui-checked': {
+              color: ({ options: { checkboxColorChecked } }) => [
+                style.getColor(checkboxColorChecked),
+                '!important',
+              ],
+              '&:hover': {
+                backgroundColor: ({ options: { checkboxColorChecked } }) => [
+                  getOpacColor(style.getColor(checkboxColorChecked), 0.04),
+                  '!important',
+                ],
+              },
+            },
+          },
+          '& .MuiSwitch-root': {
+            '& .MuiSwitch-track': {
+              backgroundColor: ({ options: { checkboxColor } }) => [
+                style.getColor(checkboxColor),
+                '!important',
+              ],
+            },
+            '& .Mui-checked': {
+              color: ({ options: { checkboxColorChecked } }) => [
+                style.getColor(checkboxColorChecked),
+                '!important',
+              ],
+              '&:hover': {
+                backgroundColor: ({ options: { checkboxColorChecked } }) => [
+                  getOpacColor(style.getColor(checkboxColorChecked), 0.04),
+                  '!important',
+                ],
+              },
+            },
+            '& .Mui-checked ~ .MuiSwitch-track': {
+              backgroundColor: ({ options: { checkboxColorChecked } }) => [
+                style.getColor(checkboxColorChecked),
+                '!important',
+              ],
+            },
+          },
+          '& .MuiTypography-root': {
+            color: ({ options: { textColor } }) => [
+              style.getColor(textColor),
+              '!important',
+            ],
+          },
+          '&.Mui-disabled': {
+            pointerEvents: 'none',
+            opacity: '0.7',
+          },
+        },
+      },
+    };
+  },
 }))();
--- src/prefabs/checkboxinput.js
+++ src/prefabs/checkboxinput.js
@@ -19,10 +19,182 @@
           value: '',
         },
         {
-          key: 'value',
-          label: 'Value',
+          label: 'Label Position',
+          key: 'position',
+          value: 'end',
+          type: 'CUSTOM',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              { name: 'Start', value: 'start' },
+              { name: 'End', value: 'end' },
+              { name: 'Top', value: 'top' },
+              { name: 'Bottom', value: 'bottom' },
+            ],
+          },
+        },
+        {
+          value: false,
+          label: 'Required',
+          key: 'required',
+          type: 'TOGGLE',
+        },
+        {
+          value: false,
+          label: 'Validation options',
+          key: 'validationOptions',
+          type: 'TOGGLE',
+        },
+        {
+          value: ['This field is required'],
+          label: 'Value required message',
+          key: 'validationValueMissing',
           type: 'VARIABLE',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'validationOptions',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          type: 'TOGGLE',
+          label: 'Disabled',
+          key: 'disabled',
+          value: false,
+        },
+        {
           value: [],
+          label: 'Helper text',
+          key: 'helperText',
+          type: 'VARIABLE',
+        },
+        {
+          label: 'Size',
+          key: 'size',
+          value: 'medium',
+          type: 'CUSTOM',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              { name: 'Medium', value: 'medium' },
+              { name: 'Small', value: 'small' },
+            ],
+          },
+        },
+        {
+          value: false,
+          label: 'Styles',
+          key: 'styles',
+          type: 'TOGGLE',
+        },
+        {
+          type: 'COLOR',
+          label: 'Checkbox color',
+          key: 'checkboxColor',
+          value: 'Accent3',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'styles',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          type: 'COLOR',
+          label: 'Checkbox color checked',
+          key: 'checkboxColorChecked',
+          value: 'Primary',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'styles',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          type: 'COLOR',
+          label: 'Text color',
+          key: 'textColor',
+          value: 'Black',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'styles',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          type: 'COLOR',
+          label: 'Helper color',
+          key: 'helperColor',
+          value: 'Accent2',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'styles',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          type: 'COLOR',
+          label: 'Error color',
+          key: 'errorColor',
+          value: 'Danger',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'styles',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          value: false,
+          label: 'Advanced settings',
+          key: 'advancedSettings',
+          type: 'TOGGLE',
+        },
+        {
+          type: 'VARIABLE',
+          label: 'name attribute',
+          key: 'nameAttribute',
+          value: [],
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'advancedSettings',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+        {
+          type: 'VARIABLE',
+          label: 'Test attribute',
+          key: 'dataComponentAttribute',
+          value: ['Checkbox'],
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'advancedSettings',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
         },
       ],
       // $onUpdate: [
