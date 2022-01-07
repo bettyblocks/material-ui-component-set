eed14459e2ba05150043a1142f460a4b6e40decc
michel ryan dolstra <michel.dolstra@bettyblocks.com>
2022-01-07 12:29:03 +0100

    fix: use UseEffect to ensure the hiddeninput value is updated

--- src/components/autoComplete.js
+++ src/components/autoComplete.js
@@ -723,7 +723,11 @@
      * The hidden input is used when `optionType` is set to `model`. Then the `valueProperty` options is used to determine what is send to the backend when a from is submitted.
      */
 
-    const currentValue = getValue();
+    let currentValue;
+
+    useEffect(() => {
+      currentValue = getValue();
+    }, [getValue]);
 
     // In the first render we want to make sure to convert the default value
     if (!multiple && !inputValue && currentValue) {
