d424ebf9d84d67d20e8819df4ed7b779e8278270
kagia <benjamin.kagia@bettyblocks.com>
2022-02-11 13:52:47 +0100

    feat: update to latest sdk 1.4.1

--- src/prefabs/options/defaultStylesOptions.ts
+++ src/prefabs/options/defaultStylesOptions.ts
@@ -1,4 +1,4 @@
-import { showIfTrue, color, toggle } from '@betty-blocks/component-sdk';
+import { showIfTrue, color, toggle, ThemeColor } from '@betty-blocks/component-sdk';
 
 const stylesAttrs = {
   configuration: { condition: showIfTrue('styles') },
@@ -7,40 +7,40 @@ const stylesAttrs = {
 export const defaultStylesOptions = {
   styles: toggle('Styles'),
   backgroundColor: color('Background color', {
-    value: 'White',
+    value: ThemeColor.WHITE,
     ...stylesAttrs,
   }),
   borderColor: color('Border color', {
-    value: 'Accent1',
+    value: ThemeColor.ACCENT_1,
     ...stylesAttrs,
   }),
   borderHoverColor: color('Border color (hover)', {
-    value: 'Black',
+    value: ThemeColor.BLACK,
     ...stylesAttrs,
   }),
   borderFocusColor: color('Border color (focus)', {
-    value: 'Primary',
+    value: ThemeColor.PRIMARY,
     ...stylesAttrs,
   }),
   hideLabel: toggle('Hide label', stylesAttrs),
   labelColor: color('Label color', {
-    value: 'Accent3',
+    value: ThemeColor.ACCENT_3,
     ...stylesAttrs,
   }),
   textColor: color('Text color', {
-    value: 'Black',
+    value: ThemeColor.BLACK,
     ...stylesAttrs,
   }),
   placeholderColor: color('Placeholder color', {
-    value: 'Light',
+    value: ThemeColor.LIGHT,
     ...stylesAttrs,
   }),
   helperColor: color('Helper color', {
-    value: 'Accent2',
+    value: ThemeColor.ACCENT_2,
     ...stylesAttrs,
   }),
   errorColor: color('Error color', {
-    value: 'Danger',
+    value: ThemeColor.DANGER,
     ...stylesAttrs,
   }),
 };
