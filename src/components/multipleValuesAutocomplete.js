2934946b216dea3b1c68f6996843ede944ff511c
Joris Pannekeet <joris.pannekeet@bettyblocks.com>
2022-02-18 11:17:53 +0100

    fix: fixed autocomplete value error

--- src/components/multipleValuesAutocomplete.js
+++ src/components/multipleValuesAutocomplete.js
@@ -718,7 +718,7 @@
             );
           }}
           onInputChange={(event, newValue) => {
-            let validation = event.target.validity;
+            let validation = event ? event.target.validity : null;
             if (isNumberType) {
               validation = customPatternValidation(event.target);
             }
