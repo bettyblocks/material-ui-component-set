5f76466704da9685aeb10633abedbcf1fb84e22c
Joris Pannekeet <joris.pannekeet@bettyblocks.com>
2022-04-28 13:42:47 +0200

    fix: fixed proper required option placement

--- src/prefabs/structures/RadioInput/options/validation.ts
+++ src/prefabs/structures/RadioInput/options/validation.ts
@@ -2,8 +2,10 @@ import { variable, toggle } from '@betty-blocks/component-sdk';
 import { showOn } from '../../../../utils';
 
 export const validation = {
-  required: toggle('Required'),
   validationOptions: toggle('Validation options'),
+  required: toggle('Required', {
+    ...showOn('validationOptions'),
+  }),
   validationValueMissing: variable('Value required message', {
     value: ['This field is required'],
     ...showOn('validationOptions'),
