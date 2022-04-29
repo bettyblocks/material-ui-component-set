80de0ff96c2741c290f276270f5b73a892060e10
Sander <sander.vanriessen@bettyblocks.com>
2022-04-29 09:56:41 +0200

    chore: add new partial prefab

---
+++ src/prefabs/partialTest.js
@@ -0,0 +1,326 @@
+(() => ({
+  name: 'Partial',
+  type: 'PARTIAL',
+  beforeCreate: ({
+    prefab,
+    save,
+    close,
+    components: { Header, Content, PartialSelector, Footer },
+  }) => {
+    const [partialId, setPartialId] = React.useState('');
+
+    return (
+      <>
+        <Header onClose={close} title="Configure open page button" />
+        <Content>
+          <PartialSelector
+            onChange={(value) => setPartialId(value)}
+            value={partialId}
+          />
+        </Content>
+        <Footer
+          onclick={close}
+          onSave={() => {
+            const newPrefab = { ...prefab };
+            newPrefab.descendants[0].descendants[0].partialId = partialId;
+            newPrefab.descendants[0].descendants[0].options[0].value =
+              partialId;
+            save(newPrefab);
+          }}
+          onSkip={() => {
+            const newPrefab = { ...prefab };
+            save(newPrefab);
+          }}
+        />
+      </>
+    );
+  },
+  structure: [
+    {
+      name: 'Row',
+      options: [
+        {
+          type: 'CUSTOM',
+          label: 'Width',
+          key: 'maxRowWidth',
+          value: 'XL',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              { name: 'S', value: 'S' },
+              { name: 'M', value: 'M' },
+              { name: 'L', value: 'L' },
+              { name: 'XL', value: 'XL' },
+              { name: 'Full', value: 'Full' },
+            ],
+          },
+        },
+        {
+          value: '',
+          label: 'Height',
+          key: 'rowHeight',
+          type: 'TEXT',
+          configuration: {
+            as: 'UNIT',
+          },
+        },
+        {
+          value: 'transparent',
+          label: 'Background color',
+          key: 'backgroundColor',
+          type: 'COLOR',
+        },
+        {
+          value: ['0rem', '0rem', '0rem', '0rem'],
+          label: 'Outer space',
+          key: 'outerSpacing',
+          type: 'SIZES',
+        },
+        {
+          value: false,
+          label: 'Advanced settings',
+          key: 'advancedSettings',
+          type: 'TOGGLE',
+        },
+        {
+          type: 'VARIABLE',
+          label: 'Test attribute',
+          key: 'dataComponentAttribute',
+          value: ['Row'],
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'advancedSettings',
+              comparator: 'EQ',
+              value: true,
+            },
+          },
+        },
+      ],
+      descendants: [
+        {
+          name: 'Column',
+          options: [
+            {
+              label: 'Toggle visibility',
+              key: 'visible',
+              value: true,
+              type: 'TOGGLE',
+              configuration: {
+                as: 'VISIBILITY',
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width',
+              key: 'columnWidth',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width (tablet landscape)',
+              key: 'columnWidthTabletLandscape',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width (tablet portrait)',
+              key: 'columnWidthTabletPortrait',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: 'flexible',
+              label: 'Column width (mobile)',
+              key: 'columnWidthMobile',
+              type: 'CUSTOM',
+              configuration: {
+                as: 'DROPDOWN',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'Fit content', value: 'fitContent' },
+                  { name: 'Flexible', value: 'flexible' },
+                  { name: 'Hidden', value: 'hidden' },
+                  { name: '1', value: '1' },
+                  { name: '2', value: '2' },
+                  { name: '3', value: '3' },
+                  { name: '4', value: '4' },
+                  { name: '5', value: '5' },
+                  { name: '6', value: '6' },
+                  { name: '7', value: '7' },
+                  { name: '8', value: '8' },
+                  { name: '9', value: '9' },
+                  { name: '10', value: '10' },
+                  { name: '11', value: '11' },
+                  { name: '12', value: '12' },
+                ],
+              },
+            },
+            {
+              value: '',
+              label: 'Height',
+              key: 'columnHeight',
+              type: 'TEXT',
+              configuration: {
+                as: 'UNIT',
+              },
+            },
+            {
+              value: 'transparent',
+              label: 'Background color',
+              key: 'backgroundColor',
+              type: 'COLOR',
+            },
+            {
+              type: 'CUSTOM',
+              label: 'Horizontal Alignment',
+              key: 'horizontalAlignment',
+              value: 'inherit',
+              configuration: {
+                as: 'BUTTONGROUP',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'None', value: 'inherit' },
+                  { name: 'Left', value: 'flex-start' },
+                  { name: 'Center', value: 'center' },
+                  { name: 'Right', value: 'flex-end' },
+                ],
+              },
+            },
+            {
+              type: 'CUSTOM',
+              label: 'Vertical Alignment',
+              key: 'verticalAlignment',
+              value: 'inherit',
+              configuration: {
+                as: 'BUTTONGROUP',
+                dataType: 'string',
+                allowedInput: [
+                  { name: 'None', value: 'inherit' },
+                  { name: 'Top', value: 'flex-start' },
+                  { name: 'Center', value: 'center' },
+                  { name: 'Bottom', value: 'flex-end' },
+                ],
+              },
+            },
+            {
+              value: ['0rem', '0rem', '0rem', '0rem'],
+              label: 'Outer space',
+              key: 'outerSpacing',
+              type: 'SIZES',
+            },
+            {
+              value: ['M', 'M', 'M', 'M'],
+              label: 'Inner space',
+              key: 'innerSpacing',
+              type: 'SIZES',
+            },
+            {
+              value: false,
+              label: 'Advanced settings',
+              key: 'advancedSettings',
+              type: 'TOGGLE',
+            },
+            {
+              type: 'VARIABLE',
+              label: 'Test attribute',
+              key: 'dataComponentAttribute',
+              value: ['Column'],
+              configuration: {
+                condition: {
+                  type: 'SHOW',
+                  option: 'advancedSettings',
+                  comparator: 'EQ',
+                  value: true,
+                },
+              },
+            },
+          ],
+          descendants: [
+            {
+              name: 'partial',
+              type: 'PARTIAL',
+              options: [
+                {
+                  type: 'PARTIAL_REFERENCE',
+                  label: 'Partial Reference',
+                  key: 'partialReferenceId',
+                  value: '""',
+                },
+                {
+                  type: 'PARTIAL_INPUT_OBJECTS',
+                  label: 'Partial inputs',
+                  key: 'partialInputMapping',
+                  value: '{}',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}))();
