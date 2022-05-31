b9efd18a7c358067cb6967ccd80ed3c123fc8fb3
Dylan Carver <dylan.carver@bettyblocks.com>
2022-05-31 16:05:31 +0200

    Revert "Revert "release""
    This reverts commit 831a931433812e6759ecaa2e7dd1d5fec6d45ba4.

--- src/components/multi&CustomAutocomplete.js
+++ src/components/multi&CustomAutocomplete.js
@@ -323,11 +323,11 @@
     /* eslint-disable no-underscore-dangle */
     if (multiple) {
       if (debouncedInputValue) {
-        if (!filter._or) {
-          filter._or = [];
+        if (!filter._and) {
+          filter._and = [];
         }
 
-        filter._or.push({
+        filter._and.push({
           [suggestionsProp.name]: {
             [searchPropIsNumber ? 'eq' : 'regex']: searchPropIsNumber
               ? parseInt(debouncedInputValue, 10)
