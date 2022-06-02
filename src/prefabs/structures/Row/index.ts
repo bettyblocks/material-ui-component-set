65645f9d8763ffe6955705bf70ec7b73467f41fd
Brian van den Berg <brian.vandenberg@bettyblocks.com>
2022-06-02 13:43:17 +0200

    chore: converted 1column component to tsx

---
+++ src/prefabs/structures/Row/index.ts
@@ -0,0 +1,17 @@
+import { component, PrefabComponentOption } from '@betty-blocks/component-sdk';
+import { PrefabComponent } from '@betty-blocks/component-sdk/build/prefabs/types/component';
+import { options as defaults } from './options';
+
+type OptionProducer = (key: string) => PrefabComponentOption;
+
+export interface Configuration {
+  label?: string;
+  options?: Record<string, OptionProducer>;
+}
+export const Row = (
+  config: Configuration,
+  descendants: PrefabComponent[] = [],
+) => {
+  const options = { ...(config.options || defaults) };
+  return component('Row', { options }, descendants);
+};
