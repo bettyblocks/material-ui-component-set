67edac5f98ef02b4ddbc008fa499f5406282cdcf
robbertruiter <robbert.ruiter@bettyblocks.com>
2022-06-15 11:46:15 +0200

    fix: icon background moves with the page now

--- src/prefabs/pageWithSimpleDashboard.js
+++ src/prefabs/pageWithSimpleDashboard.js
@@ -1189,7 +1189,7 @@
                   type: 'TOGGLE',
                 },
                 {
-                  value: 'absolute',
+                  value: 'relative',
                   label: 'Position',
                   key: 'position',
                   type: 'CUSTOM',
@@ -1581,264 +1581,265 @@
                   },
                 },
               ],
-              descendants: [],
-            },
-            {
-              name: 'Avatar',
-              options: [
+              descendants: [
                 {
-                  type: 'CUSTOM',
-                  label: 'Type',
-                  key: 'type',
-                  value: 'icon',
-                  configuration: {
-                    as: 'BUTTONGROUP',
-                    dataType: 'string',
-                    allowedInput: [
-                      {
-                        name: 'Image',
-                        value: 'img',
-                      },
-                      {
-                        name: 'Letter',
-                        value: 'letter',
-                      },
-                      {
-                        name: 'Icon',
-                        value: 'icon',
-                      },
-                    ],
-                  },
-                },
-                {
-                  value: [],
-                  label: 'Image url',
-                  key: 'imgUrl',
-                  type: 'VARIABLE',
-                  configuration: {
-                    condition: {
-                      type: 'SHOW',
-                      option: 'type',
-                      comparator: 'EQ',
-                      value: 'img',
-                    },
-                  },
-                },
-                {
-                  value: [],
-                  label: 'Image alternative text',
-                  key: 'imgAlt',
-                  type: 'VARIABLE',
-                  configuration: {
-                    condition: {
-                      type: 'SHOW',
-                      option: 'type',
-                      comparator: 'EQ',
-                      value: 'img',
-                    },
-                  },
-                },
-                {
-                  value: [],
-                  label: 'Letter',
-                  key: 'letter',
-                  type: 'VARIABLE',
-                  configuration: {
-                    condition: {
-                      type: 'SHOW',
-                      option: 'type',
-                      comparator: 'EQ',
-                      value: 'letter',
-                    },
-                  },
-                },
-                {
-                  label: 'Icon',
-                  key: 'icon',
-                  value: statusIcon,
-                  type: 'ICON',
-                  configuration: {
-                    condition: {
-                      type: 'SHOW',
-                      option: 'type',
-                      comparator: 'EQ',
+                  name: 'Avatar',
+                  options: [
+                    {
+                      type: 'CUSTOM',
+                      label: 'Type',
+                      key: 'type',
                       value: 'icon',
+                      configuration: {
+                        as: 'BUTTONGROUP',
+                        dataType: 'string',
+                        allowedInput: [
+                          {
+                            name: 'Image',
+                            value: 'img',
+                          },
+                          {
+                            name: 'Letter',
+                            value: 'letter',
+                          },
+                          {
+                            name: 'Icon',
+                            value: 'icon',
+                          },
+                        ],
+                      },
                     },
-                  },
-                },
-                {
-                  type: 'CUSTOM',
-                  label: 'Variant',
-                  key: 'variant',
-                  value: 'circle',
-                  configuration: {
-                    as: 'BUTTONGROUP',
-                    dataType: 'string',
-                    allowedInput: [
-                      {
-                        name: 'Circle',
-                        value: 'circle',
+                    {
+                      value: [],
+                      label: 'Image url',
+                      key: 'imgUrl',
+                      type: 'VARIABLE',
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'type',
+                          comparator: 'EQ',
+                          value: 'img',
+                        },
                       },
-                      {
-                        name: 'Rounded',
-                        value: 'rounded',
-                      },
-                      {
-                        name: 'Square',
-                        value: 'square',
-                      },
-                    ],
-                  },
-                },
-                {
-                  type: 'COLOR',
-                  label: 'Background color',
-                  key: 'backgroundColor',
-                  value: 'Transparent',
-                  configuration: {
-                    condition: {
-                      type: 'HIDE',
-                      option: 'type',
-                      comparator: 'EQ',
-                      value: 'img',
                     },
-                  },
-                },
-                {
-                  type: 'SIZE',
-                  label: 'Width',
-                  key: 'width',
-                  value: '40px',
-                  configuration: {
-                    as: 'UNIT',
-                  },
-                },
-                {
-                  type: 'SIZE',
-                  label: 'Height',
-                  key: 'height',
-                  value: '40px',
-                  configuration: {
-                    as: 'UNIT',
-                  },
-                },
-                {
-                  type: 'SIZES',
-                  label: 'Outer Space',
-                  key: 'margin',
-                  value: ['M', 'M', 'M', 'M'],
-                },
-                {
-                  value: true,
-                  label: 'Styles',
-                  key: 'styles',
-                  type: 'TOGGLE',
-                },
-                {
-                  value: '1.25rem',
-                  label: 'Font Size',
-                  key: 'fontSize',
-                  type: 'TEXT',
-                  configuration: {
-                    as: 'UNIT',
-                    condition: {
-                      type: 'SHOW',
-                      option: 'styles',
-                      comparator: 'EQ',
+                    {
+                      value: [],
+                      label: 'Image alternative text',
+                      key: 'imgAlt',
+                      type: 'VARIABLE',
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'type',
+                          comparator: 'EQ',
+                          value: 'img',
+                        },
+                      },
+                    },
+                    {
+                      value: [],
+                      label: 'Letter',
+                      key: 'letter',
+                      type: 'VARIABLE',
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'type',
+                          comparator: 'EQ',
+                          value: 'letter',
+                        },
+                      },
+                    },
+                    {
+                      label: 'Icon',
+                      key: 'icon',
+                      value: statusIcon,
+                      type: 'ICON',
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'type',
+                          comparator: 'EQ',
+                          value: 'icon',
+                        },
+                      },
+                    },
+                    {
+                      type: 'CUSTOM',
+                      label: 'Variant',
+                      key: 'variant',
+                      value: 'circle',
+                      configuration: {
+                        as: 'BUTTONGROUP',
+                        dataType: 'string',
+                        allowedInput: [
+                          {
+                            name: 'Circle',
+                            value: 'circle',
+                          },
+                          {
+                            name: 'Rounded',
+                            value: 'rounded',
+                          },
+                          {
+                            name: 'Square',
+                            value: 'square',
+                          },
+                        ],
+                      },
+                    },
+                    {
+                      type: 'COLOR',
+                      label: 'Background color',
+                      key: 'backgroundColor',
+                      value: 'Transparent',
+                      configuration: {
+                        condition: {
+                          type: 'HIDE',
+                          option: 'type',
+                          comparator: 'EQ',
+                          value: 'img',
+                        },
+                      },
+                    },
+                    {
+                      type: 'SIZE',
+                      label: 'Width',
+                      key: 'width',
+                      value: '48px',
+                      configuration: {
+                        as: 'UNIT',
+                      },
+                    },
+                    {
+                      type: 'SIZE',
+                      label: 'Height',
+                      key: 'height',
+                      value: '48px',
+                      configuration: {
+                        as: 'UNIT',
+                      },
+                    },
+                    {
+                      type: 'SIZES',
+                      label: 'Outer Space',
+                      key: 'margin',
+                      value: ['M', 'M', 'M', 'M'],
+                    },
+                    {
                       value: true,
+                      label: 'Styles',
+                      key: 'styles',
+                      type: 'TOGGLE',
                     },
-                  },
-                },
-                {
-                  type: 'COLOR',
-                  label: 'Text color',
-                  key: 'textColor',
-                  value: statusColor,
-                  configuration: {
-                    condition: {
-                      type: 'SHOW',
-                      option: 'styles',
-                      comparator: 'EQ',
-                      value: true,
+                    {
+                      value: '1.25rem',
+                      label: 'Font Size',
+                      key: 'fontSize',
+                      type: 'TEXT',
+                      configuration: {
+                        as: 'UNIT',
+                        condition: {
+                          type: 'SHOW',
+                          option: 'styles',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
                     },
-                  },
-                },
-                {
-                  type: 'CUSTOM',
-                  label: 'Font weight',
-                  key: 'fontWeight',
-                  value: '400',
-                  configuration: {
-                    as: 'DROPDOWN',
-                    dataType: 'string',
-                    allowedInput: [
-                      {
-                        name: '100',
-                        value: '100',
+                    {
+                      type: 'COLOR',
+                      label: 'Text color',
+                      key: 'textColor',
+                      value: statusColor,
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'styles',
+                          comparator: 'EQ',
+                          value: true,
+                        },
                       },
-                      {
-                        name: '200',
-                        value: '200',
-                      },
-                      {
-                        name: '300',
-                        value: '300',
-                      },
-                      {
-                        name: '400',
-                        value: '400',
-                      },
-                      {
-                        name: '500',
-                        value: '500',
-                      },
-                      {
-                        name: '600',
-                        value: '600',
-                      },
-                      {
-                        name: '700',
-                        value: '700',
-                      },
-                      {
-                        name: '800',
-                        value: '800',
-                      },
-                      {
-                        name: '900',
-                        value: '900',
-                      },
-                    ],
-                    condition: {
-                      type: 'SHOW',
-                      option: 'styles',
-                      comparator: 'EQ',
-                      value: true,
                     },
-                  },
-                },
-                {
-                  value: false,
-                  label: 'Advanced settings',
-                  key: 'advancedSettings',
-                  type: 'TOGGLE',
-                },
-                {
-                  type: 'VARIABLE',
-                  label: 'Test attribute',
-                  key: 'dataComponentAttribute',
-                  value: ['Avatar'],
-                  configuration: {
-                    condition: {
-                      type: 'SHOW',
-                      option: 'advancedSettings',
-                      comparator: 'EQ',
-                      value: true,
+                    {
+                      type: 'CUSTOM',
+                      label: 'Font weight',
+                      key: 'fontWeight',
+                      value: '400',
+                      configuration: {
+                        as: 'DROPDOWN',
+                        dataType: 'string',
+                        allowedInput: [
+                          {
+                            name: '100',
+                            value: '100',
+                          },
+                          {
+                            name: '200',
+                            value: '200',
+                          },
+                          {
+                            name: '300',
+                            value: '300',
+                          },
+                          {
+                            name: '400',
+                            value: '400',
+                          },
+                          {
+                            name: '500',
+                            value: '500',
+                          },
+                          {
+                            name: '600',
+                            value: '600',
+                          },
+                          {
+                            name: '700',
+                            value: '700',
+                          },
+                          {
+                            name: '800',
+                            value: '800',
+                          },
+                          {
+                            name: '900',
+                            value: '900',
+                          },
+                        ],
+                        condition: {
+                          type: 'SHOW',
+                          option: 'styles',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
                     },
-                  },
+                    {
+                      value: false,
+                      label: 'Advanced settings',
+                      key: 'advancedSettings',
+                      type: 'TOGGLE',
+                    },
+                    {
+                      type: 'VARIABLE',
+                      label: 'Test attribute',
+                      key: 'dataComponentAttribute',
+                      value: ['Avatar'],
+                      configuration: {
+                        condition: {
+                          type: 'SHOW',
+                          option: 'advancedSettings',
+                          comparator: 'EQ',
+                          value: true,
+                        },
+                      },
+                    },
+                  ],
+                  descendants: [],
                 },
               ],
-              descendants: [],
             },
           ],
         },
