5dfcfaa9b46dd303aa1a50e84cf3902c4603a56d
michel <michel.dolstra@bettyblocks.com>
2022-07-05 14:45:18 +0200

    fix: test tsx component

---
+++ src/prefabs/InnerWrapper.tsx
@@ -0,0 +1,94 @@
+import {
+  color,
+  Icon,
+  linked,
+  prefab,
+  ThemeColor,
+  wrapper,
+} from '@betty-blocks/component-sdk';
+import { Alert } from './structures/Alert';
+import { options as defaults } from './structures/Alert/options';
+import { updateOption } from '../utils';
+
+const attributes = {
+  name: 'InnerWrapperTSX',
+  category: 'LAYOUT',
+  icon: Icon.RowColumnIcon,
+  keywords: ['Layout', 'column', 'columns', '1'],
+};
+
+const optionRefs = {
+  ...defaults,
+  background: color('Background color', {
+    ref: {
+      id: '#AlertOption1',
+    },
+    value: ThemeColor.DANGER,
+  }),
+  icon: updateOption(defaults.icon, {
+    ref: {
+      id: '#AlertOption2',
+    },
+    value: 'Error',
+  }),
+  titleText: updateOption(defaults.titleText, {
+    ref: {
+      id: '#AlertOption3',
+    },
+    value: ['Error'],
+  }),
+  bodyText: updateOption(defaults.bodyText, {
+    ref: {
+      id: '#AlertOption4',
+    },
+    value: ['*Dynamic value from the Action response*'],
+  }),
+};
+
+export default prefab('InnerWrapperTSX', attributes, undefined, [
+  wrapper(
+    {
+      label: 'outer wrapper',
+      options: [
+        linked('linked1', {
+          ref: {
+            value: {
+              componentId: '#component1',
+              optionId: '#AlertOption1',
+            },
+          },
+        }),
+        linked('linked1', {
+          ref: {
+            value: {
+              componentId: '#component1',
+              optionId: '#AlertOption2',
+            },
+          },
+        }),
+        linked('linked1', {
+          ref: {
+            value: {
+              componentId: '#component1',
+              optionId: '#AlertOption3',
+            },
+          },
+        }),
+        linked('linked1', {
+          ref: {
+            value: {
+              componentId: '#component1',
+              optionId: '#AlertOption4',
+            },
+          },
+        }),
+      ],
+    },
+    [
+      Alert({
+        ref: { id: '#Component1' },
+        options: optionRefs,
+      }),
+    ],
+  ),
+]);
