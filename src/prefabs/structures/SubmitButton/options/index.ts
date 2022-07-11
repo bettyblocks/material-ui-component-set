7fbeca5c638aca2b083a7b88f0a25dc0d1ff16ac
robbertruiter <robbert.ruiter@bettyblocks.com>
2022-07-11 11:54:39 +0200

    feat: update submit button tmplt-1563

--- src/prefabs/structures/SubmitButton/options/index.ts
+++ src/prefabs/structures/SubmitButton/options/index.ts
@@ -1,31 +1,55 @@
 import {
-  buttongroup,
-  showIf,
+  hideIf,
+  icon,
+  option,
+  sizes,
   toggle,
   variable,
 } from '@betty-blocks/component-sdk';
 import { advanced } from './advanced';
-import { styles } from './styles';
+import { tooltip } from './tooltip';
 
 export const options = {
   visible: toggle('Toggle visibility', {
     value: true,
     configuration: { as: 'VISIBILITY' },
   }),
-
-  type: buttongroup(
-    'type',
-    [
-      ['Submit', 'submit'],
-      ['Reset', 'reset'],
-    ],
-    {
-      value: 'submit',
-      configuration: { condition: showIf('type', 'EQ', 'never') },
-    },
-  ),
-
   buttonText: variable('Button text', { value: ['Send'] }),
-  ...styles,
+  fullWidth: toggle('Full width', { value: false }),
+  icon: icon('Icon', { value: 'none' }),
+  size: option('CUSTOM', {
+    value: 'small',
+    label: 'Icon size',
+    configuration: {
+      as: 'BUTTONGROUP',
+      dataType: 'string',
+      allowedInput: [
+        { name: 'Small', value: 'small' },
+        { name: 'Medium', value: 'medium' },
+        { name: 'Large', value: 'large' },
+      ],
+      condition: hideIf('icon', 'EQ', 'none'),
+    },
+  }),
+  iconPosition: option('CUSTOM', {
+    label: 'Icon position',
+    value: 'start',
+    configuration: {
+      as: 'BUTTONGROUP',
+      dataType: 'string',
+      allowedInput: [
+        { name: 'Start', value: 'start' },
+        { name: 'End', value: 'end' },
+      ],
+      condition: hideIf('icon', 'EQ', 'none'),
+    },
+  }),
+  outerSpacing: sizes('Outer space', {
+    value: ['0rem', '0rem', '0rem', '0rem'],
+  }),
+  disabled: toggle('Disabled', { value: false }),
+
+  ...tooltip,
+
   ...advanced,
 };
