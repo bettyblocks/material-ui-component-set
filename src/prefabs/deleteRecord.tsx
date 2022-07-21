c41cfb41a54a4812a780d6dfb5c8d5204480859f
robbertruiter <robbert.ruiter@bettyblocks.com>
2022-07-21 11:04:00 +0200

    Revert "feat: updated deleteRecord button usage"
    This reverts commit b846e033c1fa8c877b6e96906bf5de541d8778cb.

--- src/prefabs/deleteRecord.tsx
+++ src/prefabs/deleteRecord.tsx
@@ -25,6 +25,8 @@ import { options as buttonOptions } from './structures/Button/options';
 import { Column } from './structures/Column';
 import { options as columnOptions } from './structures/Column/options';
 import { Dialog } from './structures/Dialog';
+import { OpenPageButton } from './structures/OpenPage';
+import { options as openPageButtonOptions } from './structures/OpenPage/options';
 import { Paper } from './structures/Paper';
 import { Row } from './structures/Row';
 import { options as rowOptions } from './structures/Row/options';
@@ -241,13 +243,16 @@ export default makePrefab('Delete Record', attr, beforeCreate, [
       },
     },
     [
-      Button(
+      OpenPageButton(
         {
           ref: { id: '#deleteButton' },
           options: {
-            ...buttonOptions,
+            ...openPageButtonOptions,
             buttonText: variable('Button text', { value: ['Delete'] }),
             icon: icon('Icon', { value: 'Delete' }),
+            outerSpacing: sizes('Outer space', {
+              value: ['0rem', 'M', '0rem', '0rem'],
+            }),
           },
         },
         [],
@@ -502,14 +507,14 @@ export default makePrefab('Delete Record', attr, beforeCreate, [
                       },
                     },
                     [
-                      Button(
+                      OpenPageButton(
                         {
                           ref: { id: '#cancelBtn' },
                           style: {
-                            name: 'outline',
+                            name: 'outliine',
                           },
                           options: {
-                            ...buttonOptions,
+                            ...openPageButtonOptions,
                             buttonText: variable('Button text', {
                               value: ['Cancel'],
                             }),
