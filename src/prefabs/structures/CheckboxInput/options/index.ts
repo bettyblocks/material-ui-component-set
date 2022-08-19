ff41513c06f8208222a35e285d19e13c07b6ef2e
a.bendrot <angelo.bendrot@bettyblocks.com>
2022-08-19 10:02:33 +0200

    fix: remove unused var

--- src/prefabs/structures/CheckboxInput/options/index.ts
+++ src/prefabs/structures/CheckboxInput/options/index.ts
@@ -1,85 +1,81 @@
 import {
-    option,
-    toggle,
-    text,
-    color,
-    ThemeColor,
-    sizes,
-    showIfTrue,
-    variable,
-    showIf,
+  option,
+  toggle,
+  text,
+  color,
+  ThemeColor,
+  showIfTrue,
+  variable,
+  showIf,
 } from '@betty-blocks/component-sdk';
 import { advanced } from '../../advanced';
 
 const stylesConfiguration = {
-    configuration: { condition: showIfTrue('styles') },
-  };
+  configuration: { condition: showIfTrue('styles') },
+};
 const validationConfiguration = {
   configuration: { condition: showIfTrue('validationOptions') },
 };
 
 const validationOptions = {
-    validationOptions: toggle('Validation options'),
-    required: toggle('Required', validationConfiguration),
-    validationValueMissing: variable('Value required message', {
-      value: ['This field is required'],
-      ...validationConfiguration,
-    }),
-  };
+  validationOptions: toggle('Validation options'),
+  required: toggle('Required', validationConfiguration),
+  validationValueMissing: variable('Value required message', {
+    value: ['This field is required'],
+    ...validationConfiguration,
+  }),
+};
 
 export const categories = [
-    {
-        label: 'Advanced settings',
-        expanded: false,
-        members: ['dataComponentAttribute'],
-    },
+  {
+    label: 'Advanced settings',
+    expanded: false,
+    members: ['dataComponentAttribute'],
+  },
 ];
 
-
-
 export const checkboxInputOptions = {
-    actionProperty: option('ACTION_JS_PROPERTY', {
-        label: 'Property',
-        value: '',
-    }),
-    label: variable('Label', { value: ['Checkbox'] }),
-    value: variable('Value', { value: [] }),
-    ...validationOptions,
-    disabled: toggle('Disabled'),
-    helperText: variable('Helper text'),
-    actionVariableId: option('ACTION_JS_VARIABLE', { label: 'Name', value: '' }),
-    type: text('Type', {
-        value: 'checkbox',
-        configuration: { condition: showIf('actionVariableId', 'EQ', 'never') },
-    }),
-    styles: toggle('Styles'),
-    checkboxColor: color('Checkbox color', {
-        ...stylesConfiguration,
-        value: ThemeColor.ACCENT_3,
-    }),
-    checkboxColorChecked: color('Checkbox color checked', {
-        ...stylesConfiguration,
-        value: ThemeColor.PRIMARY,
-    }),
-    textColor: color('Text color', {
-        ...stylesConfiguration,
-        value: ThemeColor.BLACK,
-    }),
-    helperColor: color('Helper color', {
-        ...stylesConfiguration,
-        value: ThemeColor.ACCENT_2,
-    }),
-    errorColor: color('Error color', {
-        ...stylesConfiguration,
-        value: ThemeColor.DANGER,
-    }),
-    validationOptions: toggle('Validation options'),
-    required: toggle('Required', validationConfiguration),
-    validationValueMissing: variable('Value required message', {
-        value: ['This field is required'],
-        ...validationConfiguration,
-    }),
+  actionProperty: option('ACTION_JS_PROPERTY', {
+    label: 'Property',
+    value: '',
+  }),
+  label: variable('Label', { value: ['Checkbox'] }),
+  value: variable('Value', { value: [] }),
+  ...validationOptions,
+  disabled: toggle('Disabled'),
+  helperText: variable('Helper text'),
+  actionVariableId: option('ACTION_JS_VARIABLE', { label: 'Name', value: '' }),
+  type: text('Type', {
+    value: 'checkbox',
+    configuration: { condition: showIf('actionVariableId', 'EQ', 'never') },
+  }),
+  styles: toggle('Styles'),
+  checkboxColor: color('Checkbox color', {
+    ...stylesConfiguration,
+    value: ThemeColor.ACCENT_3,
+  }),
+  checkboxColorChecked: color('Checkbox color checked', {
+    ...stylesConfiguration,
+    value: ThemeColor.PRIMARY,
+  }),
+  textColor: color('Text color', {
+    ...stylesConfiguration,
+    value: ThemeColor.BLACK,
+  }),
+  helperColor: color('Helper color', {
+    ...stylesConfiguration,
+    value: ThemeColor.ACCENT_2,
+  }),
+  errorColor: color('Error color', {
+    ...stylesConfiguration,
+    value: ThemeColor.DANGER,
+  }),
+  validationOptions: toggle('Validation options'),
+  required: toggle('Required', validationConfiguration),
+  validationValueMissing: variable('Value required message', {
+    value: ['This field is required'],
+    ...validationConfiguration,
+  }),
 
-
-    ...advanced('CheckboxInput'),
+  ...advanced('CheckboxInput'),
 };
