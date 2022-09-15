d1ba13d38b6616d9d1b858a064bbd8cdceca64d8
benjaminrocks <benjamin.kagia@bettyblocks.com>
2022-09-15 16:09:20 +0200

    fix: accept content type when image

--- src/prefabs/structures/FileUpload/index.ts
+++ src/prefabs/structures/FileUpload/index.ts
@@ -1,25 +1,30 @@
 import { component, toggle, variable } from '@betty-blocks/component-sdk';
 import { Button } from '../Button';
-import { options } from './options';
+import { options as defaultOptions } from './options';
 import { buttonOptions } from '..';
+import { updateOption } from '../../../utils';
 
 interface Configuration {
   supportImages?: boolean;
 }
 
 export const FileUpload = (config: Configuration = {}) => {
-  return component(
-    'FileUploadInput',
-    { options: options(config.supportImages) },
-    [
-      Button({
-        label: 'upload',
-        options: {
-          ...buttonOptions,
-          buttonText: variable('Button text', { value: ['Upload'] }),
-          fullWidth: toggle('Full width', { value: true }),
-        },
-      }),
-    ],
-  );
+  const options = defaultOptions(config.supportImages);
+
+  if (config.supportImages) {
+    options.accept = updateOption(options.accept, {
+      value: ['image/*'],
+    });
+  }
+
+  return component('FileUploadInput', { options }, [
+    Button({
+      label: 'upload',
+      options: {
+        ...buttonOptions,
+        buttonText: variable('Button text', { value: ['Upload'] }),
+        fullWidth: toggle('Full width', { value: true }),
+      },
+    }),
+  ]);
 };
