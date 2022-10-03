c14a32216c28fd7070f83735fd98bc2f62073170
michel <michel.dolstra@bettyblocks.com>
2022-10-03 11:26:29 +0200

    feat: introduce proCoder styles

---
+++ src/styles/button.ts
@@ -0,0 +1,47 @@
+export const basis = {
+  backgroundColor: {
+    type: 'THEME_COLOR',
+    value: 'primary',
+  },
+  borderColor: {
+    type: 'THEME_COLOR',
+    value: 'primary',
+  },
+  borderRadius: ['0.25rem'],
+  borderStyle: 'none',
+  borderWidth: ['0rem'],
+  boxShadow:
+    '0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12)',
+  color: {
+    type: 'THEME_COLOR',
+    value: 'white',
+  },
+  fontFamily: 'Roboto',
+  fontSize: '0.875rem',
+  fontStyle: 'normal',
+  fontWeight: '500',
+  letterSpacing: 'normal',
+  lineHeight: '1',
+  padding: ['1.5rem', '1rem', '0.6875rem', '1rem'],
+  textDecoration: 'none',
+  textTransform: 'uppercase',
+};
+
+export const states = [
+  {
+    name: 'disabled',
+    cssObject: {
+      backgroundColor: {
+        type: 'THEME_COLOR',
+        value: 'secondary',
+      },
+    },
+  },
+];
+
+export default {
+  type: 'BUTTON',
+  name: 'MyCustomStylo',
+  basis,
+  states,
+};
