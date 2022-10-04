a934713f77db60291c4fc2c30ff5250cf326e078
michel <michel.dolstra@bettyblocks.com>
2022-10-04 14:52:59 +0200

    fix: set current styles used by componentSet

--- src/styles/ButtonFilled.ts
+++ src/styles/ButtonFilled.ts
@@ -1,4 +1,3 @@
-"use strict";
 // without SDK
 // export const basis = {
 //   backgroundColor: {
@@ -28,7 +27,7 @@
 //   textDecoration: 'none',
 //   textTransform: 'uppercase',
 // };
-Object.defineProperty(exports, "__esModule", { value: true });
+
 // export const states = [
 //   {
 //     name: 'disabled',
@@ -44,40 +43,46 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //     },
 //   },
 // ];
+
 // export default {
 //   type: 'BUTTON',
 //   name: 'MyCustomStylo',
 //   basis,
 //   states,
 // };
+
 // with SDK
-const component_sdk_1 = require("@betty-blocks/component-sdk");
-exports.basis = {
-    backgroundColor: component_sdk_1.themeColor('primary'),
-    borderColor: component_sdk_1.themeColor('primary'),
-    borderRadius: ['0.25rem'],
-    borderStyle: 'none',
-    borderWidth: ['0rem'],
-    boxShadow: '0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12)',
-    color: component_sdk_1.themeColor('white'),
-    fontFamily: 'Roboto',
-    fontSize: '0.875rem',
-    fontStyle: 'normal',
-    fontWeight: '500',
-    letterSpacing: 'normal',
-    lineHeight: '1',
-    padding: ['1.5rem', '1rem', '0.6875rem', '1rem'],
-    textDecoration: 'none',
-    textTransform: 'uppercase',
+import { style, themeColor, staticColor } from '@betty-blocks/component-sdk';
+
+export const basis = {
+  backgroundColor: themeColor('primary'),
+  borderColor: themeColor('primary'),
+  borderRadius: ['0.25rem'],
+  borderStyle: 'none',
+  borderWidth: ['0rem'],
+  boxShadow:
+    '0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12)',
+  color: themeColor('white'),
+  fontFamily: 'Roboto',
+  fontSize: '0.875rem',
+  fontStyle: 'normal',
+  fontWeight: '500',
+  letterSpacing: 'normal',
+  lineHeight: '1',
+  padding: ['1.5rem', '1rem', '0.6875rem', '1rem'],
+  textDecoration: 'none',
+  textTransform: 'uppercase',
 };
-exports.states = {
-    disabled: {
-        backgroundColor: component_sdk_1.themeColor('secondary'),
-        color: component_sdk_1.staticColor('#ff00ff'),
-    },
+
+export const states = {
+  disabled: {
+    backgroundColor: themeColor('secondary'),
+    color: staticColor('#ff00ff'),
+  },
 };
-exports.default = component_sdk_1.style('Button', {
-    name: 'Filled',
-    basis: exports.basis,
-    states: exports.states,
+
+export default style('Button', {
+  name: 'Filled',
+  basis,
+  states,
 });
