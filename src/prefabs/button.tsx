520c1efed998e5c86d8e405d987aa30476efdb03
Jordy Quak <jordy.quak@bettyblocks.com>
2022-10-04 11:33:29 +0200

    feat:  legacy form component set only

--- src/prefabs/button.tsx
+++
@@ -1,9 +0,0 @@
-import { Icon, prefab } from '@betty-blocks/component-sdk';
-import { Button } from './structures';
-
-const attrs = {
-  icon: Icon.ButtonIcon,
-  category: 'BUTTON',
-  keywords: ['Button'],
-};
-export default prefab('Button', attrs, undefined, [Button({})]);
--- src/prefabs/structures/advanced.ts
+++
@@ -1,9 +0,0 @@
-import { variable } from '@betty-blocks/component-sdk';
-
-export const advanced = (value: string) => {
-  return {
-    dataComponentAttribute: variable('Test attribute', {
-      value: [value],
-    }),
-  };
-};
