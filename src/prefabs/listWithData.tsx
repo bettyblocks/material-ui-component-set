f66943ab37eaa40c6a9f2411c2848408a72d991f
thomas <thomas.dekker@bettyblocks.com>
2022-10-31 15:47:04 +0100

    fix: list item not showing Property name in list with data prefab

--- src/prefabs/listWithData.tsx
+++ src/prefabs/listWithData.tsx
@@ -21,9 +21,28 @@ const beforeCreate = ({
   prefab,
   save,
   close,
+  helpers: { useModelQuery },
 }: BeforeCreateArgs) => {
   const [modelId, setModelId] = React.useState('');
-  const [property, setProperty] = React.useState('');
+  const [primaryProperty, setPrimaryProperty] = React.useState('');
+
+  const { data } = useModelQuery({
+    variables: { id: modelId },
+    skip: !modelId,
+  });
+
+  const enrichVarObj = (obj: any) => {
+    const returnObject = obj;
+    if (data && data.model) {
+      const property = data.model.properties.find(
+        (prop: any) => prop.id === obj.id[0],
+      );
+      if (property) {
+        returnObject.name = `{{ ${data.model.name}.${property.name} }}`;
+      }
+    }
+    return returnObject;
+  };
 
   // TODO: find out why structure: PrefabReference[] isn't allowed..
   const reduceStructure = (refValue: string, structure: any) =>
@@ -53,10 +72,10 @@ const beforeCreate = ({
         <Field label="Property">
           <PropertySelector
             onChange={(value: string) => {
-              setProperty(value);
+              setPrimaryProperty(value);
             }}
             modelId={modelId}
-            value={property}
+            value={primaryProperty}
           />
         </Field>
       </Content>
@@ -76,7 +95,7 @@ const beforeCreate = ({
             type: 'VARIABLE',
             label: 'Primary text',
             key: 'primaryText',
-            value: [property],
+            value: [enrichVarObj(primaryProperty)],
           };
           save(newPrefab);
         }}
