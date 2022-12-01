fb4484251bf06b6fb18ad3c3d30ae349f40d0455
KevinVH <113349570+kvhout@users.noreply.github.com>
2022-12-01 11:06:31 +0100

    chore: no longer splits the list into multiple lists

--- src/components/richTextInput.js
+++ src/components/richTextInput.js
@@ -466,29 +466,18 @@
       ) {
         const lastNode = Node.last(editor, editor.selection.focus.path);
         if (lastNode[0].text !== '' && editor.selection.focus.offset === 0) {
-          if (isBlockActive(editor, 'numbered-list', 'type')) {
-            toggleBlock(editor, 'numbered-list');
-            return;
+          Transforms.setNodes(editor, { type: 'none' });
+          if (
+            editor.children[editor.selection.anchor.path[0]].children[0]
+              .type === 'none'
+          ) {
+            if (isBlockActive(editor, 'numbered-list', 'type')) {
+              toggleBlock(editor, 'numbered-list');
+              return;
+            }
+            toggleBlock(editor, 'bullet-list');
           }
-          toggleBlock(editor, 'bulleted-list');
-          return;
         }
-
-        if (
-          window.getSelection().toString().split('\n')[0] !==
-            lastNode[0].text &&
-          (lastNode[0].text !== '' || lastNode[1].length !== 3)
-        )
-          return;
-
-        Transforms.setNodes(editor, { type: 'none' });
-        if (isBlockActive(editor, 'numbered-list', 'type')) {
-          toggleBlock(editor, 'numbered-list');
-          return;
-        }
-
-        toggleBlock(editor, 'bulleted-list');
-        return;
       }
 
       if (event.key === 'Enter') {
