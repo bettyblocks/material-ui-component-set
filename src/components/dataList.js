ee254fae805bb6164d0b08ba33020c67a4f9b619
robbertruiter <robbert.ruiter@bettyblocks.com>
2022-12-02 16:19:50 +0100

    chore: fix prettier issue

--- src/components/dataList.js
+++ src/components/dataList.js
@@ -57,13 +57,11 @@
 
         const builderLayout = () => (
           <div data-component={dataComponentAttributeText || 'DataList'}>
-            {searchProperty &&
-              searchProperty.type &&
-              searchProperty.id !== '' && (
-                <div className={classes.header}>
-                  <SearchComponent label={searchPropertyLabel} />
-                </div>
-              )}
+            {searchProperty && searchProperty.type && searchProperty.id !== '' && (
+              <div className={classes.header}>
+                <SearchComponent label={searchPropertyLabel} />
+              </div>
+            )}
             <div ref={listRef} className={isGrid ? classes.grid : undefined}>
               <div
                 className={
