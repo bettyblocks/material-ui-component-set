66019509c840c4cf724613de62845019214c4111
Alla Maley <alla.maley@bettyblocks.com>
2022-12-12 12:32:47 +0100

    feat: propposal for children selector

--- src/prefabs/dataTable.tsx
+++ src/prefabs/dataTable.tsx
@@ -3,8 +3,22 @@ import {
   Icon,
   prefab as makePrefab,
   BeforeCreateArgs,
+  size,
+  color,
+  ThemeColor,
+  property as propertyFunc,
+  toggle,
+  variable,
+  font,
+  option,
 } from '@betty-blocks/component-sdk';
-import { DataTable, DataTableColumn } from './structures';
+import {
+  DataTable,
+  DataTableColumn,
+  dataTableColumnOptions,
+  boxOptions,
+  Box,
+} from './structures';
 
 const attrs = {
   icon: Icon.DataTable,
@@ -128,10 +142,145 @@ const beforeCreate = ({
   );
 };
 
+// const reconfigure = {
+//   type: 'WRAPPER',
+//   children: [
+//     DataTableColumn({
+//       options: {
+//         ...dataTableColumnOptions,
+//         width: size('Width', {
+//           value: '',
+//           ref: {
+//             id: '#columnWidth',
+//           },
+//         }),
+//       },
+//       ref: { id: '#dataTableColumnId' },
+//     }),
+//   ],
+//   options: [
+//     {
+//       width: linked({
+//         label: 'column width',
+//         value: {
+//           ref: {
+//             componentId: '#dataTableColumnId',
+//             optionId: '#columnWidth',
+//           },
+//         },
+//       }),
+//     },
+//   ],
+//   wizardType: 'PropertySelector',
+// };
+
 const reconfigure = {
-  children: [DataTableColumn({})],
-  wizardType: 'PropertySelector',
+  type: 'WRAPPER',
+  children: [
+    DataTableColumn(
+      {
+        options: {
+          ...dataTableColumnOptions,
+          visible: toggle('Initial visibility', {
+            value: true,
+            showInReconfigure: true,
+            showInAddChild: false,
+          }),
+          sortable: toggle('Sortable', {
+            value: false,
+            showInReconfigure: true,
+          }),
+          headerText: variable('Header text', {
+            value: ['Header text sample'],
+            showInReconfigure: true,
+          }),
+          type: font('Header type', {
+            value: 'Body2',
+            showInReconfigure: true,
+          }),
+          content: variable('Content', {
+            value: ['blah'],
+            showInReconfigure: true,
+            configuration: {
+              as: 'MULTILINE',
+            },
+          }),
+          bodyType: font('Body type', {
+            value: 'Body1',
+            showInReconfigure: true,
+          }),
+          horizontalAlignment: option('CUSTOM', {
+            label: 'Column Alignment',
+            value: 'left',
+            showInReconfigure: true,
+            configuration: {
+              as: 'BUTTONGROUP',
+              dataType: 'string',
+              allowedInput: [
+                { name: 'Left', value: 'left' },
+                { name: 'Center', value: 'center' },
+                { name: 'Right', value: 'right' },
+              ],
+            },
+          }),
+          borderColor: color('BorderColor', {
+            value: ThemeColor.DARK,
+            showInReconfigure: true,
+            showInAddChild: true,
+          }),
+          width: size('Width', {
+            value: 'S',
+            showInReconfigure: true,
+            showInAddChild: true,
+          }),
+          property: propertyFunc('Property', {
+            value: '',
+            showInReconfigure: true,
+            showInAddChild: false,
+          }),
+          background: color('Background', {
+            value: ThemeColor.MEDIUM,
+            showInReconfigure: true,
+          }),
+        },
+      },
+      [
+        Box({
+          options: {
+            ...boxOptions,
+            width: size('Width my first box', {
+              value: 'L',
+              showInReconfigure: false,
+              showInAddChild: true,
+            }),
+          },
+        }),
+        Box({
+          options: {
+            ...boxOptions,
+            width: size('Width my other box', {
+              value: 'M',
+              showInReconfigure: false,
+              showInAddChild: true,
+            }),
+          },
+        }),
+      ],
+    ),
+  ],
+  wizardType: 'ChildrenSelector',
 };
+
+// const reconfigure = {
+//   children: [
+//     {
+//       structure: DataTableColumn({}, [Box({})]),
+//       options: ['width', 'property', 'nestedChildNameOption'],
+//       optionsEdit: ['width', 'property', 'name'],
+//     },
+//   ],
+//   wizardType: 'PropertySelector',
+// };
 
 export default makePrefab(
   'Datatable',
