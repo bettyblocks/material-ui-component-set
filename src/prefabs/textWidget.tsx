f4e8ec59681c81672104b61d15ff35b96c08966a
Brian van den Berg <brian.vandenberg@bettyblocks.com>
2022-12-15 14:45:15 +0100

    chore: update text widget with working beforecreate

--- src/prefabs/textWidget.tsx
+++ src/prefabs/textWidget.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import {
-  prefab,
+  prefab as makePrefab,
   Icon,
-  InteractionType,
   PrefabInteraction,
   BeforeCreateArgs,
   wrapper,
@@ -15,101 +14,218 @@ import {
   toggle,
   buttongroup,
   font,
+  PrefabComponent,
+  linked,
+  PrefabComponentOption,
+  component as makeComponent,
 } from '@betty-blocks/component-sdk';
+import { options as formOptions } from './structures/ActionJSForm/options';
 import {
   Box,
   boxOptions,
-  Text,
+  Text as TextComp,
   TextInput,
   textOptions,
   textInputOptions,
 } from './structures';
+import { IdPropertyProps, ModelQuery } from './types';
+
+const interactions: PrefabInteraction[] = [];
 
 const beforeCreate = ({
+  components: { Content, Header, Field, Footer, PropertySelector, Text },
+  prefab,
   save,
   close,
-  prefab: originalPrefab,
-  components: {
-    Header,
-    Content,
-    Footer,
-    Field,
-    ModelSelector,
-    PropertySelector,
-    Text: BeforeCreateText,
+  helpers: {
+    useModelQuery,
+    useModelIdSelector,
+    createBlacklist,
+    createUuid,
+    prepareAction,
+    getPageName,
+    setOption,
   },
-  helpers: { useModelQuery },
 }: BeforeCreateArgs) => {
-  const [modelId, setModelId] = React.useState();
-  const [model, setModel] = React.useState(null);
+  const [primaryProperty, setPrimaryProperty] = React.useState('');
+  const [idProperty, setIdProperty] = React.useState<IdPropertyProps>();
   const [validationMessage, setValidationMessage] = React.useState('');
-  const [idProperty, setIdProperty] = React.useState(null);
 
-  const modelRequest = useModelQuery({
+  const modelId = useModelIdSelector();
+  const componentId = createUuid();
+  const pageName = getPageName();
+  const unsupportedKinds = createBlacklist(['TEXT', 'URL', 'IBAN', 'STRING']);
+
+  const { data } = useModelQuery({
     variables: { id: modelId },
-    onCompleted: (result) => {
-      setModel(result.model);
-      // setIdProperty(result.model.properties.find(({ name }) => name === 'id'));
+    onCompleted: (result: ModelQuery) => {
+      setIdProperty(result.model.properties.find(({ name }) => name === 'id'));
     },
   });
 
-  const validate = () => {
-    if (modelRequest.loading) {
-      setValidationMessage(
-        'Model details are still loading, please try submitting again.',
+  const enrichVarObj = (obj: any) => {
+    const returnObject = obj;
+    if (data && data.model) {
+      const property = data.model.properties.find(
+        (prop: any) => prop.id === obj.id[0],
       );
-      return false;
+      if (property) {
+        returnObject.name = `{{ ${data.model.name}.${property.name} }}`;
+      }
     }
-
-    return true;
+    return returnObject;
   };
 
+  const transformProperty = (obj: any) => {
+    const outputProp = { ...obj };
+    if (!obj) {
+      setValidationMessage('No property is selected.');
+    }
+    if (data && data.model) {
+      const property = data.model.properties.find(
+        (prop: any) => prop.id === obj.id[0],
+      );
+      if (property) {
+        outputProp.label = property.label;
+        outputProp.kind = property.kind;
+      }
+    }
+    return outputProp;
+  };
+
+  const treeSearch = (refValue: string, structure: any) =>
+    structure.reduce((acc: string, component: PrefabComponent) => {
+      if (acc) return acc;
+      if (
+        component.ref &&
+        Object.values(component.ref).indexOf(refValue) > -1
+      ) {
+        return component;
+      }
+      return treeSearch(refValue, component.descendants);
+    }, null);
   return (
     <>
-      <Header onClose={close} title="Configure your text widget" />
+      <Header title="Configure your text widget" onClose={close} />
       <Content>
-        <Field
-          label="Select model"
-          error={
-            validationMessage && (
-              <BeforeCreateText color="#e82600">
-                {validationMessage}
-              </BeforeCreateText>
-            )
-          }
-        >
-          <ModelSelector
-            onChange={(id: any) => {
-              setModelId(id);
-            }}
-            margin
-          />
-        </Field>
-        <Field label="What question do you want to display?">
+        <Field label="Property">
           <PropertySelector
-            modelId={modelId}
-            onChange={(value: any) => {
-              setIdProperty(value);
+            onChange={(value: string) => {
+              setPrimaryProperty(value);
             }}
-            value={idProperty}
-            disabled={!modelId}
+            modelId={modelId}
+            value={primaryProperty}
+            disabledKinds={unsupportedKinds}
+            error={
+              validationMessage && (
+                <Text color="#e82600">{validationMessage}</Text>
+              )
+            }
           />
         </Field>
       </Content>
       <Footer
-        onSave={() => {
-          if (validate()) {
-            const newPrefab = { ...originalPrefab };
+        onSave={async (): Promise<void> => {
+          const newPrefab = { ...prefab };
 
-            save(newPrefab);
+          const text = treeSearch('#questionText', newPrefab.structure);
+          const textInput = treeSearch('#TextInput', newPrefab.structure);
+          const textWidgetForm = treeSearch(
+            '#TextWidgetForm',
+            newPrefab.structure,
+          );
+          const propertyObj = transformProperty(primaryProperty);
+          setOption(text, 'content', (opt: PrefabComponentOption) => ({
+            ...opt,
+            value: [propertyObj.label],
+          }));
+          setOption(textInput, 'placeholder', (opt: PrefabComponentOption) => ({
+            ...opt,
+            value: [],
+          }));
+
+          if (!idProperty) {
+            throw new Error('We could not find an idProperty.');
           }
+          textWidgetForm.id = componentId;
+          const result = await prepareAction(
+            componentId,
+            idProperty,
+            [transformProperty(primaryProperty)],
+            'create',
+            undefined,
+            `${pageName} - create ${propertyObj.label}`,
+            'public',
+          );
+
+          setOption(
+            textWidgetForm,
+            'actionId',
+            (opts: PrefabComponentOption) => ({
+              ...opts,
+              value: result.action.actionId,
+              configuration: { disabled: true },
+            }),
+          );
+          if (!modelId) {
+            // eslint-disable-next-line no-console
+            console.error('unable to set model option, no model selected');
+            return;
+          }
+          setOption(textWidgetForm, 'model', (opts: PrefabComponentOption) => ({
+            ...opts,
+            value: modelId,
+            configuration: {
+              disabled: true,
+            },
+          }));
+          setOption(
+            textInput,
+            'actionProperty',
+            (opt: PrefabComponentOption) => ({
+              ...opt,
+              value: {
+                modelProperty: enrichVarObj(primaryProperty),
+              },
+            }),
+          );
+          setOption(
+            textInput,
+            'actionVariableId',
+            (opt: PrefabComponentOption) => ({
+              ...opt,
+              value: '',
+              configuration: {
+                condition: {
+                  type: 'HIDE',
+                  option: 'actionVariableId',
+                  comparator: 'EQ',
+                  value: '',
+                },
+              },
+            }),
+          );
+
+          // const interaction = {
+          //   name: 'Submit',
+          //   sourceEvent: 'onblur',
+          //   parameters: [],
+          //   ref: {
+          //     targetComponentId: '#TextWidgetForm',
+          //     sourceComponentId: '#TextInput',
+          //   },
+          //   type: 'Global' as InteractionType,
+          // };
+
+          // newPrefab.interactions?.push(interaction);
+          setValidationMessage('');
+          save(newPrefab);
         }}
+        onClose={close}
       />
     </>
   );
 };
-
-const interactions: PrefabInteraction[] = [];
 
 const attributes = {
   category: 'LAYOUT',
@@ -118,10 +234,39 @@ const attributes = {
   variables: [],
 };
 
-export default prefab('Text Widget', attributes, beforeCreate, [
+export default makePrefab('Text Widget', attributes, beforeCreate, [
   wrapper(
     {
       label: 'Text widget',
+      options: {
+        question: linked({
+          label: 'Question',
+          value: {
+            ref: {
+              componentId: '#questionText',
+              optionId: '#textContent',
+            },
+          },
+        }),
+        placeholder: linked({
+          label: 'Placeholder',
+          value: {
+            ref: {
+              componentId: '#TextInput',
+              optionId: '#textInputPlaceholder',
+            },
+          },
+        }),
+        required: linked({
+          label: 'Required to answer',
+          value: {
+            ref: {
+              componentId: '#TextInput',
+              optionId: '#textInputRequired',
+            },
+          },
+        }),
+      },
     },
     [
       Box(
@@ -149,64 +294,75 @@ export default prefab('Text Widget', attributes, beforeCreate, [
           },
         },
         [
-          Text(
-            {
-              options: {
-                ...textOptions,
-                content: variable('Content', {
-                  ref: { id: '#textContent' },
-                  value: [],
-                  configuration: { as: 'MULTILINE' },
-                }),
-                type: font('Font', { value: ['Body1'] }),
-                outerSpacing: sizes('Outer space', {
-                  value: ['0rem', '0rem', 'S', '0rem'],
-                }),
-                fontWeight: option('CUSTOM', {
-                  label: 'Font weight',
-                  value: '500',
-                  configuration: {
-                    as: 'DROPDOWN',
-                    dataType: 'string',
-                    allowedInput: [
-                      { name: '100', value: '100' },
-                      { name: '200', value: '200' },
-                      { name: '300', value: '300' },
-                      { name: '400', value: '400' },
-                      { name: '500', value: '500' },
-                      { name: '600', value: '600' },
-                      { name: '700', value: '700' },
-                      { name: '800', value: '800' },
-                      { name: '900', value: '900' },
-                    ],
+          makeComponent(
+            'Form',
+            { ref: { id: '#TextWidgetForm' }, options: { ...formOptions } },
+            [
+              TextComp(
+                {
+                  ref: { id: '#questionText' },
+                  options: {
+                    ...textOptions,
+                    content: variable('Content', {
+                      ref: { id: '#textContent' },
+                      value: [],
+                      configuration: { as: 'MULTILINE' },
+                    }),
+                    type: font('Font', { value: ['Body1'] }),
+                    outerSpacing: sizes('Outer space', {
+                      value: ['0rem', '0rem', 'S', '0rem'],
+                    }),
+                    fontWeight: option('CUSTOM', {
+                      label: 'Font weight',
+                      value: '500',
+                      configuration: {
+                        as: 'DROPDOWN',
+                        dataType: 'string',
+                        allowedInput: [
+                          { name: '100', value: '100' },
+                          { name: '200', value: '200' },
+                          { name: '300', value: '300' },
+                          { name: '400', value: '400' },
+                          { name: '500', value: '500' },
+                          { name: '600', value: '600' },
+                          { name: '700', value: '700' },
+                          { name: '800', value: '800' },
+                          { name: '900', value: '900' },
+                        ],
+                      },
+                    }),
                   },
-                }),
-              },
-            },
-            [],
-          ),
-          TextInput(
-            {
-              label: 'Text field',
-              options: {
-                ...textInputOptions,
-                hideLabel: toggle('Hide label', { value: true }),
-                placeholder: variable('Placeholder', {
-                  ref: { id: '#textInputPlaceholder' },
-                  value: [''],
-                }),
-                margin: buttongroup(
-                  'Margin',
-                  [
-                    ['None', 'none'],
-                    ['Dense', 'dense'],
-                    ['Normal', 'normal'],
-                  ],
-                  { value: 'none' },
-                ),
-              },
-            },
-            [],
+                },
+                [],
+              ),
+              TextInput(
+                {
+                  label: 'Text field',
+                  ref: { id: '#TextInput' },
+                  options: {
+                    ...textInputOptions,
+                    hideLabel: toggle('Hide label', { value: true }),
+                    placeholder: variable('Placeholder', {
+                      ref: { id: '#textInputPlaceholder' },
+                      value: [''],
+                    }),
+                    required: toggle('Required', {
+                      ref: { id: '#textInputRequired' },
+                    }),
+                    margin: buttongroup(
+                      'Margin',
+                      [
+                        ['None', 'none'],
+                        ['Dense', 'dense'],
+                        ['Normal', 'normal'],
+                      ],
+                      { value: 'none' },
+                    ),
+                  },
+                },
+                [],
+              ),
+            ],
           ),
         ],
       ),
