42cd72fccd916154593fa42d6efbffc75dfb6352
ingmar-stipriaan <ingmar.stipriaan@bettyblocks.com>
2023-01-12 11:03:48 +0100

    feat: use reconfigure in options

--- src/prefabs/structures/DataTable/options/index.ts
+++ src/prefabs/structures/DataTable/options/index.ts
@@ -19,6 +19,8 @@ import {
   addChild,
 } from '@betty-blocks/component-sdk';
 import { advanced } from '../../advanced';
+import { DataTableColumn } from '../../DataTableColumn';
+import { dataTableColumnOptions } from '../../DataTableColumn/options';
 
 export const categories = [
   {
@@ -69,9 +71,22 @@ export const categories = [
   },
 ];
 
+const children = [
+  DataTableColumn({
+    options: {
+      ...dataTableColumnOptions,
+      property: property('Property', { value: '', showInAddChild: true }),
+    },
+  }),
+];
+
 export const dataTableOptions = {
-  reconfigure: reconfigure('Reconfigure', { value: '' }),
-  addChild: addChild('Add child', { value: '' }),
+  reconfigure: reconfigure('Reconfigure', {
+    value: { children, reconfigureWizardType: 'PropertiesSelector' },
+  }),
+  addChild: addChild('Add child', {
+    value: { children, addChildWizardType: 'PropertySelector' },
+  }),
   model: modelAndRelation('Model', { value: '' }),
   filter: filter('Filter', {
     value: {},
