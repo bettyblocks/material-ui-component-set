f43d022fb3a2dce4eeacdf5361ecd9f2fea0d003
robbertruiter <robbert.ruiter@bettyblocks.com>
2023-02-16 10:46:25 +0100

    fix: removed font weight overwrite in title

--- src/prefabs/title.tsx
+++ src/prefabs/title.tsx
@@ -1,4 +1,4 @@
-import { prefab, Icon, option } from '@betty-blocks/component-sdk';
+import { prefab, Icon } from '@betty-blocks/component-sdk';
 import { Text, textOptions } from './structures';
 
 const attr = {
@@ -20,28 +20,6 @@ const attr = {
   ],
 };
 
-const defaultOptions = { ...textOptions };
-
-defaultOptions.fontWeight = option('CUSTOM', {
-  label: 'Font weight',
-  value: '300',
-  configuration: {
-    as: 'DROPDOWN',
-    dataType: 'string',
-    allowedInput: [
-      { name: '100', value: '100' },
-      { name: '200', value: '200' },
-      { name: '300', value: '300' },
-      { name: '400', value: '400' },
-      { name: '500', value: '500' },
-      { name: '600', value: '600' },
-      { name: '700', value: '700' },
-      { name: '800', value: '800' },
-      { name: '900', value: '900' },
-    ],
-  },
-});
-
 export default prefab('Title', attr, undefined, [
-  Text({ options: defaultOptions }, []),
+  Text({ options: textOptions }, []),
 ]);
