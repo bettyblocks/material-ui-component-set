b1b739363d0d831d56524544fea216d6dcf52d63
Sander <sander.vanriessen@bettyblocks.com>
2023-03-09 16:35:35 +0100

    fix: remove the custom set current record

--- src/components/dataContainer.js
+++ src/components/dataContainer.js
@@ -35,7 +35,6 @@
         const parsedLoadingText = useText(loadingText);
         const dataComponentAttributeText =
           useText(dataComponentAttribute) || 'DataContainer';
-        const [, setOptions] = useOptions();
 
         const [interactionFilter, setInteractionFilter] = useState({});
         const [filterv2, setFilterV2] = useState({});
@@ -122,15 +121,6 @@
         const where = useFilter(completeFilter);
 
         useEffect(() => {
-          B.defineFunction('setCurrentRecord', (value) => {
-            const id = Number(value);
-            if (typeof id === 'number') {
-              setOptions({
-                currentRecord: id,
-              });
-            }
-          });
-
           B.defineFunction('Advanced filter', (value) => {
             setFilterV2(value.where);
           });
