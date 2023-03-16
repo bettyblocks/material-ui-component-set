ef337f65a0a000d0785ddfe9ea0e316828f60083
ingmar-stipriaan <ingmar.stipriaan@bettyblocks.com>
2023-03-16 14:47:45 +0100

    chore: remove condition from multilinewidget

--- src/prefabs/multilineWidget.tsx
+++ src/prefabs/multilineWidget.tsx
@@ -21,11 +21,9 @@ import {
 import { options as formOptions } from './structures/ActionJSForm/options';
 import {
   Box,
-  Conditional,
-  Text as TextComp,
+  Text,
   TextArea,
   boxOptions,
-  conditionalOptions,
   textAreaOptions,
   textOptions,
 } from './structures';
@@ -55,6 +53,9 @@ export default prefab('Multiline question', attributes, undefined, [
           },
           configuration: {
             showOnDrop: true,
+          },
+          optionRef: {
+            id: '#property',
           },
         }),
         placeholder: linked({
@@ -96,205 +97,147 @@ export default prefab('Multiline question', attributes, undefined, [
       },
     },
     [
-      Conditional(
+      Box(
         {
-          ref: { id: '#conditional' },
+          ref: {
+            id: '#questionBox',
+          },
           options: {
-            ...conditionalOptions,
-            left: variable('Left', {
-              ref: { id: '#conditionOptionLeft' },
-              value: [],
+            ...boxOptions,
+            backgroundColor: color('Background color', {
+              value: ThemeColor.WHITE,
             }),
-            compare: option('CUSTOM', {
-              label: 'Compare',
-              ref: { id: '#conditionOptionCompare' },
-              value: 'eq',
+            borderColor: color('Border color', {
+              value: ThemeColor.MEDIUM,
+            }),
+            borderWidth: size('Border thickness', {
+              value: '1px',
               configuration: {
-                as: 'DROPDOWN',
-                dataType: 'string',
-                allowedInput: [
-                  {
-                    name: 'Equals',
-                    value: 'eq',
-                  },
-                  {
-                    name: 'Not equal',
-                    value: 'neq',
-                  },
-                  {
-                    name: 'Contains',
-                    value: 'contains',
-                  },
-                  {
-                    name: 'Does not contain',
-                    value: 'notcontains',
-                  },
-                  {
-                    name: 'Greater than',
-                    value: 'gt',
-                  },
-                  {
-                    name: 'Less than',
-                    value: 'lt',
-                  },
-                  {
-                    name: 'Greater than or equal to',
-                    value: 'gteq',
-                  },
-                  {
-                    name: 'Less than or equal to',
-                    value: 'lteq',
-                  },
-                ],
+                as: 'UNIT',
               },
             }),
-            right: variable('Right', {
-              ref: { id: '#conditionOptionRight' },
-              value: [],
+            borderRadius: size('Border radius', {
+              value: '5px',
+              configuration: {
+                as: 'UNIT',
+              },
+            }),
+            outerSpacing: sizes('Outer space', {
+              value: ['0rem', '0rem', 'M', '0rem'],
+              ref: {
+                id: '#questionBoxOuterSpacing',
+              },
+            }),
+            displayLogic: displayLogic('Display logic', {
+              value: {},
+              ref: {
+                id: '#questionBoxDisplayLogic',
+              },
             }),
           },
         },
         [
-          Box(
+          component(
+            'Form',
             {
-              ref: {
-                id: '#questionBox',
-              },
+              ref: { id: '#MultilineWidgetForm' },
               options: {
-                ...boxOptions,
-                backgroundColor: color('Background color', {
-                  value: ThemeColor.WHITE,
-                }),
-                borderColor: color('Border color', {
-                  value: ThemeColor.MEDIUM,
-                }),
-                borderWidth: size('Border thickness', {
-                  value: '1px',
+                ...formOptions,
+                actionId: option('ACTION_JS', {
+                  label: 'Action',
+                  value: '',
                   configuration: {
-                    as: 'UNIT',
-                  },
-                }),
-                borderRadius: size('Border radius', {
-                  value: '5px',
-                  configuration: {
-                    as: 'UNIT',
-                  },
-                }),
-                outerSpacing: sizes('Outer space', {
-                  value: ['0rem', '0rem', 'M', '0rem'],
-                  ref: {
-                    id: '#questionBoxOuterSpacing',
-                  },
-                }),
-                displayLogic: displayLogic('Display logic', {
-                  value: {},
-                  ref: {
-                    id: '#questionBoxDisplayLogic',
+                    createAction: {
+                      template: 'update',
+                      permissions: 'inherit',
+                    },
                   },
                 }),
               },
             },
             [
-              component(
-                'Form',
+              Text(
                 {
-                  ref: { id: '#MultilineWidgetForm' },
+                  ref: { id: '#questionText' },
                   options: {
-                    ...formOptions,
-                    actionId: option('ACTION_JS', {
-                      label: 'Action',
-                      value: '',
+                    ...textOptions,
+                    content: variable('Content', {
+                      ref: { id: '#textContent' },
+                      value: [],
+                      configuration: { as: 'MULTILINE' },
+                      optionRef: {
+                        sourceId: '#property',
+                        inherit: 'label',
+                      },
+                    }),
+                    type: font('Font', {
+                      ref: { id: '#textCompType' },
+                      value: ['Body1'],
+                    }),
+                    outerSpacing: sizes('Outer space', {
+                      value: ['0rem', '0rem', 'S', '0rem'],
+                    }),
+                    fontWeight: option('CUSTOM', {
+                      label: 'Font weight',
+                      value: '500',
                       configuration: {
-                        createAction: {
-                          template: 'update',
-                          permissions: 'inherit',
-                        },
+                        as: 'DROPDOWN',
+                        dataType: 'string',
+                        allowedInput: [
+                          { name: '100', value: '100' },
+                          { name: '200', value: '200' },
+                          { name: '300', value: '300' },
+                          { name: '400', value: '400' },
+                          { name: '500', value: '500' },
+                          { name: '600', value: '600' },
+                          { name: '700', value: '700' },
+                          { name: '800', value: '800' },
+                          { name: '900', value: '900' },
+                        ],
                       },
                     }),
                   },
                 },
-                [
-                  TextComp(
-                    {
-                      ref: { id: '#questionText' },
-                      options: {
-                        ...textOptions,
-                        content: variable('Content', {
-                          ref: { id: '#textContent' },
-                          value: [],
-                          configuration: { as: 'MULTILINE' },
-                        }),
-                        type: font('Font', {
-                          ref: { id: '#textCompType' },
-                          value: ['Body1'],
-                        }),
-                        outerSpacing: sizes('Outer space', {
-                          value: ['0rem', '0rem', 'S', '0rem'],
-                        }),
-                        fontWeight: option('CUSTOM', {
-                          label: 'Font weight',
-                          value: '500',
-                          configuration: {
-                            as: 'DROPDOWN',
-                            dataType: 'string',
-                            allowedInput: [
-                              { name: '100', value: '100' },
-                              { name: '200', value: '200' },
-                              { name: '300', value: '300' },
-                              { name: '400', value: '400' },
-                              { name: '500', value: '500' },
-                              { name: '600', value: '600' },
-                              { name: '700', value: '700' },
-                              { name: '800', value: '800' },
-                              { name: '900', value: '900' },
-                            ],
-                          },
-                        }),
+                [],
+              ),
+              TextArea(
+                {
+                  label: 'Text area',
+                  ref: { id: '#textInput' },
+                  options: {
+                    ...textAreaOptions,
+                    property: property('Property', {
+                      value: '',
+                      ref: { id: '#textInputProperty' },
+                      configuration: {
+                        createProperty: {
+                          type: 'TEXT',
+                        },
                       },
-                    },
-                    [],
-                  ),
-                  TextArea(
-                    {
-                      label: 'Text area',
-                      ref: { id: '#textInput' },
-                      options: {
-                        ...textAreaOptions,
-                        property: property('Property', {
-                          value: '',
-                          ref: { id: '#textInputProperty' },
-                          configuration: {
-                            createProperty: {
-                              type: 'string',
-                            },
-                          },
-                        }),
-                        hideLabel: toggle('Hide label', { value: true }),
-                        placeholder: variable('Placeholder', {
-                          ref: { id: '#textInputPlaceholder' },
-                          value: [''],
-                        }),
-                        required: toggle('Required', {
-                          ref: { id: '#textInputRequired' },
-                        }),
-                        rows: number('Rows', {
-                          ref: { id: '#textInputRows' },
-                          value: 4,
-                        }),
-                        margin: buttongroup(
-                          'Margin',
-                          [
-                            ['None', 'none'],
-                            ['Dense', 'dense'],
-                            ['Normal', 'normal'],
-                          ],
-                          { value: 'none' },
-                        ),
-                      },
-                    },
-                    [],
-                  ),
-                ],
+                    }),
+                    hideLabel: toggle('Hide label', { value: true }),
+                    placeholder: variable('Placeholder', {
+                      ref: { id: '#textInputPlaceholder' },
+                      value: [''],
+                    }),
+                    required: toggle('Required', {
+                      ref: { id: '#textInputRequired' },
+                    }),
+                    rows: number('Rows', {
+                      value: 4,
+                    }),
+                    margin: buttongroup(
+                      'Margin',
+                      [
+                        ['None', 'none'],
+                        ['Dense', 'dense'],
+                        ['Normal', 'normal'],
+                      ],
+                      { value: 'none' },
+                    ),
+                  },
+                },
+                [],
               ),
             ],
           ),
