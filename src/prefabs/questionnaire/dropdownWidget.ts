a8d9c5dc00f605de3156e526fa06108cbb0a15e7
Stefan van Delft <stefan.van.delft@bettyblocks.com>
2023-05-30 14:41:59 +0200

    chore: Add selectableObjectKey to dropdown widget.

--- src/prefabs/questionnaire/dropdownWidget.ts
+++ src/prefabs/questionnaire/dropdownWidget.ts
@@ -166,6 +166,7 @@ export const dropdownWidget = [
                       },
                       disabled: true,
                       manageObjectValues: {
+                        selectableObjectKey: false,
                         value: [],
                       },
                     },
