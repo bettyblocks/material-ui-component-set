8fea131ce958978c365049dfe4e8e1be35175159
Rob Dekker <rob-ajax@live.nl>
2023-09-29 11:33:55 +0200

    feat: add debounceDelay option for onChange

--- src/prefabs/structures/TextInput/options/advanced.ts
+++ src/prefabs/structures/TextInput/options/advanced.ts
@@ -1,6 +1,7 @@
-import { variable } from '@betty-blocks/component-sdk';
+import { number, variable } from '@betty-blocks/component-sdk';
 
 export const advanced = {
+  debounceDelay: number('Debounce delay (in ms)'),
   dataComponentAttribute: variable('Test attribute', {
     value: [],
   }),
