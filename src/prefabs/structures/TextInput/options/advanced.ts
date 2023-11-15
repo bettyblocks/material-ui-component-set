4658991f3048a0907590678c819a526b8c364ad4
Rob Dekker <rob-ajax@live.nl>
2023-11-15 14:24:20 +0100

    chore: add "0" as default value for debounce delay

--- src/prefabs/structures/TextInput/options/advanced.ts
+++ src/prefabs/structures/TextInput/options/advanced.ts
@@ -1,7 +1,9 @@
 import { number, variable } from '@betty-blocks/component-sdk';
 
 export const advanced = {
-  debounceDelay: number('Interaction (change) delay in milliseconds'),
+  debounceDelay: number('Interaction (change) delay in milliseconds', {
+    value: '0',
+  }),
   dataComponentAttribute: variable('Test attribute', {
     value: [],
   }),
