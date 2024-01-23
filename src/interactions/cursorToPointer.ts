1068204b1fc4b51258b0136464af40bb0210ce16
Dylan Carver <dylan.carver@bettyblocks.com>
2024-01-23 11:05:51 +0100

    build: move type guard into cursorToPointer function to satisfy linter

--- src/interactions/cursorToPointer.ts
+++ src/interactions/cursorToPointer.ts
@@ -1,10 +1,9 @@
 /* eslint @typescript-eslint/no-unused-vars: ["error", { "varsIgnorePattern": "cursorToPointer" }] */
 
-function isEventType(event: any): event is Event {
-  return 'target' in event;
-}
-
 function cursorToPointer({ event }: { event: unknown }): void {
+  function isEventType(unknownEvent: any): unknownEvent is Event {
+    return 'target' in unknownEvent;
+  }
   if (isEventType(event)) {
     // eslint-disable-next-line no-param-reassign
     (event.target as HTMLAnchorElement).style.cursor = 'pointer';
