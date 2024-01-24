f5ab9341161d08679aaf8c1bf84e53d29d7de8e3
Joris Pannekeet <joris.pannekeet@bettyblocks.com>
2024-01-24 11:07:32 +0100

    fix: update cli

--- src/interactions/refetchPageVariable.ts
+++ src/interactions/refetchPageVariable.ts
@@ -1,9 +1,17 @@
+/* eslint @typescript-eslint/no-unused-vars: ["error", { "varsIgnorePattern": "refetchPageVariable" }] */
+
 interface PageVariableId {
   id: string;
 }
 
 
-function refetchPageVariable({ event, pageVariableId }: { event: Event, pageVariableId: PageVariableId }): void {
+function refetchPageVariable({
+  event,
+  pageVariableId
+}: {
+  event: Event,
+  pageVariableId: PageVariableId
+}): void {
   // @ts-ignore
   const promise = window.dispatcher(window.pageEmptyApi.endpoints[`get${pageVariableId}`].initiate());
   const { refetch } = promise;
