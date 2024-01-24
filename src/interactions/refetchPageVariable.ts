fd2fdb36c517e62a4e602dafd7da884383868f8d
Joris Pannekeet <joris.pannekeet@bettyblocks.com>
2024-01-24 10:05:45 +0100

    feat: added pageVairbale refetch interaction

--- src/interactions/refetchPageVariable.ts
+++ src/interactions/refetchPageVariable.ts
@@ -1,18 +1,12 @@
-// @ts-nocheck
 interface PageVariableId {
   id: string;
 }
 
 
 function refetchPageVariable({ event, pageVariableId }: { event: Event, pageVariableId: PageVariableId }): void {
-  console.log({ pageVariableId });
-
-  debugger;
-  const promise = window.pageEmptyApi.endpoints.get12d8a94f6f714ecd8a7ea5fc22e28789.initiate();
-  const { refetch } = promise
-  console.log({ refetch, promise });
-  console.log('fired');
-
+  // @ts-ignore
+  const promise = window.dispatcher(window.pageEmptyApi.endpoints[`get${pageVariableId}`].initiate());
+  const { refetch } = promise;
 
   refetch();
 }
