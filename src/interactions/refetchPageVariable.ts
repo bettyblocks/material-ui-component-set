c586eb16cd14c701f4186d41ea4b77e7be191f69
Joris Pannekeet <joris.pannekeet@bettyblocks.com>
2024-02-06 13:43:41 +0100

    feat: add refetch page variable interaction

---
+++ src/interactions/refetchPageVariable.ts
@@ -0,0 +1,19 @@
+/* eslint @typescript-eslint/no-unused-vars: ["error", { "varsIgnorePattern": "refetchPageVariable" }] */
+
+interface PageVariableId {
+  id: string;
+}
+function refetchPageVariable({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  event,
+  pageVariableId,
+}: {
+  event: Event;
+  pageVariableId: PageVariableId;
+}): void {
+  const functionName = `refetchPageVariableById${pageVariableId}`;
+
+  if (typeof (<any>window)[functionName] === 'function') {
+    (<any>window)[functionName]();
+  }
+}
