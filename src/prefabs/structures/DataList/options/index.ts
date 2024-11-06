32a2460fd5dc92403c44d0c8175a81776aaf6a8d
Jordy Quak <jordy.quak@bettyblocks.com>
2024-11-06 15:40:22 +0100

    chore: add fonts to datalist pagination

--- src/prefabs/structures/DataList/options/index.ts
+++ src/prefabs/structures/DataList/options/index.ts
@@ -9,6 +9,7 @@ import {
   sizes,
   variable,
   modelAndRelation,
+  font,
 } from '@betty-blocks/component-sdk';
 import { advanced } from '../../advanced';
 
@@ -16,7 +17,13 @@ export const categories = [
   {
     label: 'Pagination',
     expanded: false,
-    members: ['pagination', 'labelNumberOfPages', 'take', 'placeholderTake'],
+    members: [
+      'pagination',
+      'labelNumberOfPages',
+      'take',
+      'placeholderTake',
+      'paginationType',
+    ],
   },
   {
     label: 'Spacing',
@@ -91,6 +98,9 @@ export const dataListOptions = {
   }),
   placeholderTake: number('Placeholder rows', {
     value: '',
+  }),
+  paginationType: font('Pagination text style', {
+    value: 'Body2',
   }),
   labelNumberOfPages: variable(`Pagination label (x 'of' y)`, {
     value: ['of'],
