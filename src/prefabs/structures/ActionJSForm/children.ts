144b20f3bd4d30f49eb446862078b0eaa3996cd0
Ingmar van Stipriaan <ingmar.stipriaan@bettyblocks.com>
2024-12-16 15:46:11 +0100

    chore: add addChild option to actionJsForm

--- src/prefabs/structures/ActionJSForm/children.ts
+++ src/prefabs/structures/ActionJSForm/children.ts
@@ -1,4 +1,8 @@
-import { property, variable } from '@betty-blocks/component-sdk';
+import {
+  CreateActionInputVariableKind,
+  property,
+  variable,
+} from '@betty-blocks/component-sdk';
 
 import {
   AutocompleteInput,
@@ -26,9 +30,69 @@ import {
 } from '..';
 
 const defaultOptions = {
-  property: property('Property', { value: '', showInReconfigure: true }),
-  label: variable('Label', { value: [''], showInReconfigure: true }),
+  property: property('Property', {
+    value: '',
+    showInReconfigure: true,
+  }),
+  label: variable('Label', {
+    value: [''],
+    showInReconfigure: true,
+  }),
 };
+
+const propertyOption = (
+  allowedKinds: string[],
+  actionInputType: CreateActionInputVariableKind,
+) => {
+  return {
+    property: property('Property', {
+      value: '',
+      showInReconfigure: true,
+      showInAddChild: true,
+      optionRef: {
+        id: '#propertyRef',
+      },
+      configuration: {
+        allowedKinds,
+        createActionInputVariable: {
+          type: actionInputType,
+        },
+      },
+    }),
+  };
+};
+
+const labelOption = {
+  label: variable('Label', {
+    value: [''],
+    showInReconfigure: true,
+    optionRef: {
+      sourceId: '#propertyRef',
+      inherit: [{ name: '$name', id: '$id', type: 'PROPERTY_LABEL' }],
+    },
+  }),
+};
+
+export const inputTypes = [
+  {
+    label: 'Text (single line)',
+    structure: [
+      TextInput({
+        label: 'Text (single line) input',
+        inputLabel: 'Text (single line)',
+        type: 'text',
+        options: {
+          ...textInputOptions,
+          ...labelOption,
+          ...propertyOption(
+            ['TEXT', 'URL', 'IBAN', 'STRING'],
+            CreateActionInputVariableKind.TEXT,
+          ),
+        },
+      }),
+    ],
+  },
+];
 
 export const children = [
   TextInput({
