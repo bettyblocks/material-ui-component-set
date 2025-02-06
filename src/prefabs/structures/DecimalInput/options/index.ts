eceb1210eedd7b72025b40ac93abf9adbffa8b4b
michel <michel.dolstra@bettyblocks.com>
2025-02-06 11:16:21 +0100

    chore: remove showOnDrop config

--- src/prefabs/structures/DecimalInput/options/index.ts
+++ src/prefabs/structures/DecimalInput/options/index.ts
@@ -29,7 +29,6 @@ export const options = {
       allowedKinds: ['INTEGER', 'PRICE', 'PRICE_EXPRESSION'],
       disabled: true,
       condition: hideIf('property', 'EQ', ''),
-      showOnDrop: true,
     },
   }),
 
